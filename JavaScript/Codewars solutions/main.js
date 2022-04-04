4508f90365c3317c040991663655dbab50c2efea
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-04-04 04:17:55 -0700

    Update main.js

--- JavaScript/Codewars solutions/main.js
+++ JavaScript/Codewars solutions/main.js
@@ -24,3 +24,22 @@ function isTriangle(a,b,c)
 {
    return a + b > c && a + c > b && c + b > a;
 }
+
+// Description:
+
+// Mr. Scrooge has a sum of money 'P' that he wants to invest. Before he does, he wants to know how many years 'Y' this sum 'P' has to be kept in the bank in order for it to amount to a desired sum of money 'D'.
+
+// The sum is kept for 'Y' years in the bank where interest 'I' is paid yearly. After paying taxes 'T' for the year the new sum is re-invested.
+
+// Note to Tax: not the invested principal is taxed, but only the year's accrued interest
+
+function calculateYears(principal, interest, tax, desired) {
+  // your code
+let years = 0;
+while (principal < desired) {
+  principal += principal * interest * (1 - tax);
+  years++
+}
+return years
+}
+console.log(calculateYears(1000, 0.05, 0.18, 1100))
