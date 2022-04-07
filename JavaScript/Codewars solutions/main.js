34dcef16d06e3dc38356db286d7228172ea03441
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-04-07 13:15:09 -0700

    Update main.js

--- JavaScript/Codewars solutions/main.js
+++ JavaScript/Codewars solutions/main.js
@@ -68,3 +68,20 @@ function int(x) {
 
 }
 console.log(int(-5))
+
+
+// As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+
+// The input to the function will be an array of three distinct numbers (Haskell: a tuple).
+
+// For example:
+
+// gimme([2, 3, 1]) => 0
+
+// 2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+
+function gimme (triplet) {
+  let arr = triplet.slice(0).sort((a,b) => a - b)
+  return triplet.indexOf(arr[1])
+}
+console.log(gimme([2, 3, 1]))
