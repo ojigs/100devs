1a2bb7bc2e657c4faf6e65a19ba937cfc88ed69d
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-04-08 16:52:51 -0700

    Update main.js

--- JavaScript/Codewars solutions/main.js
+++ JavaScript/Codewars solutions/main.js
@@ -100,3 +100,33 @@ function longest(a, b) {
   }
   return bArr.sort((a,b) => a > b ? 1 : -1).toString()
 }
+
+
+// You are given two arrays a1 and a2 of strings. Each string is composed with letters from a to z. Let x be any string in the first array and y be any string in the second array.
+
+// Find max(abs(length(x) − length(y)))
+
+// If a1 and/or a2 are empty return -1 in each language except in Haskell (F#) where you will return Nothing (None).
+// Example:
+
+let a1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
+let a2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
+// mxdiflg(a1, a2) --> 13
+
+// Bash note:
+
+//     input : 2 strings with substrings separated by ,
+//     output: number as a string
+
+function mdfFile(a, b) {
+  if (a1.length === 0 || a2.length === 0) return -1
+  let aLength = a.map(item => item.length) 
+  let bLength = b.map(item => item.length)
+  let newArr = []
+  for (const x of aLength) {
+    for (const y of bLength) {
+      newArr.push(Math.abs(x-y))
+    }
+  }
+  return Math.max( ...newArr )
+}
