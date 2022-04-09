394ae86cd021009933a6cad702d9e24c9e5e7bca
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-04-09 13:03:53 -0700

    Update main.js

--- JavaScript/Codewars solutions/main.js
+++ JavaScript/Codewars solutions/main.js
@@ -130,3 +130,50 @@ function mdfFile(a, b) {
   }
   return Math.max( ...newArr )
 }
+
+
+// This kata is the first of a sequence of four about "Squared Strings".
+// You are given a string of n lines, each substring being n characters long: For example:
+// s = "abcd\nefgh\nijkl\nmnop"
+// We will study some transformations of this square of strings.
+//     Vertical mirror: vert_mirror (or vertMirror or vert-mirror)
+//     vert_mirror(s) => "dcba\nhgfe\nlkji\nponm"
+//     Horizontal mirror: hor_mirror (or horMirror or hor-mirror)
+//      hor_mirror(s) => "mnop\nijkl\nefgh\nabcd"
+// or printed:
+
+// vertical mirror   |horizontal mirror   
+// abcd --> dcba     |abcd --> mnop 
+// efgh     hgfe     |efgh     ijkl 
+// ijkl     lkji     |ijkl     efgh 
+// mnop     ponm     |mnop     abcd 
+
+// Task:
+//     Write these two functions
+// and
+//     high-order function oper(fct, s) where
+//     fct is the function of one variable f to apply to the string s (fct will be one of vertMirror, horMirror)
+
+// Examples:
+
+s = "abcd\nefgh\nijkl\nmnop"
+// oper(vert_mirror, s) => "dcba\nhgfe\nlkji\nponm"
+// oper(hor_mirror, s) => "mnop\nijkl\nefgh\nabcd"
+
+let str = "hSgdHQ\nHnDMao\nClNNxX\niRvxxH\nbqTVvA\nwvSyRu"
+
+function horMirror(s) {
+  return s.split('\n').reverse().map(item => item+'\n').join('').slice(0, -1)
+}
+console.log(horMirror(s))
+
+function verMirror(s) {
+  return s.split('\n').map(item => item.split('').reverse().join('')).map(item => item+'\n').join('')
+}
+console.log(verMirror(str))
+
+function oper(fct, s) {
+  return fct(s)
+}
+console.log(oper(verMirror, s))
+
