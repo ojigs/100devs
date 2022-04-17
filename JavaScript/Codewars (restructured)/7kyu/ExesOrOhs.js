e5191bd3b54924aa8bb44f3ba11802f449af8322
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-04-17 10:42:10 -0700

    Create ExesOrOhs.js

---
+++ JavaScript/Codewars (restructured)/7kyu/ExesOrOhs.js
@@ -0,0 +1,24 @@
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+// Examples input/output:
+
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+function XO(str) {
+    let a =  str.split('').filter(e => e.toLowerCase() == 'o')
+    let b = str.split('').filter(e => e.toLowerCase() === 'x')
+    return a.length === b.length ? true : false
+}
+let str = 'ooom'
+console.log(XO(str))
+
+Test cases:
+Test.assertEquals(XO('xo'),true);
+Test.assertEquals(XO("xxOo"),true);
+Test.assertEquals(XO("xxxm"),false);
+Test.assertEquals(XO("Oo"),false);
+Test.assertEquals(XO("ooom"),false);
