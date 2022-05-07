bbeab56e14793ea1543537359e6c9c668057e6e4
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-05-07 11:40:00 -0700

    Update main.js

--- JavaScript/Codewars solutions/main.js
+++ JavaScript/Codewars solutions/main.js
@@ -178,11 +178,25 @@ function oper(fct, s) {
 console.log(oper(verMirror, s))
 
 
-
+//SQUARE(n)SUM
 // Complete the square sum function so that it squares each number passed into it and then sums the results together.
 
 // For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 
+//My solution
 function squareSum(numbers){
   return numbers.reduce((sum, current) => sum + Math.pow(current, 2), 0)
+}
+
+//Other solution
+function squareSum(numbers){
+  return numbers.map(square).reduce(sum);
+}
+
+function square(number) {
+  return number * number;
+}
+
+function sum(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
 }
