733a95500fd68fedf0279320f81623786595a114
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-05-08 04:42:18 -0700

    Update replaceWithAlphabetPosition.js

--- JavaScript/Codewars (restructured)/6kyu/replaceWithAlphabetPosition.js
+++ JavaScript/Codewars (restructured)/6kyu/replaceWithAlphabetPosition.js
@@ -11,3 +11,15 @@
 
 // Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
 
+
+//best solutions
+function alphabetPosition(text) {
+  var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  return text.toLowerCase().split('')
+  .filter( letter => {
+    let index = alphabet.indexOf(letter);
+    return index > -1;
+  }  )
+  .map( letter => alphabet.indexOf(letter) + 1 )
+  .join(' ')
+}
