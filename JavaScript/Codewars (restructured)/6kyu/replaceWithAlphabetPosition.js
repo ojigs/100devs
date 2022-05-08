a80d73151c6fb3a55138e4dacecbfba558d95dec
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-05-08 06:21:02 -0700

    Update replaceWithAlphabetPosition.js

--- JavaScript/Codewars (restructured)/6kyu/replaceWithAlphabetPosition.js
+++ JavaScript/Codewars (restructured)/6kyu/replaceWithAlphabetPosition.js
@@ -24,6 +24,15 @@ console.log(alphabetPosition("The sunset sets at twelve o' clock."))
 
 //best solutions
 function alphabetPosition(text) {
+  return text
+    .toUpperCase()
+    .match(/[a-z]/gi)
+    .map( (c) => c.charCodeAt() - 64)
+    .join(' ');
+}
+
+
+function alphabetPosition(text) {
   var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
   return text.toLowerCase().split('')
   .filter( letter => {
