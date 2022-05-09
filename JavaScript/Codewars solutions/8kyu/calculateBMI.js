3c9609b7b427d0c83f7d0ed720469406e98f278b
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-05-09 15:48:54 -0700

    Update calculateBMI.js

--- JavaScript/Codewars solutions/8kyu/calculateBMI.js
+++ JavaScript/Codewars solutions/8kyu/calculateBMI.js
@@ -7,3 +7,30 @@
 // if bmi <= 30.0 return "Overweight"
 
 // if bmi > 30 return "Obese"
+
+
+//My solution 
+function bmi(weight, height) {
+  let bodyMassIndex = weight/height**2
+  switch (true) {
+      case (bodyMassIndex <= 18.5): {
+        return 'Underweight'
+      }
+      case (bodyMassIndex <= 25.0): {
+        return 'Normal'
+      }
+      case (bodyMassIndex <= 30.0): {
+        return 'Overweight'
+      }
+      default: {
+      return 'Obese'
+      }
+  }
+}
+console.log(bmi(80, 0.18)  //'Normal'
+
+
+//other solutions
+const bmi = (w, h, bmi = w/h/h) =>  bmi <= 18.5 ? "Underweight" :
+                                    bmi <= 25 ? "Normal" :
+                                    bmi <= 30 ? "Overweight" : "Obese";
