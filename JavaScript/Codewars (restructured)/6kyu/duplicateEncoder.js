02f91dcc99480595be7a0d722989ea09a9adeb10
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-05-11 05:42:04 -0700

    Update duplicateEncoder.js

--- JavaScript/Codewars (restructured)/6kyu/duplicateEncoder.js
+++ JavaScript/Codewars (restructured)/6kyu/duplicateEncoder.js
@@ -31,3 +31,20 @@ function duplicateEncode(word){
     .join('');
 }
 
+
+
+function duplicateEncode(word){
+   
+    var unique='';
+    word = word.toLowerCase();
+    for(var i=0; i<word.length; i++){
+        if(word.lastIndexOf(word[i]) == word.indexOf(word[i])){
+            unique += '(';
+        }
+        else{
+            unique += ')';
+        }
+    }
+    return unique;
+
+}
