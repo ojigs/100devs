2c31870c78b7a2071c49156a92f756ec8d9a28b7
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-05-11 05:28:53 -0700

    Update duplicateEncoder.js

--- JavaScript/Codewars (restructured)/6kyu/duplicateEncoder.js
+++ JavaScript/Codewars (restructured)/6kyu/duplicateEncoder.js
@@ -9,3 +9,14 @@
 // Notes
 
 // Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+
+
+function duplicateEncode(word){
+  return word
+    .toLowerCase()
+    .split('')
+    .map( function (a, i, w) {
+      return w.indexOf(a) == w.lastIndexOf(a) ? '(' : ')'
+    })
+    .join('');
+}
