1a2a299c11074b2c80d96b9a78089f9026b7172a
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-05-12 16:17:21 -0700

    Update sumOfTwpLowestPositiveIntegers.js

--- JavaScript/Codewars (restructured)/7kyu/sumOfTwpLowestPositiveIntegers.js
+++ JavaScript/Codewars (restructured)/7kyu/sumOfTwpLowestPositiveIntegers.js
@@ -3,3 +3,7 @@
 // For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
 
 // [10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+function sumTwoSmallestNumbers(numbers) {  
+  //Code here
+}
