aaccd891a2db586ae88a794904735eb9f4a55150
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-05-14 12:52:58 -0700

    Update sumOfFirstNthTermOfSeries.js

--- JavaScript/Codewars (restructured)/7kyu/sumOfFirstNthTermOfSeries.js
+++ JavaScript/Codewars (restructured)/7kyu/sumOfFirstNthTermOfSeries.js
@@ -18,3 +18,35 @@
 // 2 --> 1 + 1/4 --> "1.25"
 // 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
 
+//My Solution
+function SeriesSum(n)
+{
+  // Happy Coding ^_^
+  let arr = []
+  let r = 1
+  for (let i = 0; i < n; i++) {
+      arr.push(1/r)
+      r += 3
+  }
+ return (arr.reduce((sum, curr) => sum + curr, 0)).toFixed(2)
+      
+}
+
+//Other solutions
+function SeriesSum(n) {
+  for (var s = 0, i = 0; i < n; i++) {
+    s += 1 / (1 + i * 3)
+  }
+  
+  return s.toFixed(2)
+}
+
+
+function SeriesSum(n)
+{
+  var s = 0;
+  while(n) {
+    s += 1/(1 + 3 * --n);
+  }
+  return s.toFixed(2);
+}
