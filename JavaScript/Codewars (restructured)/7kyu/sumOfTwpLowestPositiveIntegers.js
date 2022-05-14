1e817371276ca474bd0958f8505515d7944df82c
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-05-14 08:29:20 -0700

    Update sumOfTwpLowestPositiveIntegers.js

--- JavaScript/Codewars (restructured)/7kyu/sumOfTwpLowestPositiveIntegers.js
+++ JavaScript/Codewars (restructured)/7kyu/sumOfTwpLowestPositiveIntegers.js
@@ -4,6 +4,21 @@
 
 // [10, 343445353, 3453445, 3453545353453] should return 3453455.
 
+//My solution
 function sumTwoSmallestNumbers(numbers) {  
-  //Code here
+    //Code here
+    return numbers.sort((a, b) => a - b).filter((item, index) => index <= 1).reduce((sum, curr) => sum + curr, 0)
+  }
+
+
+//Other solutions
+function sumTwoSmallestNumbers(numbers){  
+  numbers = numbers.sort(function(a, b){return a - b; });
+  return numbers[0] + numbers[1];
+};
+
+
+function sumTwoSmallestNumbers(numbers) {  
+  var [ a, b ] = numbers.sort((a, b) => a - b)
+  return a + b
 }
