5fd681d8e027bdd72887127b21b71cbcbcb58488
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-05-16 10:58:44 -0700

    Update removingElements.js

--- JavaScript/Codewars solutions/8kyu/removingElements.js
+++ JavaScript/Codewars solutions/8kyu/removingElements.js
@@ -5,3 +5,27 @@
 // ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
 
 // None of the arrays will be empty, so you don't have to worry about that!
+
+//My solution
+function removeEveryOther(arr){
+  //your code here
+  return arr.filter((item,index) => index % 2 == 0)
+}
+
+//Other solutions
+function removeEveryOther(arr){
+  var newArr=[];
+for (var i = 0; i < arr.length; i+=2){
+  newArr.push(arr[i]);
+  }
+return newArr;
+}
+
+
+function removeEveryOther(arr){
+  //your code here
+  for (var i = 1; i < arr.length;i++){
+      arr.splice(i,1);
+  }
+  return arr;
+}
