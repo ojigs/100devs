802ee8032368a960dc1cbf274ad569cdb5a6ca55
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-05-16 10:05:16 -0700

    Update beginnerSeries.js

--- JavaScript/Codewars solutions/8kyu/beginnerSeries.js
+++ JavaScript/Codewars solutions/8kyu/beginnerSeries.js
@@ -5,3 +5,22 @@
 // 1.08 --> 30
 
 // Note! The input is a Real number (actual type is language dependent) and is >= 0. The result should be an Integer.
+
+//My solution
+function cockroachSpeed(s) {
+    //Good Luck!
+    return Math.floor(100000/3600 * s)
+}
+
+console.log(cockroachSpeed(1.08))
+
+//other solutions
+const cockroachSpeed = s => Math.floor(s / 0.036);
+
+
+function cockroachSpeed(s) {
+  const secsInHour = 3600;
+  const centimetersInKilometers = 100000;
+  
+  return Math.floor((s*centimetersInKilometers)/secsInHour);
+}
