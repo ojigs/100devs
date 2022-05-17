b8a40a99a52a08b4acdde56667d7a5876369b384
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-05-17 15:22:30 -0700

    Update stringRepeat.js

--- JavaScript/Codewars solutions/8kyu/stringRepeat.js
+++ JavaScript/Codewars solutions/8kyu/stringRepeat.js
@@ -2,3 +2,26 @@
 
 // repeatStr(6, "I") // "IIIIII"
 // repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+
+//My solution
+function repeatStr(n, s) {
+    let arr = []
+    for (let i = 0; i < n; i++) {
+        arr.push(s)
+    }
+    return arr.join('')
+}
+
+//other solutions
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
+
+
+function repeatStr (n, s) {
+var str="";
+for(var i=0; i < n; i++)
+  str+=s;
+  return str;
+}
