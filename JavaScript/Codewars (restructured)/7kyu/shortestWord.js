0219d8fee9c804c8d4e5dd8387adc8f7ff96db70
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-05-17 15:41:03 -0700

    Update shortestWord.js

--- JavaScript/Codewars (restructured)/7kyu/shortestWord.js
+++ JavaScript/Codewars (restructured)/7kyu/shortestWord.js
@@ -3,3 +3,23 @@
 // String will never be empty and you do not need to account for different data types.
 
 //My solution
+function findShort(s){
+    return s.split(' ').sort((a, b) => a.length - b.length)[0].length
+}
+
+//Other solutions
+function findShort(s){
+    return Math.min(...s.split(" ").map (s => s.length));
+}
+
+
+function findShort(s){
+  var arr = s.split(' ');
+  var smallest = arr[0];
+  for (var i = 0; i < arr.length; i++) {
+    if(arr[i].length < smallest.length){
+      smallest = arr[i];
+    }
+  }
+  return smallest.length;
+}
