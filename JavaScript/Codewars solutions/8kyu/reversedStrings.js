04999eb1d20dc070fe5cd79f40d4fe1e947c98ae
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-05-18 13:15:41 -0700

    Update reversedStrings.js

--- JavaScript/Codewars solutions/8kyu/reversedStrings.js
+++ JavaScript/Codewars solutions/8kyu/reversedStrings.js
@@ -3,3 +3,20 @@
 // 'world'  =>  'dlrow'
 // 'word'   =>  'drow'
 
+//My solution
+function solution(str){
+  return str.split('').reverse().join('')
+}
+
+
+//Other solutions
+const solution = s => [...s].reverse().join('')
+
+
+
+function solution(s){
+  var o = '';
+  for (var i = s.length - 1; i >= 0; i--)
+    o += s[i];
+  return o;
+}
