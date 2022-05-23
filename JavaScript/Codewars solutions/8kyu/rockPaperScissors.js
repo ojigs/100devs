021aaec3c0313b93885e41d6e6d6ca17eaafb8df
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-05-23 15:07:49 -0700

    Update rockPaperScissors.js

--- JavaScript/Codewars solutions/8kyu/rockPaperScissors.js
+++ JavaScript/Codewars solutions/8kyu/rockPaperScissors.js
@@ -21,3 +21,32 @@ const rps = (p1, p2) => {
     return 'Draw!'
   }
 };
+
+
+//Other solutions
+const rps = (p1, p2) => {
+  var map = {
+    'rock': 'scissors',
+    'scissors': 'paper',
+    'paper': 'rock'
+  };
+  
+  if (p1 == p2) {
+    return 'Draw!';
+  } else {
+    return 'Player ' + (map[p1] == p2 ? 1 : 2) + ' won!';
+  }
+};
+
+
+function rps(p1, p2) {
+  var choices = ['rock', 'paper', 'scissors'];
+  var x = choices.indexOf(p1);
+  var y = choices.indexOf(p2);
+  
+  if (x === y) return 'Draw!';
+  if (x === 0 && y === 2) return 'Player 1 won!';
+  if (x === 2 && y === 0) return 'Player 2 won!';
+  if (x > y) return 'Player 1 won!'
+  else return 'Player 2 won!';
+}
