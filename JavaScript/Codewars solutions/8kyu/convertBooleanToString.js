468ccd328a8f32b9c11972525b41b5cddfd61e36
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-05-22 23:00:17 -0700

    Update convertBooleanToString.js

--- JavaScript/Codewars solutions/8kyu/convertBooleanToString.js
+++ JavaScript/Codewars solutions/8kyu/convertBooleanToString.js
@@ -7,3 +7,16 @@ function booleanToString(b){
   //your code here
   return b.toString()
 }
+
+
+//other solutions
+function booleanToString(b){
+  return b ? 'true' : 'false';
+}
+
+
+function booleanToString(b){
+  if (typeof b == 'boolean') {
+    return String(b);
+  }
+}
