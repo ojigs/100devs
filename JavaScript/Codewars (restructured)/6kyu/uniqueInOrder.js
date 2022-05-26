e45a261dfc7b52a46252832d6866de1d119745a2
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-05-26 01:20:46 -0700

    Update uniqueInOrder.js

--- JavaScript/Codewars (restructured)/6kyu/uniqueInOrder.js
+++ JavaScript/Codewars (restructured)/6kyu/uniqueInOrder.js
@@ -5,3 +5,39 @@
 // uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
 // uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
 // uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+
+
+//My solution
+const uniqueInOrder = iterable => {
+  //your code here - remember iterable can be a string or an array
+  if (typeof iterable == 'string') {
+    iterable = iterable.split('')
+  }
+  let arr = []
+  for (let i = 0; i < iterable.length; i++) {
+    if (iterable[i+1] !== iterable[i]) {
+      arr.push(iterable[i])
+    }
+  }
+  return arr
+}
+
+
+//other solutions
+function uniqueInOrder(it) {
+  var result = []
+  var last
+  
+  for (var i = 0; i < it.length; i++) {
+    if (it[i] !== last) {
+      result.push(last = it[i])
+    }
+  }
+  
+  return result
+}
+
+
+var uniqueInOrder=function(iterable){
+    return [...iterable].filter((a, i) => a !== iterable[i-1])
+}
