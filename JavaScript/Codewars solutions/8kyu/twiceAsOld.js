b5e85b8524a7f679e38cdcccfbd7374ebc761ee0
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-05-30 03:23:01 -0700

    Update twiceAsOld.js

--- JavaScript/Codewars solutions/8kyu/twiceAsOld.js
+++ JavaScript/Codewars solutions/8kyu/twiceAsOld.js
@@ -4,3 +4,19 @@
 //     current age of his son (years)
 
 // Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+
+//My solution
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  // your code here
+  return Math.abs(dadYearsOld - 2 * sonYearsOld)
+  
+}
+
+
+//Other solutions
+function twiceAsOld(a, b) {
+  return a>2*b ? a-2*b : 2*b-a;
+}
+
+
+
