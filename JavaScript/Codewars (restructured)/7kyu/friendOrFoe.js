3fabebb6b5ed3d8662b139f3ee2fc51a1a28fe2d
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-06-02 11:06:50 -0700

    Update friendOrFoe.js

--- JavaScript/Codewars (restructured)/7kyu/friendOrFoe.js
+++ JavaScript/Codewars (restructured)/7kyu/friendOrFoe.js
@@ -9,3 +9,32 @@
 // friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
 
 // Note: keep the original order of the names in the output.
+
+
+//My solution
+function friend(friends){
+  //your code here
+  return friends.filter(item => item.length === 4)
+}
+
+
+//other solution
+function friend(friends){
+  
+  //Create new array called "myFriends" for add your friends
+  var i,
+      len = friends.length,
+      myFriends = [];
+  
+  
+  for (i = 0; i < len; i++) {
+        
+       //Check for names with length equal to four and it is not a number
+       if(friends[i].length == 4 && isNaN(friends[i])) {
+          myFriends.push(friends[i]);
+       }
+      
+  }
+    
+  return myFriends;
+}
