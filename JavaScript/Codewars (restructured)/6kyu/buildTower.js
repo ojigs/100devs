f0d55f6ba15b202bf6791ada0c3fca26b93732e6
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-06-03 13:54:11 -0700

    Update buildTower.js

--- JavaScript/Codewars (restructured)/6kyu/buildTower.js
+++ JavaScript/Codewars (restructured)/6kyu/buildTower.js
@@ -41,3 +41,16 @@ function towerBuilder(n) {
   }
   return arr
 }
+
+
+
+//other solution
+function towerBuilder(nFloors) {
+  var tower = [];
+  for (var i = 0; i < nFloors; i++) {
+    tower.push(" ".repeat(nFloors - i - 1)
+             + "*".repeat((i * 2)+ 1)
+             + " ".repeat(nFloors - i - 1));
+  }
+  return tower;
+}
