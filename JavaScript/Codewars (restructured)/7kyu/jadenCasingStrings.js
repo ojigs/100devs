bef6ea5205d470adc78f92c64c7f2d546ababa07
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-06-04 15:19:33 -0700

    Update jadenCasingStrings.js

--- JavaScript/Codewars (restructured)/7kyu/jadenCasingStrings.js
+++ JavaScript/Codewars (restructured)/7kyu/jadenCasingStrings.js
@@ -6,3 +6,9 @@
 
 // Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
 // Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+
+
+//My solution
+String.prototype.toJadenCase = function() {
+  return this.split(' ').map(item => item.replace(item[0], item[0].toUpperCase())).join(' ')
+}
