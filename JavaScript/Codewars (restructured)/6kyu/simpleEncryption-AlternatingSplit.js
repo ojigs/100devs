6aced419df7ee158e9c088b4173e42344a5ee781
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-06-06 15:00:36 -0700

    Update simpleEncryption-AlternatingSplit.js

--- JavaScript/Codewars (restructured)/6kyu/simpleEncryption-AlternatingSplit.js
+++ JavaScript/Codewars (restructured)/6kyu/simpleEncryption-AlternatingSplit.js
@@ -65,3 +65,19 @@ function decrypt(encryptedText, n) {
   }
   return encryptedText;
 }
+
+
+function encrypt(text, n) {
+  for (let i = 0; i < n; i++) {
+    text = text && text.replace(/.(.|$)/g, '$1') + text.replace(/(.)./g, '$1') 
+  }
+  return text
+}
+
+function decrypt(text, n) {
+  let l = text && text.length / 2 | 0
+  for (let i = 0; i < n; i++) {
+    text = text.slice(l).replace(/./g, (_, i) => _ + (i < l ? text[i] : ''))
+  }
+  return text
+}
