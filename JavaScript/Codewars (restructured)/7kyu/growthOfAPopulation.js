59253746f8bdeee43187f790d6bc101acf979e64
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-06-09 13:31:24 -0700

    Update growthOfAPopulation.js

--- JavaScript/Codewars (restructured)/7kyu/growthOfAPopulation.js
+++ JavaScript/Codewars (restructured)/7kyu/growthOfAPopulation.js
@@ -42,3 +42,23 @@ function nbYear(p0, percent, aug, p) {
 
 
 //other solutions
+function nbYear(p0, percent, aug, p) {
+    
+  for (var years = 0; p0 < p; years++) {
+    p0 = Math.floor(p0 + p0 * percent / 100 + aug);
+  }
+  return years
+}
+
+
+function nbYear(p0, percent, aug, p) {
+    var i = 1;
+    var mult = 1 + percent / 100.0;
+    var prev = p0;
+    while (prev < p) {
+        var ne = Math.floor(prev * mult + aug)
+        prev = ne
+        i++;
+    }
+    return (i - 1);
+}
