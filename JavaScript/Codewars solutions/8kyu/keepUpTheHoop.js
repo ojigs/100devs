80b8c4aeaa41240425f70ed5fac036592b673730
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-06-11 13:13:51 -0700

    Update keepUpTheHoop.js

--- JavaScript/Codewars solutions/8kyu/keepUpTheHoop.js
+++ JavaScript/Codewars solutions/8kyu/keepUpTheHoop.js
@@ -7,3 +7,21 @@
 //     If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
 //     If he doesn't get 10 hoops, return the string "Keep at it until you get it".
 
+
+//my solution
+function hoopCount (n) {
+   return n < 10 ? "Keep at it until you get it" : "Great, now move on to tricks"
+}
+
+
+
+//other solution
+function hoopCount (n) {
+  if(Number.isInteger(n) ) {
+   if(n>=10) {
+     return "Great, now move on to tricks";
+   } else {
+     return "Keep at it until you get it";
+   };
+ };
+}
