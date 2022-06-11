832ce017999c187060956f1ee9f060a8de7016cb
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-06-10 17:05:02 -0700

    Update filterOutTheGeese.js

--- JavaScript/Codewars solutions/8kyu/filterOutTheGeese.js
+++ JavaScript/Codewars solutions/8kyu/filterOutTheGeese.js
@@ -15,3 +15,24 @@
 // ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
 
 // The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated.
+
+
+//My solution
+function gooseFilter (birds) {
+  const geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+//   let arr = []
+//   for (let i = 0; i < birds.length; i++) {
+//     if (!geese.includes(birds[i])) {
+//       arr.push(birds[i])
+//     }
+//   }
+//   return arr
+  return birds.filter(item => !geese.includes(item))
+};
+
+
+//other solutions
+function gooseFilter (birds) {
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  return birds.filter( bird => geese.indexOf(bird) < 0 );
+};
