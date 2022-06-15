d7bf7cab33ba8ed61680ccb3f6fa4bb3a7a90852
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-06-15 10:16:41 -0700

    Update countOddNumberBelowN.js

--- JavaScript/Codewars solutions/8kyu/countOddNumberBelowN.js
+++ JavaScript/Codewars solutions/8kyu/countOddNumberBelowN.js
@@ -7,3 +7,11 @@
 // 15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
 
 // Expect large Inputs!
+
+
+//My solution
+function oddCount(n){
+  return Math.floor(n/2)
+}
+
+//This kata was designed to work like a math problem. First instict was to use a for loop but got execution timeout. Tried arrays, same thing, then I settled for this math solution and that solved it
