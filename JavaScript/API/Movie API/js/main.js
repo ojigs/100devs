be42b06c5ded2ef54a4eaf43f497b2476e3c20c7
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-06-17 05:29:34 -0700

    Update main.js

--- JavaScript/API/Movie API/js/main.js
+++ JavaScript/API/Movie API/js/main.js
@@ -28,3 +28,45 @@ function getFetch(){
       });
 }
 
+//Handle response and error for all data response types
+function handleResponse(res) {
+  let contentType = res.headers.get('content-type')
+  if (contentType.includes('application/json')) {
+    return handleJSONResponse(res)
+  } else if (contentType.includes('text/html')) {
+    return handleTextResponse(res)
+  } else {
+    throw new Error (`Sorry, content-type ${contentType} is unknown`)
+  }
+}
+
+//Handle error for json response object. I have introduced this function so that if there is an error receiving the response, I can easily know from the .catch handler exactly what the error is. With this I can have a start on how to go about fixing it.
+function handleJSONResponse(res) {
+  return res.json()
+    .then(json => {
+      if (res.ok) {
+        return json
+      } else {
+        return Promise.reject({}, json, {
+          status: res.status,
+          statusText: res.statusText
+        })
+      }
+    })
+}
+
+//handle error for XML request
+function handleTextResponse(res) {
+  return res.text()
+    .then(text => {
+      if (res.ok) {
+        return text
+      } else {
+        return Promise.reject({}, text, {
+          status: res.status,
+          statusText: res.statusText,
+          err: text
+        })
+      }
+    })
+}
