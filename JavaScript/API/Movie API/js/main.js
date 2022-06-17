f584c7f8e5820fb357ef8e38e104a347acd33040
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-06-17 05:30:30 -0700

    Update main.js

--- JavaScript/API/Movie API/js/main.js
+++ JavaScript/API/Movie API/js/main.js
@@ -8,7 +8,7 @@ function getFetch(){
   const url = key + choice
 
   fetch(url)
-      .then(res => res.json()) // parse response as JSON
+      .then(handleResponse) // parse response as JSON
       .then(data => {
         console.log(data)
         document.querySelector('img').src = data.Poster;
