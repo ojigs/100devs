221c274f86a84cfd0c75aac40cb636f13dbd459a
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-07-01 20:57:59 +0100

    Update disemvowelTrolls.js

--- JavaScript/Codewars (restructured)/7kyu/disemvowelTrolls.js
+++ JavaScript/Codewars (restructured)/7kyu/disemvowelTrolls.js
@@ -13,3 +13,29 @@
 function disemvowel(str) {
   return str.replace(/[aeiou]/ig, '')
 }
+
+
+//other solutions
+const vowels = 'aeiou';
+
+function disemvowel(str) {
+  return str
+    .split('')
+    .filter(letter => !vowels.includes(letter.toLowerCase()))
+    .join('');
+}
+
+
+const disemvowel = (str) => {
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+  let newStr = '';
+  for (let i = 0; i <= str.length; i++) {
+    let char = str.charAt(i);
+    if (vowels.indexOf(char) == -1) {
+      newStr += char;
+    }
+  }
+    return newStr;
+    
+  
+};
