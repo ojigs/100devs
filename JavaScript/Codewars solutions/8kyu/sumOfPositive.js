4360d561c54a940fe0a156242638ad5ecbfa193b
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-07-03 23:14:22 +0100

    Update sumOfPositive.js

--- JavaScript/Codewars solutions/8kyu/sumOfPositive.js
+++ JavaScript/Codewars solutions/8kyu/sumOfPositive.js
@@ -7,3 +7,26 @@
 // Note: if there is nothing to sum, the sum is default to 0.
 // Arrays
 // Fundamentals
+
+
+//My solution
+function positiveSum(arr) {
+  return arr.filter(item => !(item < 0)).reduce((sum, curr) => sum + curr, 0)
+}
+
+
+//other solution
+function positiveSum(arr) {
+  var total = 0;    
+  for (i = 0; i < arr.length; i++) {    // setup loop to go through array of given length
+    if (arr[i] > 0) {                   // if arr[i] is greater than zero
+      total += arr[i];                  // add arr[i] to total
+    }
+  }
+  return total;                         // return total
+}
+
+
+function positiveSum(arr) {
+   return arr.reduce((a,b)=> a + (b > 0 ? b : 0),0);
+}
