c18696cef06f9d96779ed00c84d76ec33acfe3e3
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-07-22 17:43:44 +0100

    Update mumbling.js

--- JavaScript/Codewars (restructured)/7kyu/mumbling.js
+++ JavaScript/Codewars (restructured)/7kyu/mumbling.js
@@ -8,3 +8,18 @@
 // accum("cwAt") -> "C-Ww-Aaa-Tttt"
 
 // The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+
+//My solution
+function accum(s) {
+  return s.toLowerCase()
+          .split("")
+          .map((item,index)=>item.toUpperCase()+item.repeat(index))
+          .join("-")
+}
+
+
+//other solution
+function accum(s) {
+  return s.split('').map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-');
+}
