35eb71007ff4316bc9e92137d67c3dd602708814
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-07-22 17:09:03 +0100

    Create multiplyingTwoNumbers.js

---
+++ JavaScript/Codewars solutions/8kyu/multiplyingTwoNumbers.js
@@ -0,0 +1,15 @@
+// Description:
+
+// Implement a function which multiplies two numbers.
+
+
+//my solution
+function multiply(a, b) {
+  return a * b
+}
+
+
+function multiply(a, b){
+  if(typeof a == 'number' && typeof b == 'number')
+    return a * b;  
+}
