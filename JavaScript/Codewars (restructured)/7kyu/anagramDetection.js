1b2b4d7654a2a8b8071af538cff612c639584b0b
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-07-25 00:17:23 +0100

    Update anagramDetection.js

--- JavaScript/Codewars (restructured)/7kyu/anagramDetection.js
+++ JavaScript/Codewars (restructured)/7kyu/anagramDetection.js
@@ -11,3 +11,21 @@
 
 //     "Buckethead" is an anagram of "DeathCubeK"
 
+
+
+//My solution
+const isAnagram = function(test, original) {
+  test = test.toLowerCase().split('').sort().join('')
+  original = original.toLowerCase().split('').sort().join('')
+  return test == original
+};
+
+
+//othe solution
+String.prototype.sortLetters = function() {
+  return this.toLowerCase().split('').sort().join('');
+}
+
+var isAnagram = function(test, original) {
+  return test.sortLetters() == original.sortLetters();
+};
