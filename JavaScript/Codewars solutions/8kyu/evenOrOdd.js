4cc326e9bf823d7fbf3b9b7233d0f199780f0675
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-07-26 18:06:13 +0100

    Create evenOrOdd.js

---
+++ JavaScript/Codewars solutions/8kyu/evenOrOdd.js
@@ -0,0 +1,10 @@
+// Description:
+
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+// Mathematics
+// Fundamentals
+
+//My solution
+function even_or_odd(number) {
+  return number % 2 === 0 ? "Even" : "Odd"
+}
