dec2bff6a2255980301f2d134366ce03e4f0c0d5
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-07-26 18:01:46 +0100

    Update characterRecognitionSoftware.js

--- JavaScript/Codewars solutions/8kyu/characterRecognitionSoftware.js
+++ JavaScript/Codewars solutions/8kyu/characterRecognitionSoftware.js
@@ -11,3 +11,23 @@
 //     I is misinterpreted as 1
 
 // The test cases contain numbers only by mistake.
+
+
+//My solution
+function correct(string)
+{ return (
+  string.replace(/5/g, 'S').replace(/1/g, 'I').replace(/0/g, 'O')
+)
+}
+
+
+//other solution
+const corrections = {
+  '5': 'S',
+  '0': 'O',
+  '1': 'I',
+};
+
+const correct = string => (
+  string.replace(/[501]/g, character => corrections[character])
+);
