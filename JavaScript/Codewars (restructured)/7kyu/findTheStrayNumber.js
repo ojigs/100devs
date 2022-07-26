9839be7b029127c7f72da653da4b45bd0af63527
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-07-26 18:30:38 +0100

    Update findTheStrayNumber.js

--- JavaScript/Codewars (restructured)/7kyu/findTheStrayNumber.js
+++ JavaScript/Codewars (restructured)/7kyu/findTheStrayNumber.js
@@ -10,3 +10,16 @@
 // [1, 1, 2] ==> 2
 // [17, 17, 3, 17, 17, 17, 17] ==> 3
 
+
+//My solution
+function stray(numbers) {
+  return numbers.filter((e, i, a) => e !== a[i+1] && e !== a[i-1]).pop()
+}
+
+
+//other solution
+function stray(numbers){
+  for (var i in numbers){
+     if (numbers.indexOf(numbers[i]) === numbers.lastIndexOf(numbers[i])){return numbers[i]}
+  }
+}
