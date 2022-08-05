c1fa25664c98816ef7013d7acd317da91049327a
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-08-05 02:58:22 +0100

    Update larioAndMuigiPipeProblem.js

--- JavaScript/Codewars solutions/8kyu/larioAndMuigiPipeProblem.js
+++ JavaScript/Codewars solutions/8kyu/larioAndMuigiPipeProblem.js
@@ -12,3 +12,17 @@
 // Example
 
 // Input:  1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
+
+
+//My solution
+function pipeFix(numbers){
+  let arr = []
+  for (let i = numbers[0]; i <= numbers[numbers.length - 1]; i++) {
+    arr.push(i)
+  }
+  return arr
+}
+
+
+//other solution
+let pipeFix = nums => Array.from({ length: nums.pop() - nums[0] + 1 }, (v, i) => i + nums[0]);
