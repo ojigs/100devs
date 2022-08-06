29fb7d5cb7a65ad578edbee26fe60356791418f3
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-08-07 00:15:17 +0100

    Update sumOfASequence.js

--- JavaScript/Codewars (restructured)/7kyu/sumOfASequence.js
+++ JavaScript/Codewars (restructured)/7kyu/sumOfASequence.js
@@ -12,3 +12,33 @@
 // 2,6,2 --> 12 (2 + 4 + 6)
 // 1,5,1 --> 15 (1 + 2 + 3 + 4 + 5)
 // 1,5,3  --> 5 (1 + 4)
+
+
+//My solution
+const sequenceSum = (begin, end, step) => {
+  let seq = []
+  for (let i = begin; i <= end; i+=step) {
+    seq.push(i)
+  }
+  return seq.reduce((sum, current) => sum + current, 0)
+};
+
+
+//other solution
+const sequenceSum = (begin, end, step) => {
+  var sum = 0;
+  for(var i=begin;i<=end;i+=step)
+  {
+    sum += i;
+  }
+  return sum;
+};
+
+
+//Recursive solution
+const sequenceSum = (begin, end, step) => {
+  if (begin > end) {
+    return 0;
+  }
+  return begin + sequenceSum(begin + step, end, step);
+};
