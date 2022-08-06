17a522f5e8f282576161c5b0059a4be16d382354
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-08-07 00:56:08 +0100

    Create 5WithoutNumbers.js

---
+++ JavaScript/Codewars solutions/8kyu/5WithoutNumbers.js
@@ -0,0 +1,20 @@
+// NESCRIPTION
+
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// Good luck :)
+
+
+
+//My solution
+function unusualFive() {
+  return [undefined,undefined,undefined,undefined,undefined].length
+}
+
+
+//other solution
+function unusualFive() {
+  return 'fucku'.length
+}
