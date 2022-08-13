c465c1c5b3ed1884654d7c3784d1e58dde8b730f
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-08-13 15:24:35 +0100

    Update quarterOfTheYear.js

--- JavaScript/Codewars solutions/8kyu/quarterOfTheYear.js
+++ JavaScript/Codewars solutions/8kyu/quarterOfTheYear.js
@@ -3,3 +3,19 @@
 // Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
 
 // For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+
+
+//My solution
+const quarterOf = (month) => {
+  if (month < 4) {
+    return 1
+  } else if (month < 7) {
+    return 2
+  } else if (month < 10) {
+    return 3
+  } else return 4
+}
+
+
+//other solution
+const quarterOf = m => Math.ceil(m/3);
