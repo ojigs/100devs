ab3d0a4fedcddcb1ec1784372403d45bc28032f8
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-08-13 14:03:48 +0100

    Update beginnerSeries#3SumOfNumbers.js

--- JavaScript/Codewars (restructured)/7kyu/beginnerSeries#3SumOfNumbers.js
+++ JavaScript/Codewars (restructured)/7kyu/beginnerSeries#3SumOfNumbers.js
@@ -12,3 +12,21 @@
 // (-1, 0) --> -1 (-1 + 0 = -1)
 // (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
 
+
+// My solution
+function getSum( a,b ) {
+  let sum = 0
+  let arr = [a,b].sort((a,b)=>a-b)
+  for (let i=arr[0]; i <= arr[1]; i++) {
+    sum += i
+  }
+  return sum
+}
+
+
+//Other solution
+const GetSum = (a, b) => {
+  let min = Math.min(a, b),
+      max = Math.max(a, b);
+  return (max - min + 1) * (min + max) / 2;
+}
