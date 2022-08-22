8d41e43205863c946c2ed1078edee26cf5c9b5e3
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-08-22 03:03:32 +0100

    Update breakCamelCase.js

--- JavaScript/Codewars (restructured)/6kyu/breakCamelCase.js
+++ JavaScript/Codewars (restructured)/6kyu/breakCamelCase.js
@@ -7,3 +7,37 @@
 // "identifier"   =>  "identifier"
 // ""             =>  ""
 
+
+
+//My solution
+// complete the function
+function solution(string) {
+  //given a string
+  //return a modified string with camelCasing broken up
+  //'breakCamelCase' should return 'break Camel Case'
+  //test logic for camel case
+  //replace camel Case with a space
+  //return modified string
+  if (string.length > 0) {
+    return string.split('')
+      .map(e => e == e.toUpperCase() ? ` ${e}` : e)
+      .join('')
+  }
+  return ''
+}
+
+
+
+//other solution
+// complete the function
+function solution(string) {
+  return(string.replace(/([A-Z])/g, ' $1'));
+
+}
+
+
+
+// complete the function
+function solution(string) {
+  return string.replace(/([a-z])([A-Z])/g, "$1 $2");
+}
