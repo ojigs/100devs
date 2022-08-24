c10ce29b9102100a766409ab1fdf1262dceba3cf
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-08-24 22:41:20 +0100

    Update binToDecimal.js

--- JavaScript/Codewars solutions/8kyu/binToDecimal.js
+++ JavaScript/Codewars solutions/8kyu/binToDecimal.js
@@ -1,3 +1,9 @@
 // Description:
 
 // Complete the function which converts a binary number (given as a string) to a decimal number.
+
+
+//My solution
+function binToDec(bin){
+  return parseInt(bin, 2)
+}
