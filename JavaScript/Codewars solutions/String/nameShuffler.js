accab1cde367fd09fd061de263c8e4806641bed4
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-09-01 13:13:32 +0100

    Update nameShuffler.js

--- JavaScript/Codewars solutions/String/nameShuffler.js
+++ JavaScript/Codewars solutions/String/nameShuffler.js
@@ -6,3 +6,16 @@
 
 // "john McClane" --> "McClane john"
 
+
+
+//My solution (most of the time i was occupied with trying to come up with a solution that involved string methods only. Ended with an array structure
+function nameShuffler(str){
+  let tempStr = str.split(' ')
+  return tempStr[1] +" " + tempStr[0]
+}
+
+
+//other solution
+function nameSuffle(str){
+  return str.split(' ').reverse().join(' ')
+}
