ec4f23bac8869b9815967441356e16e315b332d8
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-09-03 23:18:04 +0100

    Update theCouponCode.js

--- JavaScript/Codewars solutions/String/theCouponCode.js
+++ JavaScript/Codewars solutions/String/theCouponCode.js
@@ -14,3 +14,20 @@
 // checkCoupon("123", "123", "July 9, 2015", "July 9, 2015")  ===  true
 // checkCoupon("123", "123", "July 9, 2015", "July 2, 2015")  ===  false
 
+
+
+//My solution
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+  
+  let d1 = new Date(currentDate)
+  let d2 = new Date(expirationDate)
+  
+  return (enteredCode === correctCode) && (d1 <= d2 )
+  
+}
+
+
+//other solution
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+  return enteredCode === correctCode && Date.parse(expirationDate) >= Date.parse(currentDate)
+}
