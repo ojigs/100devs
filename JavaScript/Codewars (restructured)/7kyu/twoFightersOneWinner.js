deb89c30bca8702116e10d3cf770c0a12baac986
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-09-04 00:36:02 +0100

    Update twoFightersOneWinner.js

--- JavaScript/Codewars (restructured)/7kyu/twoFightersOneWinner.js
+++ JavaScript/Codewars (restructured)/7kyu/twoFightersOneWinner.js
@@ -23,3 +23,36 @@
 //         this.damagePerAttack = damagePerAttack;
 //         this.toString = function() { return this.name; }
 // }
+
+
+
+//My solution
+function declareWinner(fighter1, fighter2, firstAttacker) {
+  let currentAttacker = firstAttacker
+  while (fighter1.health >= 0 && fighter2.health >=0) {
+    if (currentAttacker == fighter1.name) {
+      fighter2.health -= fighter1.damagePerAttack
+      currentAttacker = fighter2.name
+    } else {
+      fighter1.health -= fighter2.damagePerAttack
+      currentAttacker = fighter1.name
+    }
+  }
+  
+  return fighter1.health <= 0 ? fighter2.name : fighter1.name
+}
+
+
+
+//other solution
+function declareWinner(fighter1, fighter2, firstAttacker) {
+  var fac1 = Math.ceil( fighter1.health / fighter2.damagePerAttack );
+  var fac2 = Math.ceil( fighter2.health / fighter1.damagePerAttack );
+  if(fac1 < fac2) {
+    return fighter2.name;
+  } else if(fac2 < fac1) {
+    return fighter1.name;
+  } else {
+    return firstAttacker;
+  }
+}
