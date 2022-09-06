c552e366347f9b9d969c5f3685c8ed1c5a4bf41b
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-09-06 17:22:01 +0100

    Create removeAnchorFromURL.js

---
+++ JavaScript/Codewars (restructured)/7kyu/removeAnchorFromURL.js
@@ -0,0 +1,10 @@
+https://www.codewars.com/kata/51f2b4448cadf20ed0000386
+
+// Description:
+
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+// Examples
+
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
