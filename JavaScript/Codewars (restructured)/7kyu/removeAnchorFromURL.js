ee67f553f30159aade2d9e5680164e7672a0ca11
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-09-06 18:11:12 +0100

    Update removeAnchorFromURL.js

--- JavaScript/Codewars (restructured)/7kyu/removeAnchorFromURL.js
+++ JavaScript/Codewars (restructured)/7kyu/removeAnchorFromURL.js
@@ -8,3 +8,15 @@ https://www.codewars.com/kata/51f2b4448cadf20ed0000386
 // "www.codewars.com#about" --> "www.codewars.com"
 // "www.codewars.com?page=1" -->"www.codewars.com?page=1"
 
+
+//My solution
+function removeUrlAnchor(url){
+  return url.split('#')[0]
+}
+
+
+//other solution
+function removeUrlAnchor(url){
+  // TODO: complete
+  return url.replace(/#.*/gi,"");
+}
