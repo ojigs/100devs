d2c1abb51be9a6ec3b34d32ffbb3b214c3e999a0
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-09-07 15:47:41 +0100

    Update logicDrills.js

--- JavaScript/Codewars solutions/8kyu/logicDrills.js
+++ JavaScript/Codewars solutions/8kyu/logicDrills.js
@@ -7,3 +7,22 @@ https://www.codewars.com/kata/58649884a1659ed6cb000072/javascript
 // Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
 
 // For example, when the input is green, output should be yellow.
+
+
+//My solution
+function updateLight(current) {
+  const changeObj = {
+    red: 'green',
+    yellow: 'red',
+    green: 'yellow'
+  }
+  return changeObj[current]
+}
+
+
+//other solution
+function updateLight(current) {
+  
+  return current === 'yellow' ? 'red' : current === 'green' ? 'yellow' : 'green';
+
+}
