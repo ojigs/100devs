af63820dbdfed2748611049e8febeb865054cd04
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-09-07 15:06:39 +0100

    Update whatColorIsTheBrightest.js

--- JavaScript/Codewars (restructured)/7kyu/whatColorIsTheBrightest.js
+++ JavaScript/Codewars (restructured)/7kyu/whatColorIsTheBrightest.js
@@ -20,3 +20,35 @@ https://www.codewars.com/kata/62eb800ba29959001c07dfee
 // brightest(["#00FF00", "#FFFF00", "#01130F"]) == "#00FF00"
 
 // Note that both input and output should use upper case for characters A, B, C, D, E, F.
+
+
+// Forfeited
+function sB(s){
+  return Math.max(...[s.slice(1, 3), s.slice(3, 5), s.slice(5, 7)].map(x => parseInt(x, 16)))
+}
+
+function brightest(colors){
+  return colors.sort((a, b) => sB(b) - sB(a))[0]
+}
+
+
+//other solution
+function brightest(colors){
+  //Find brightness value of each color
+  //Sort based on brightness
+  //Return the first index, since it will be the brightest
+  const sortedColors = colors;
+  sortedColors.sort((a, b)=>{
+    let rValue = parseInt(a.slice(1, 3), 16);
+    let gValue = parseInt(a.slice(3, 5), 16);
+    let bValue = parseInt(a.slice(5), 16);
+    let brightness = Math.max(rValue, gValue, bValue);
+    
+    let rValue2 = parseInt(b.slice(1, 3), 16);
+    let gValue2 = parseInt(b.slice(3, 5), 16);
+    let bValue2 = parseInt(b.slice(5), 16);
+    let brightness2 = Math.max(rValue2, gValue2, bValue2);
+    return brightness2 - brightness;
+  });
+  return sortedColors[0];
+}
