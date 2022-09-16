799dfd861a34c264f481f899b8c3efdce212ce68
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-09-16 11:52:03 +0100

    Create orderedCountOfDocument.js

---
+++ JavaScript/Codewars (restructured)/7kyu/orderedCountOfDocument.js
@@ -0,0 +1,9 @@
+https://www.codewars.com/kata/57a6633153ba33189e000074
+
+// Description:
+
+// Count the number of occurrences of each character and return it as a list of tuples in order of appearance. For empty output return an empty list.
+
+// Example:
+
+// orderedCount("abracadabra") == [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]]
