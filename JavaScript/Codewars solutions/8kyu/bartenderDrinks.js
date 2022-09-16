a4495938ab41073343cef46f6864d6c3de03ab16
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-09-16 11:49:33 +0100

    Update bartenderDrinks.js

--- JavaScript/Codewars solutions/8kyu/bartenderDrinks.js
+++ JavaScript/Codewars solutions/8kyu/bartenderDrinks.js
@@ -15,3 +15,48 @@
 // Note: anything else is the default case: if the input to the function is not any of the values in the table, then the return value should be "Beer".
 
 // Make sure you cover the cases where certain words do not show up with correct capitalization. For example, the input "pOLitiCIaN" should still return "Your tax dollars".
+
+
+//My solution
+function getDrinkByProfession(param){
+  let obj = {
+    jabroni: "Patron Tequila",
+    "school counselor": "Anything with Alcohol",
+    programmer: "Hipster Craft Beer",
+    "bike gang member": "Moonshine",
+    politician: "Your tax dollars",
+    rapper: "Cristal"
+  }
+  return obj[param.toLowerCase()] || 'Beer'
+}
+
+
+//other solutions
+function getDrinkByProfession(param) {
+  param = param.toLowerCase();
+  
+  switch(param) {
+    case "jabroni": return "Patron Tequila";
+    case "school counselor": return "Anything with Alcohol";
+    case "programmer": return "Hipster Craft Beer";
+    case "bike gang member": return "Moonshine";
+    case "politician": return "Your tax dollars";
+    case "rapper": return "Cristal";
+    default: return "Beer";
+  }
+}
+
+
+function getDrinkByProfession(param){
+  let map = new Map([
+      ["jabroni", "Patron Tequila"],
+      ["school counselor", "Anything with Alcohol"],
+      ["programmer", "Hipster Craft Beer"],
+      ["bike gang member", "Moonshine"],
+      ["politician", "Your tax dollars"],
+      ["rapper", "Cristal"]
+      ]);
+      
+   let normal = param.toLowerCase();
+   return map.has(normal) ? map.get(normal) : "Beer";
+}
