d6ba98bafcd13f31a1dfe8b215ee26e7d8e2e822
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-09-26 11:53:59 +0100

    Create movingZerosToTheEnd.js

---
+++ JavaScript/Codewars (restructured)/5kyu/movingZerosToTheEnd.js
@@ -0,0 +1,6 @@
+https://www.codewars.com/kata/52597aa56021e91c93000cb0/train/javascript
+//Description
+
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+// moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,
