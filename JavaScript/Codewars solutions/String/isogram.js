de5a32b7293141286cdc83e8f934049967b9fbdf
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-09-29 12:53:29 +0100

    Update isogram.js

--- JavaScript/Codewars solutions/String/isogram.js
+++ JavaScript/Codewars solutions/String/isogram.js
@@ -6,3 +6,25 @@ https://www.codewars.com/kata/54ba84be607a92aa900000f1
 // Example: (Input --> Output)
 
 // "Dermatoglyphics" --> true "aba" --> false "moOse" --> false (ignore letter case)
+
+
+//My solution
+function isIsogram(str){
+  return str.toLowerCase().split('').every((e,i,a) => a.indexOf(e) === a.lastIndexOf(e))
+}
+
+
+//other solutionn
+function isIsogram(str){
+  return new Set(str.toUpperCase()).size == str.length;
+}
+
+
+function isIsogram(str){
+  return !str.match(/([a-z]).*\1/i);
+}
+
+
+function isIsogram(str){ 
+  return !/(\w).*\1/i.test(str)
+}
