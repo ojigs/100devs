5131c3f8a90d2dde2a57326561b82cecbecf9534
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-09-30 12:22:49 +0100

    Update reservedWords.js

--- JavaScript/Codewars solutions/String/reservedWords.js
+++ JavaScript/Codewars solutions/String/reservedWords.js
@@ -7,3 +7,20 @@ https://www.codewars.com/kata/51c8991dee245d7ddf00000e
 
 // "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
 
+
+//My solution
+function reverseWords(str){
+  return str.split(' ').reverse().join(' ')
+}
+
+
+//other solution
+function reverseWords(str){
+  var reverse = [];
+  var words = str.split(" ");
+  for(let i=words.length-1; i>=0; i--){
+    reverse.push(words[i]);
+    
+  }
+  return reverse.join(" ")
+}
