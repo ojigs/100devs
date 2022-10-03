8d53f82a8b66ffe1115527429bebce09c82ae3d0
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-10-03 12:20:47 +0100

    Create areaOfASquare.js

---
+++ JavaScript/Codewars solutions/8kyu/areaOfASquare.js
@@ -0,0 +1,10 @@
+https://www.codewars.com/kata/5748838ce2fab90b86001b1a/javascript
+// Description:
+
+// Complete the function that calculates the area of the red square, when the length of the circular arc A is given as the input. Return the result rounded to two decimals.
+
+// Graph
+
+// Note: use the π value provided in your language (Math::PI, M_PI, math.pi, etc)
+// Fundamentals
+// Mathematics
