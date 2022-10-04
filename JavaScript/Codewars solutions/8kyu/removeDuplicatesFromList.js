2c0c12d7858786b41ff9334df0d897ba3c18928a
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-10-04 12:02:42 +0100

    Update removeDuplicatesFromList.js

--- JavaScript/Codewars solutions/8kyu/removeDuplicatesFromList.js
+++ JavaScript/Codewars solutions/8kyu/removeDuplicatesFromList.js
@@ -4,3 +4,15 @@ https://www.codewars.com/kata/57a5b0dfcf1fa526bb000118/javascript
 // Define a function that removes duplicates from an array of numbers and returns it as a result.
 
 // The order of the sequence has to stay the same.
+
+
+//My solution
+function distinct(a) {
+  return a.filter((e,i,a) => a.indexOf(e) === i);
+}
+
+
+//other solution
+function distinct(a) {
+  return [...new Set(a)];
+}
