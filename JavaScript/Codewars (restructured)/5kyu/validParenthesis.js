7f72769fbcfd636e3967c0d17b3c745fdf3dcdf9
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-10-04 13:00:45 +0100

    Update validParenthesis.js

--- JavaScript/Codewars (restructured)/5kyu/validParenthesis.js
+++ JavaScript/Codewars (restructured)/5kyu/validParenthesis.js
@@ -12,3 +12,33 @@ https://www.codewars.com/kata/52774a314c2333f0a7000688/train/javascript
 // Constraints
 
 // 0 <= input.length <= 100
+
+
+//My solution
+function validParentheses(parens) {
+  const map = {')' : '('}
+  let stack = []
+  for (let i = 0; i < parens.length; i++) {
+    if (parens[i] === '(') {
+      stack.push(parens[i])
+    } else if (stack[stack.length - 1] === map[parens[i]]) {
+      stack.pop()
+    } else {
+      return false
+    }
+  }
+  return stack.length ? false : true
+}
+
+
+//other solution
+function validParentheses(parens){
+  var n = 0;
+  for (var i = 0; i < parens.length; i++) {
+    if (parens[i] == '(') n++;
+    if (parens[i] == ')') n--;
+    if (n < 0) return false;
+  }
+  
+  return n == 0;
+}
