0833b412529efc84fd2e864006db912a426867fa
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-10-06 13:01:10 +0100

    Update enumerableMagic#25.js

--- JavaScript/Codewars solutions/8kyu/enumerableMagic#25.js
+++ JavaScript/Codewars solutions/8kyu/enumerableMagic#25.js
@@ -4,3 +4,9 @@ https://www.codewars.com/kata/545afd0761aa4c3055001386/javascript
 // Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
 
 // If you need help, here's a reference:
+
+
+//My solution
+function take(arr, n) {
+  return arr.slice(0, n)
+}
