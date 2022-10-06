6057f3ee1cb2c8c69ec3855f8be36f2e7ca51403
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-10-06 12:57:18 +0100

    Update regexPasswordValidation.js

--- JavaScript/Codewars (restructured)/5kyu/regexPasswordValidation.js
+++ JavaScript/Codewars (restructured)/5kyu/regexPasswordValidation.js
@@ -11,3 +11,33 @@ https://www.codewars.com/kata/52e1476c8147a7547a000811/train/javascript
 
 
 //My solution
+// assign your RegExp to REGEXP
+const REGEXP = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{6,}$/;
+
+
+//other solution
+function validate(password) {
+  if(password.length <6) return false;
+  var lowerCase="abcdefghijklmnopqrstuvwxyz";
+  var upperCase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var number="0123456789";
+  var index = [-1,-1,-1];
+  for(i = 0; i< password.length; i++){
+    if(lowerCase.indexOf(password.charAt(i)) >= 0){
+      index[0] = lowerCase.indexOf(password.charAt(i));
+    }else {
+      if(upperCase.indexOf(password.charAt(i)) >= 0){
+        index[1] = upperCase.indexOf(password.charAt(i));
+      }
+      else{
+        if(number.indexOf(password.charAt(i)) >= 0){
+          index[2] = number.indexOf(password.charAt(i));
+        }else return false;
+      }
+    }
+  }
+  if(index[0] === -1 || index[1] === -1 || index[2] === -1){
+    return false;
+  }
+  return true;
+}
