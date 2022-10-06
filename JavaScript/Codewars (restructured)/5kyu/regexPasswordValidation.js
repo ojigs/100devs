aead7c40db6758531df8aff6e9b0046499b1afe0
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-10-06 12:52:49 +0100

    Create regexPasswordValidation.js

---
+++ JavaScript/Codewars (restructured)/5kyu/regexPasswordValidation.js
@@ -0,0 +1,13 @@
+https://www.codewars.com/kata/52e1476c8147a7547a000811/train/javascript
+//Description
+
+// You need to write regex that will validate a password to make sure it meets the following criteria:
+
+//     At least six characters long
+//     contains a lowercase letter
+//     contains an uppercase letter
+//     contains a digit
+//     only contains alphanumeric characters (note that '_' is not alphanumeric)
+
+
+//My solution
