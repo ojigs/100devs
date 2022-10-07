b9eaec73bb9fe648c0bf4907a8a05b857ebd88d6
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-10-07 13:25:02 +0100

    Create findThePosition.js

---
+++ JavaScript/Codewars solutions/8kyu/findThePosition.js
@@ -0,0 +1,8 @@
+
+https://www.codewars.com/kata/5808e2006b65bff35500008f/javascript// Description:
+
+// When provided with a letter, return its position in the alphabet.
+
+// Input :: "a"
+
+// Ouput :: "Position of alphabet: 1"
