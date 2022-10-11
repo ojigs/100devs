258d13d2bebacf34197162c4649b291fcd23aa68
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-10-11 15:45:05 +0100

    Update writeNumberInExpandedForm.js

--- JavaScript/Codewars (restructured)/6kyu/writeNumberInExpandedForm.js
+++ JavaScript/Codewars (restructured)/6kyu/writeNumberInExpandedForm.js
@@ -12,3 +12,21 @@ https://www.codewars.com/kata/5842df8ccbd22792a4000245/train/javascript
 // NOTE: All numbers will be whole numbers greater than 0.
 
 // If you liked this kata, check out part 2!!
+
+
+//My solution
+function expandedForm(num) {
+  num = num + ''
+  return num.split('').map((e, i, a) => e + '0'.repeat(a.length - i - 1))
+    .filter(e => !(e < 1)).join(' + ')
+}
+
+
+//other solution
+const expandedForm = n => n.toString()
+                            .split("")
+                            .reverse()
+                            .map( (a, i) => a * Math.pow(10, i))
+                            .filter(a => a > 0)
+                            .reverse()
+                            .join(" + ");
