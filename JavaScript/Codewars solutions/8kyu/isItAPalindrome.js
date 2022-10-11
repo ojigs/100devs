dced72cbde83812b1454a601aa5b0137283b4117
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-10-11 15:16:38 +0100

    Update isItAPalindrome.js

--- JavaScript/Codewars solutions/8kyu/isItAPalindrome.js
+++ JavaScript/Codewars solutions/8kyu/isItAPalindrome.js
@@ -2,3 +2,9 @@ https://www.codewars.com/kata/57a1fd2ce298a731b20006a4/javascript
 // Description:
 
 // Write a function that checks if a given string (case insensitive) is a palindrome.
+
+
+//My solution
+function isPalindrome(x) {
+  return x.toLowerCase() === x.toLowerCase().split('').reverse().join('')
+}
