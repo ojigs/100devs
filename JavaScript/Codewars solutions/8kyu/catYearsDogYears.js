eb05c50d7ca3c5c4bf3dd88d0efe76614e91a8b1
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-10-11 16:05:58 +0100

    Update and rename catYears.js to catYearsDogYears.js

--- JavaScript/Codewars solutions/8kyu/catYearsDogYears.js
+++ JavaScript/Codewars solutions/8kyu/catYearsDogYears.js
@@ -24,3 +24,32 @@ https://www.codewars.com/kata/5a6663e9fd56cb5ab800008b/javascript
 //     15 dog years for first year
 //     +9 dog years for second year
 //     +5 dog years for each year after that
+
+
+
+//My solution
+var humanYearsCatYearsDogYears = function(humanYears) {
+  let dogYears = 0, catYears = 0;
+  for (let i = 1; i <= humanYears; i++) {
+    if (i ==  1) {
+      dogYears += 15
+      catYears += 15
+    } else if (i == 2) {
+      dogYears += 9
+      catYears += 9
+    } else {
+      dogYears += 5
+      catYears += 4
+    }
+  }
+  return [humanYears, catYears, dogYears]
+}
+
+
+
+//other solution
+var humanYearsCatYearsDogYears = function(y) {
+  if (y == 1) return [1, 15, 15]
+  if (y == 2) return [2, 24, 24]
+  return [y, (y-2) * 4 + 24, (y-2) * 5 + 24]
+}
