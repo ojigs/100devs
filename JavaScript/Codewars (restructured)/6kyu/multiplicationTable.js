78acc1f8ca142793133b9de4d42386a6e8fda062
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-10-12 12:19:56 +0100

    Update multiplicationTable.js

--- JavaScript/Codewars (restructured)/6kyu/multiplicationTable.js
+++ JavaScript/Codewars (restructured)/6kyu/multiplicationTable.js
@@ -10,3 +10,29 @@ https://www.codewars.com/kata/534d2f5b5371ecf8d2000a08/train/javascript
 // 3 6 9
 
 // for given example, the return value should be: [[1,2,3],[2,4,6],[3,6,9]]
+
+
+//My solution
+multiplicationTable = function(size) {
+  return Array.from({length: size})
+              .map((e, i) => [...Array(size)]
+                             .map((el, ind) => (ind + 1) * (i+1)))
+}
+
+
+
+//other solution
+multiplicationTable = function(size) {
+  var result = [];
+
+  for (var i = 0; i < size; i++) {
+    result[i] = [];
+    for(var j = 0; j < size; j++) {
+      result[i][j] = (i + 1) * (j + 1);
+    }
+  }
+  
+  return result
+}
+
+
