f9de55cefbb84b20d3307a531a7dfda7de32b98c
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-10-13 12:26:54 +0100

    Update drinkAbout.js

--- JavaScript/Codewars solutions/8kyu/drinkAbout.js
+++ JavaScript/Codewars solutions/8kyu/drinkAbout.js
@@ -23,3 +23,24 @@ https://www.codewars.com/kata/56170e844da7c6f647000063/javascript
 // 20 --> "drink beer"
 // 30 --> "drink whisky"
 
+
+//My solution
+function peopleWithAgeDrink(old) {
+  //function to return drink type
+  //we get a number, parameter not dtring, 
+  //return toddy || coke || beer || whisky
+  //13 --> drink toddy
+  //21 --> drink whisky
+  // use conditionals, if/else statements, switch-case, 
+  if (old < 14) return 'drink toddy'
+  else if (old < 18) return 'drink coke'
+  else if (old < 21) return 'drink beer'
+  else return 'drink whisky'
+};
+
+
+// other solution
+const peopleWithAgeDrink = (age) =>
+  age < 14 ? "drink toddy" :
+  age < 18 ? "drink coke" :
+  age < 21 ? "drink beer" : "drink whisky"
