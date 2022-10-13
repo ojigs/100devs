a901bdb5fe912769bc2a85480b12160173a06197
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-10-13 14:45:55 +0100

    Update dataReverse.js

--- JavaScript/Codewars (restructured)/6kyu/dataReverse.js
+++ JavaScript/Codewars (restructured)/6kyu/dataReverse.js
@@ -20,3 +20,28 @@ https://www.codewars.com/kata/569d488d61b812a0f7000015/train/javascript
 // [1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
 
 // Note: In the C and NASM languages you are given the third parameter which is the number of segment blocks.
+
+
+//My solution
+function dataReverse(data) {
+  //function reverse data
+  //given array of data; data in multiples of 8; data always binary, array
+  //to return bytes with their order reversed
+  //[1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0] --> [0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1]
+  //break data into equal parts of 8; slice along 8 digits, reverse, join
+  let cache = []
+  for (let i = 0; i < data.length; i+=8) {
+    cache.push(data.slice(i, i+8))
+  }
+  return cache.reverse().flatMap(e => e)
+}
+
+
+//other solution
+const dataReverse = data => {
+  const bytes = [];
+  for (let i = 0; i < data.length; i += 8) {
+    bytes.unshift(...data.slice(i, i + 8));
+  }
+  return bytes;
+};
