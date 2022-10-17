3efd44d84089f03eb3055482c855ed17843e67f1
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-10-17 23:50:58 +0100

    Update howManyStairs.js

--- JavaScript/Codewars solutions/8kyu/howManyStairs.js
+++ JavaScript/Codewars solutions/8kyu/howManyStairs.js
@@ -1,3 +1,4 @@
+https://www.codewars.com/kata/56fc55cd1f5a93d68a001d4e/javascript
 // Description:
 
 // Suzuki is a monk who climbs a large staircase to the monastery as part of a ritual. Some days he climbs more stairs than others depending on the number of students he must train in the morning. He is curious how many stairs might be climbed over the next 20 years and has spent a year marking down his daily progress.
