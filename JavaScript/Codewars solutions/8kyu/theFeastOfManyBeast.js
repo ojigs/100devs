87e9d085b7647ef86878e5f640f8a19fcfeac799
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-10-17 23:45:12 +0100

    Update theFeastOfManyBeast.js

--- JavaScript/Codewars solutions/8kyu/theFeastOfManyBeast.js
+++ JavaScript/Codewars solutions/8kyu/theFeastOfManyBeast.js
@@ -1,3 +1,4 @@
+https://www.codewars.com/kata/5aa736a455f906981800360d/javascript
 // Description:
 
 // All of the animals are having a feast! Each animal is bringing one dish. There is just one rule: the dish must start and end with the same letters as the animal's name. For example, the great blue heron is bringing garlic naan and the chickadee is bringing chocolate cake.
@@ -5,3 +6,15 @@
 // Write a function feast that takes the animal's name and dish as arguments and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
 
 // Assume that beast and dish are always lowercase strings, and that each has at least two letters. beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string. They will not contain numerals.
+
+
+//My solution
+function feast(beast, dish) {
+  return beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1]
+}
+
+
+//other solution
+function feast(beast, dish) {
+  return beast[0]===dish[0] && beast.slice(-1)===dish.slice(-1);
+}
