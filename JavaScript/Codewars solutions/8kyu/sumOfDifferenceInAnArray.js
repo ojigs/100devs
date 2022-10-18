e65cdc91e24a0b87cc2e54029c6d96dcfa0355f8
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-10-18 12:21:16 +0100

    Update sumOfDifferenceInAnArray.js

--- JavaScript/Codewars solutions/8kyu/sumOfDifferenceInAnArray.js
+++ JavaScript/Codewars solutions/8kyu/sumOfDifferenceInAnArray.js
@@ -13,3 +13,24 @@ https://www.codewars.com/kata/5b73fe9fb3d9776fbf00009e/javascript
 // If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell, None in Rust).
 // Arrays
 // Fundamentals
+
+
+//MY SOLUTION
+function sumOfDifferences(arr) {
+  if (arr.length < 2) return 0
+  arr = arr.sort((a,b) => b - a).map((e,i,a) => e - a[i + 1])
+  arr.pop()
+  return arr.reduce((a,c) => a + c)
+  }
+
+
+//other solution
+function sumOfDifferences(arr) {
+    return arr.length > 1 ? Math.max(...arr) - Math.min(...arr) : 0;
+}
+
+
+const sumOfDifferences = arr => arr
+  .sort((a, b) => b - a)
+  .map((a, i) => a - arr[i + 1] || 0)
+  .reduce((a, b) => a + b, 0);
