5afd1cfb5eabbe63acfa35b29d67a89932f5d254
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-10-19 13:43:42 +0100

    Create regularBallSuperBall.js

---
+++ JavaScript/Codewars solutions/8kyu/regularBallSuperBall.js
@@ -0,0 +1,12 @@
+https://www.codewars.com/kata/53f0f358b9cb376eca001079/javascript
+// Description:
+
+// Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+
+// If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+// ball1 = new Ball();
+// ball2 = new Ball("super");
+
+// ball1.ballType     //=> "regular"
+// ball2.ballType     //=> "super"
