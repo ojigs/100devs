cc70867ea29d54d92d439b689ec8549b22b48887
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-10-23 21:22:46 +0100

    Update allUnique.js

--- JavaScript/Codewars (restructured)/7kyu/allUnique.js
+++ JavaScript/Codewars (restructured)/7kyu/allUnique.js
@@ -4,3 +4,13 @@ https://www.codewars.com/kata/553e8b195b853c6db4000048/train/javascript
 // Write a program to determine if a string contains only unique characters. Return true if it does and false otherwise.
 
 // The string may contain any of the 128 ASCII characters. Characters are case-sensitive, e.g. 'a' and 'A' are considered different characters.
+
+
+//My solution
+function hasUniqueChars(str){
+  return str.split('').every((e,i,a) => a.indexOf(e) === i)
+}
+
+
+//other solution
+let hasUniqueChars = (str) => new Set(str).size === str.length;
