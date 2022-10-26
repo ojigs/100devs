b131771585ca6ea12e02f28fa0930a0d21c39533
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-10-26 16:22:15 +0100

    Update nthSmallestElement4.js

--- JavaScript/Codewars (restructured)/7kyu/nthSmallestElement4.js
+++ JavaScript/Codewars (restructured)/7kyu/nthSmallestElement4.js
@@ -1,3 +1,4 @@
+https://www.codewars.com/kata/5a512f6a80eba857280000fc/train/javascript
 // Desxription
 
 // Introduction and warm-up (highly recommended): Playing With Lists/Arrays Series
@@ -21,3 +22,10 @@
 // Playing With Lists/Arrays Series
 // More Enjoyable Katas
 // Enjoy Learning !!
+
+
+
+//My solution
+function nthSmallest(arr, pos){
+  return arr.sort((a,b) => a - b)[pos - 1]
+}
