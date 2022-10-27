9d72258db5a9c379d3a3343771c8dcccd3fd5377
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-10-27 13:00:31 +0100

    Update factorial.js

--- JavaScript/Codewars (restructured)/7kyu/factorial.js
+++ JavaScript/Codewars (restructured)/7kyu/factorial.js
@@ -8,3 +8,35 @@ https://www.codewars.com/kata/54ff0d1f355cfd20e60001fc/javascript
 // More details about factorial can be found here.
 // Algorithms
 // Fundamentals
+
+
+
+//My solution
+function factorial(n) {
+  if (n < 0 || n > 12) throw new RangeError()
+  if (n === 0) return 1
+  return n * factorial(n-1)
+}
+
+
+function factorial(n) {
+  if (n < 0 || n > 12) throw new RangeError()
+  if (cache[n]) {
+    return cache[n]
+  }
+  let result = n * factorial(n - 1)
+  cache[n] = result
+  return result
+}
+
+let cache = { '0': 1}
+
+
+//other solution
+function factorial(n){
+
+ if (n < 0 || n > 12) {
+    throw new RangeError('Parameter must be between ' + 0 + ' and ' + 12);
+  }
+return n<2 ? 1 : n*factorial(n-1);
+}
