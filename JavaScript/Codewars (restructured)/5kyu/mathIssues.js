57c6cfa5ae939f554a3ce5e8e013d9870fe5f4c6
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-11-01 13:49:56 +0100

    Update mathIssues.js

--- JavaScript/Codewars (restructured)/5kyu/mathIssues.js
+++ JavaScript/Codewars (restructured)/5kyu/mathIssues.js
@@ -9,3 +9,39 @@ https://www.codewars.com/kata/5267faf57526ea542e0007fb/train/javascript
 //     Math.ceil()
 //     Math.floor()
 
+
+//My solution
+Math.round = function(number) {
+  let min = number.toString().split('.')
+  if (min[1] && min[1][0] >= 5) {
+    min[0] = +min[0] + 1
+  }
+  return +min[0]
+};
+
+Math.ceil = function(number) {
+  if (Number.isInteger(number)) return number
+  let max = number.toString().split('.')
+  return +max[0] + 1
+};
+
+Math.floor = function(number) {
+  if (Number.isInteger(number)) return number
+  let min = number.toString().split('.')
+  return +min[0]
+};
+
+
+
+//other solution
+Math.round = function(number) {
+  return (number - parseInt(number) >= 0.5) ? parseInt(number) + 1 : parseInt(number) ;
+};
+
+Math.ceil = function(number) {
+  return (parseInt(number) === number) ? number : parseInt(number) + 1;
+};
+
+Math.floor = function(number) {
+  return parseInt(number);
+};
