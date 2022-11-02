14fb3b385c4782e3f34049a059f0030cd9e62574
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-11-02 13:34:22 +0100

    Update maximumSubarraySum.js

--- JavaScript/Codewars (restructured)/5kyu/maximumSubarraySum.js
+++ JavaScript/Codewars (restructured)/5kyu/maximumSubarraySum.js
@@ -9,3 +9,42 @@ https://www.codewars.com/kata/54521e9ec8e60bc4de000d6c/train/javascript
 // Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array. If the list is made up of only negative numbers, return 0 instead.
 
 // Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
+
+
+
+//My solution
+const maxSequence = function(arr){
+  let max = [0]
+  arr.reduce((a,c) => {
+    if (a + c > 0) {
+      max.push(a + c)
+      return a + c
+    } else {
+      return 0
+    }
+  }, 0)
+  return Math.max(...max)
+}
+
+
+
+//other solution
+var maxSequence = function(arr){
+  var min = 0, ans = 0, i, sum = 0;
+  for (i = 0; i < arr.length; ++i) {
+    sum += arr[i];
+    min = Math.min(sum, min);
+    ans = Math.max(ans, sum - min);
+  }
+  return ans;
+}
+
+
+
+var maxSequence = function(arr){
+    var currentSum = 0;
+    return arr.reduce(function(maxSum, number){
+        currentSum = Math.max(currentSum+number, 0);
+        return Math.max(currentSum, maxSum);
+    }, 0);
+}
