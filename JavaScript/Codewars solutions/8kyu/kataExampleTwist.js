878e1c9ef9e822857bed0e4aab6c2da945dc16e0
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-11-06 23:46:53 +0100

    Update kataExampleTwist.js

--- JavaScript/Codewars solutions/8kyu/kataExampleTwist.js
+++ JavaScript/Codewars solutions/8kyu/kataExampleTwist.js
@@ -4,3 +4,12 @@ https://www.codewars.com/kata/525c1a07bb6dda6944000031/javascript
 // This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
 
 // Add the value "codewars" to the array websites/Websites 1,000 times.
+
+
+
+//My solution
+// add the value "codewars" to the websites array 1,000 times
+var websites = []
+for (let i = 0; i < 1000; i++) {
+  websites.push('codewars')
+}
