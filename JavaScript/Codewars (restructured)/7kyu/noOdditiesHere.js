440a02acb6b7aa073a68ea6f9c279daba010ea38
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-11-06 23:54:11 +0100

    Update noOdditiesHere.js

--- JavaScript/Codewars (restructured)/7kyu/noOdditiesHere.js
+++ JavaScript/Codewars (restructured)/7kyu/noOdditiesHere.js
@@ -4,3 +4,10 @@ https://www.codewars.com/kata/51fd6bc82bc150b28e0000ce/javascript
 // Write a small function that returns the values of an array that are not odd.
 
 // All values in the array will be integers. Return the good values in the order they are given.
+
+
+
+//My solution
+function noOdds( values ){
+  return values.filter(e => !(e%2))
+}
