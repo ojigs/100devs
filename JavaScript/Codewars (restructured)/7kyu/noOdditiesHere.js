d0610aa8a2210f14e54a5340a4299b2c3449e741
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-11-06 23:50:35 +0100

    Create noOdditiesHere.js

---
+++ JavaScript/Codewars (restructured)/7kyu/noOdditiesHere.js
@@ -0,0 +1,6 @@
+https://www.codewars.com/kata/51fd6bc82bc150b28e0000ce/javascript
+// Description:
+
+// Write a small function that returns the values of an array that are not odd.
+
+// All values in the array will be integers. Return the good values in the order they are given.
