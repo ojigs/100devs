2fc872f9082047cff5c809e26e5922b6d7164d33
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-11-11 10:19:23 +0100

    Create validateCodeWithSimpleRegex.js

---
+++ JavaScript/Codewars solutions/8kyu/validateCodeWithSimpleRegex.js
@@ -0,0 +1,6 @@
+https://www.codewars.com/kata/56a25ba95df27b7743000016/javascript
+// Description:
+
+// Basic regex tasks. Write a function that takes in a numeric code of any length. The function should check if the code begins with 1, 2, or 3 and return true if so. Return false otherwise.
+
+// You can assume the input will always be a number.
