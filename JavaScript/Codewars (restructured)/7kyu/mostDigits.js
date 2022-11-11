59f9c7cdc261a39cc53362a24acfb064036d9ed2
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-11-11 11:06:08 +0100

    Create mostDigits.js

---
+++ JavaScript/Codewars (restructured)/7kyu/mostDigits.js
@@ -0,0 +1,6 @@
+https://www.codewars.com/kata/58daa7617332e59593000006/javascript
+// Description:
+
+// Find the number with the most digits.
+
+// If two numbers in the argument array have the same number of digits, return the first one in the array.
