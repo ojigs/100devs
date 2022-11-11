55dc6b83573cd1d6bcf4cb30cf8b53fec29c350d
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-11-11 10:18:29 +0100

    Update grasshopperCombineStrings.js

--- JavaScript/Codewars solutions/8kyu/grasshopperCombineStrings.js
+++ JavaScript/Codewars solutions/8kyu/grasshopperCombineStrings.js
@@ -11,3 +11,14 @@ https://www.codewars.com/kata/55f73f66d160f1f1db000059/javascript
 // returns:
 
 // 'James Stevens'
+
+
+
+// My solution
+function combineNames (a, b) {
+  return `${a} ${b}`
+}
+
+
+// other solution
+const combineNames = (...names) => names.join(' ');
