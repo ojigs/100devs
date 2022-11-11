13fa5eaa0ed9f76af4b46fe2e19dfab3eaefaf5b
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-11-11 11:48:39 +0100

    Update greetMe.js

--- JavaScript/Codewars (restructured)/7kyu/greetMe.js
+++ JavaScript/Codewars (restructured)/7kyu/greetMe.js
@@ -8,3 +8,15 @@ https://www.codewars.com/kata/535474308bb336c9980006f2/javascript
 // "riley" --> "Hello Riley!"
 // "JACK"  --> "Hello Jack!"
 
+
+
+//My solution
+var greet = function(name) {
+  return `Hello ` + name[0].toUpperCase() + name.toLowerCase().substring(1) + '!'
+};
+
+
+// other solution
+var greet = function(name) {
+  return "Hello " + name.charAt(0).toUpperCase() + name.slice(1).toLowerCase() + "!";
+};
