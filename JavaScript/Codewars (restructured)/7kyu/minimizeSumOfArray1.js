8c553518a45336d792d92eab2eca4785f010d00e
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-11-11 10:29:51 +0100

    Create minimizeSumOfArray1.js

---
+++ JavaScript/Codewars (restructured)/7kyu/minimizeSumOfArray1.js
@@ -0,0 +1,31 @@
+https://www.codewars.com/kata/5a523566b3bfa84c2e00010b/javascript
+// Description:
+// Introduction and Warm-up (Highly recommended)
+// Playing With Lists/Arrays Series
+// Task
+
+// Given an array of integers , Find the minimum sum which is obtained from summing each Two integers product .
+// Notes
+
+//     Array/list will contain positives only .
+//     Array/list will always have even size
+
+// Input >> Output Examples
+
+// minSum({5,4,2,3}) ==> return (22) 
+
+// Explanation:
+
+//     The minimum sum obtained from summing each two integers product ,  5*2 + 3*4 = 22
+
+// minSum({12,6,10,26,3,24}) ==> return (342)
+
+// Explanation:
+
+//     The minimum sum obtained from summing each two integers product ,  26*3 + 24*6 + 12*10 = 342
+
+// minSum({9,2,8,7,5,4,0,6}) ==> return (74)
+
+// Explanation:
+
+//     The minimum sum obtained from summing each two integers product ,  9*0 + 8*2 +7*4 +6*5 = 74
