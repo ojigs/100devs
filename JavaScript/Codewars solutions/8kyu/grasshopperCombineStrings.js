7f302c7a5cd242cbd57eef5c3978e3b6893019fd
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-11-11 10:14:27 +0100

    Create grasshopperCombineStrings.js

---
+++ JavaScript/Codewars solutions/8kyu/grasshopperCombineStrings.js
@@ -0,0 +1,13 @@
+https://www.codewars.com/kata/55f73f66d160f1f1db000059/javascript
+// Description:
+// Combine strings function
+
+// Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+// Example:
+
+// combineNames('James', 'Stevens')
+
+// returns:
+
+// 'James Stevens'
