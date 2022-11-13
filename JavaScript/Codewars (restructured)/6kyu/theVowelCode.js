7dc156939770d8b8ff0d258a52e589ec70bdd849
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-11-13 23:47:07 +0100

    Update theVowelCode.js

--- JavaScript/Codewars (restructured)/6kyu/theVowelCode.js
+++ JavaScript/Codewars (restructured)/6kyu/theVowelCode.js
@@ -16,3 +16,38 @@ https://www.codewars.com/kata/53697be005f803751e0015aa/
 // For example, decode("h3 th2r2") would return "hi there".
 
 // For the sake of simplicity, you can assume that any numbers passed into the function will correspond to vowels.
+
+
+
+// My solution
+function encode(string) {
+  const vowels = {a: 1, e: 2, i: 3, o: 4, u: 5}
+  for (let i = 0; i < string.length; i++) {
+    if (vowels[string[i]]) {
+      string = string.replace(string[i], vowels[string[i]])
+    }
+  }
+  return string
+}
+
+function decode(string) {
+  const vowels = {1: 'a', 2: 'e', 3: 'i', 4: 'o', 5: 'u'}
+  for (let i = 0; i < string.length; i++) {
+    if (vowels[string[i]]) {
+      string = string.replace(string[i], vowels[string[i]])
+    }
+  }
+  return string
+}
+
+
+//other solution
+// turn vowels into numbers
+function encode(string){
+  return string.replace(/[aeiou]/g, function (x) { return '_aeiou'.indexOf(x) });
+}
+
+//turn numbers back into vowels
+function decode(string){
+  return string.replace(/[1-5]/g, function (x) { return '_aeiou'.charAt(x) });
+}
