3b890349fe51b62d0726d76a5b91fc7846439a8d
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-11-13 02:06:00 +0100

    Create findTheCapitals.js

---
+++ JavaScript/Codewars (restructured)/7kyu/findTheCapitals.js
@@ -0,0 +1,8 @@
+https://www.codewars.com/kata/539ee3b6757843632d00026b/javascript
+// Description:
+// Instructions
+
+// Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+// Example
+
+// Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
