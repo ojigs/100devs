344dffbb90b1dce3743881fdb7b44cbea7a7e559
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-11-22 23:51:55 +0100

    Create removeConsecutiveDuplicateWords.js

---
+++ JavaScript/Codewars (restructured)/7kyu/removeConsecutiveDuplicateWords.js
@@ -0,0 +1,12 @@
+https://www.codewars.com/kata/5b39e91ee7a2c103300018b3/train/javascript
+// Description
+
+// Your task is to remove all consecutive duplicate words from a string, leaving only first words entries. For example:
+
+// "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"
+
+// --> "alpha beta gamma delta alpha beta gamma delta"
+
+// Strings
+// Regular Expressions
+// Algorithms
