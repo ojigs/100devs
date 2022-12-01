40b6aba58d6cce8f10a618fc205d9d38c11ea172
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-01 12:40:00 +0100

    Update and rename findTheDivisor.js to findTheDivisors.js

--- JavaScript/Codewars (restructured)/7kyu/findTheDivisors.js
+++ JavaScript/Codewars (restructured)/7kyu/findTheDivisors.js
@@ -7,3 +7,23 @@ https://www.codewars.com/kata/544aed4c4a30184e960010f4
 // divisors(12); // should return [2,3,4,6]
 // divisors(25); // should return [5]
 // divisors(13); // should return "13 is prime"
+
+
+// My solution
+function divisors(integer) {
+  const arr = []
+  for (let i = 2; i < integer; i++) {
+    if (!(integer % i)) {
+      arr.push(i)
+    }
+  }
+  return arr.length ? arr : `${integer} is prime`
+};
+
+
+
+// other solutions
+function divisors(integer) {
+  var divisors = [...Array(integer).keys()].slice(2, integer).filter(div => integer % div == 0);
+  return divisors.length > 0 ? divisors : integer + ' is prime';
+};
