1337b971f7e8e36f9517335e5e929abc69f3f252
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-02 12:22:02 +0100

    Update beginnerReduceButGrow.js

--- JavaScript/Codewars solutions/8kyu/beginnerReduceButGrow.js
+++ JavaScript/Codewars solutions/8kyu/beginnerReduceButGrow.js
@@ -5,3 +5,18 @@ https://www.codewars.com/kata/57f780909f7e8e3183000078/javascript
 
 // [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
 
+
+// My solution
+function grow(x){
+  return x.reduce((a,c) => a*c)
+}
+
+
+// other solution
+const grow = x => {
+  let res = 1;
+  for (let i = 0; i < x.length; i++) {
+    res *= x[i];
+  }
+  return res;
+};
