5997aebd0cb48f7a9880dbcb81392df5bb10133e
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-03 17:18:36 +0100

    Update getNumberFromString.js

--- JavaScript/Codewars solutions/8kyu/getNumberFromString.js
+++ JavaScript/Codewars solutions/8kyu/getNumberFromString.js
@@ -6,3 +6,16 @@ https://www.codewars.com/kata/57a37f3cbb99449513000cd8/javascript
 // Function:
 
 // getNumberFromString(s)
+
+
+
+// My solution
+function getNumberFromString(s) {
+  return +s.match(/\d/g).join('')
+}
+
+
+// other solution
+function getNumberFromString(s) {
+  return +s.replace(/\D/g, "");
+}
