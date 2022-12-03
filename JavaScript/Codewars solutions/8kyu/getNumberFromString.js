3459e23b51076bd438a413e4d7523800bda744e2
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-03 16:46:40 +0100

    Create getNumberFromString.js

---
+++ JavaScript/Codewars solutions/8kyu/getNumberFromString.js
@@ -0,0 +1,8 @@
+https://www.codewars.com/kata/57a37f3cbb99449513000cd8/javascript
+// Description:
+
+// Write a function which removes from string all non-digit characters and parse the remaining to number. E.g: "hell5o wor6ld" -> 56
+
+// Function:
+
+// getNumberFromString(s)
