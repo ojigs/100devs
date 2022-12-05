dacd9534699f68f3aac1231e6480e8e3e5ef522f
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-05 12:27:07 +0100

    Update and rename surfaceAreaAndVolumeOfABoxx.js to surfaceAreaAndVolumeOfABox.js

--- JavaScript/Codewars solutions/8kyu/surfaceAreaAndVolumeOfABox.js
+++ JavaScript/Codewars solutions/8kyu/surfaceAreaAndVolumeOfABox.js
@@ -2,3 +2,15 @@ https://www.codewars.com/kata/565f5825379664a26b00007c/javascript
 // Description:
 
 // Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+
+
+
+// My solution
+function getSize(width, height, depth) {
+  const area = 2*(width*height + width*depth + depth*height)
+  const volume = width*height*depth
+  return [area, volume]
+}
+
+
+
