06f3ffecb1be366c8d938933d8182cbaaecdef65
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-05 12:19:06 +0100

    Create surfaceAreaAndVolumeOfABoxx.js

---
+++ JavaScript/Codewars solutions/8kyu/surfaceAreaAndVolumeOfABoxx.js
@@ -0,0 +1,4 @@
+https://www.codewars.com/kata/565f5825379664a26b00007c/javascript
+// Description:
+
+// Write a function that returns the total surface area and volume of a box as an array: [area, volume]
