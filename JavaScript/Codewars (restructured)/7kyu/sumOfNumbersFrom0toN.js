69810ee42805710b990a542d7f0bacd7cdd9b2c4
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-07 11:33:33 +0100

    Create sumOfNumbersFrom0toN.js

---
+++ JavaScript/Codewars (restructured)/7kyu/sumOfNumbersFrom0toN.js
@@ -0,0 +1,33 @@
+https://www.codewars.com/kata/56e9e4f516bcaa8d4f001763/javascript
+// Description:
+
+// Description:
+
+// We want to generate a function that computes the series starting from 0 and ending until the given number.
+// Example:
+
+// Input:
+
+// > 6
+
+// Output:
+
+//     0+1+2+3+4+5+6 = 21
+
+// Input:
+
+// > -15
+
+// Output:
+
+//     -15<0
+
+// Input:
+
+// > 0
+
+// Output:
+
+//     0=0
+
+// Fundamentals
