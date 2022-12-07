31ec7362d18149d100ba234e3a6faee0844c90a5
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-07 12:17:21 +0100

    Update sumOfNumbersFrom0toN.js

--- JavaScript/Codewars (restructured)/7kyu/sumOfNumbersFrom0toN.js
+++ JavaScript/Codewars (restructured)/7kyu/sumOfNumbersFrom0toN.js
@@ -31,3 +31,34 @@ https://www.codewars.com/kata/56e9e4f516bcaa8d4f001763/javascript
 //     0=0
 
 // Fundamentals
+
+
+// My solution
+var SequenceSum = (function() {
+  let arr = []
+  function SequenceSum() {
+    console.log(arr.length)
+  }
+
+  SequenceSum.showSequence = function(count) {
+    for (let i = 0; i <= count; i++) {
+      arr.push(i)
+    }
+    return arr
+  };
+
+  return SequenceSum;
+
+})();
+
+
+
+
+// other solution
+class SequenceSum {
+  static showSequence(n) {
+    return n < 0
+      ? `${n}<0`
+      : `${Array.from({length: n+1}, (v, k) => k).join('+')}${n?' = ':'='}${n*(n+1)/2}`;
+  }
+}
