2f656108710b2357ae1e38ed91e0e69a36eea467
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-08 11:40:07 +0100

    Create grasshopperTerminalGameCombatFunction.js

---
+++ JavaScript/Codewars solutions/8kyu/grasshopperTerminalGameCombatFunction.js
@@ -0,0 +1,4 @@
+https://www.codewars.com/kata/586c1cf4b98de0399300001d/javascript
+// Description:
+
+// Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
