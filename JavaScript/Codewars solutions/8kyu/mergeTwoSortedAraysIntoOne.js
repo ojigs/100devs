545b1bc54683345639a03964c3c14c3870ca4564
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-08 12:55:43 +0100

    Update mergeTwoSortedAraysIntoOne.js

--- JavaScript/Codewars solutions/8kyu/mergeTwoSortedAraysIntoOne.js
+++ JavaScript/Codewars solutions/8kyu/mergeTwoSortedAraysIntoOne.js
@@ -17,3 +17,13 @@ https://www.codewars.com/kata/5899642f6e1b25935d000161/javascript
 // Happy coding!
 // Fundamentals
 // Arrays
+
+
+
+// My solution
+function mergeArrays(arr1, arr2) {
+  return Array.from(new Set(arr1.concat(arr2).sort((a,b) => (a-b))));
+}
+
+
+// other soluyion
