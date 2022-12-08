6f7465f18f9eeb86f73a8403b0ce3f52a7b1aee7
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-08 11:46:55 +0100

    Update grasshopperTerminalGameCombatFunction.js

--- JavaScript/Codewars solutions/8kyu/grasshopperTerminalGameCombatFunction.js
+++ JavaScript/Codewars solutions/8kyu/grasshopperTerminalGameCombatFunction.js
@@ -2,3 +2,14 @@ https://www.codewars.com/kata/586c1cf4b98de0399300001d/javascript
 // Description:
 
 // Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
+
+
+// My solution
+function combat(health, damage) {
+  return health - damage >= 0 ? health - damage : 0
+    0
+}
+
+
+// other solution
+const combat = (health, damage) => Math.max(0, health - damage);
