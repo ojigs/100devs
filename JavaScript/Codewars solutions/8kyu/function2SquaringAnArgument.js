613f476a4c1fe3565062c36161289ec1fd8e3a21
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-09 13:38:20 +0100

    Update function2SquaringAnArgument.js

--- JavaScript/Codewars solutions/8kyu/function2SquaringAnArgument.js
+++ JavaScript/Codewars solutions/8kyu/function2SquaringAnArgument.js
@@ -2,3 +2,9 @@ https://www.codewars.com/kata/523b623152af8a30c6000027/javascript
 // Description:
 
 // Now you have to write a function that takes an argument and returns the square of it.
+
+
+// My solution
+function square(x) {
+  return x**2
+}
