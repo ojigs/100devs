ef6d75ccd4ed71a843b75e7c499b4b129514c1a6
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-09 13:40:41 +0100

    Create sumOfOddCubedNumbers.js

---
+++ JavaScript/Codewars (restructured)/7kyu/sumOfOddCubedNumbers.js
@@ -0,0 +1,6 @@
+https://www.codewars.com/kata/580dda86c40fa6c45f00028a/javascript
+// Description:
+
+// Find the sum of the odd numbers within an array, after cubing the initial integers. The function should return undefined if any of the values aren't numbers.
+// Fundamentals
+// Functional Pr
