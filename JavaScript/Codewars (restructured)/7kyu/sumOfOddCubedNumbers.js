e32a6bb7f143cbdf0d8a2e0ac5c4f9d50851924e
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-09 14:11:03 +0100

    Update sumOfOddCubedNumbers.js

--- JavaScript/Codewars (restructured)/7kyu/sumOfOddCubedNumbers.js
+++ JavaScript/Codewars (restructured)/7kyu/sumOfOddCubedNumbers.js
@@ -4,3 +4,20 @@ https://www.codewars.com/kata/580dda86c40fa6c45f00028a/javascript
 // Find the sum of the odd numbers within an array, after cubing the initial integers. The function should return undefined if any of the values aren't numbers.
 // Fundamentals
 // Functional Pr
+
+
+
+// My solution
+function cubeOdd(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (!Number.isInteger(arr[i])) return undefined
+  }
+  return arr.reduce((a, c) => a + (c % 2 ? c**3 : 0), 0) 
+}
+
+
+// other solution
+let cubeOdd = a => {
+  var isNumeric = a.every(x=>!isNaN(x))
+  return isNumeric ? a.filter(n=>n%2).reduce((s,n)=>s+(n*n*n),0) : undefined
+}
