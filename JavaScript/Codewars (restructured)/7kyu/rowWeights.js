35f68f2fb10cdd5e21060f1ca1855fbecfd4331c
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-12 11:41:15 +0100

    Create rowWeights.js

---
+++ JavaScript/Codewars (restructured)/7kyu/rowWeights.js
@@ -0,0 +1,6 @@
+https://www.codewars.com/kata/5abd66a5ccfd1130b30000a9/javascript
+// Description:
+// Scenario
+
+// Several people are standing in a row divided into two teams.
+// The first person goes into team 1, the second goes into team 2, the third goes into team 1, and so on.
