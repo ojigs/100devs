cde7226b32e714cf69a24ffa73e0d05ec9258997
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-13 12:37:51 +0100

    Update sumOfCubes.js

--- JavaScript/Codewars (restructured)/7kyu/sumOfCubes.js
+++ JavaScript/Codewars (restructured)/7kyu/sumOfCubes.js
@@ -10,3 +10,14 @@ https://www.codewars.com/kata/59a8570b570190d313000037/javascript
 // 2 --> 9 (sum of the cubes of 1 and 2 is 1 + 8)
 // 3 --> 36 (sum of the cubes of 1, 2, and 3 is 1 + 8 + 27)
 
+
+// My solution
+function sumCubes(n){
+  return Array.from({length: n}, (e,i) => i + 1).reduce((a,c) => a + c**3)
+}
+
+
+// other solution
+function sumCubes(n) {
+  return (n * (n + 1) / 2) ** 2;
+}
