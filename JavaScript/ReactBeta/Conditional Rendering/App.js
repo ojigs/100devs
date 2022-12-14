f26afb35ce43606b068b177ba70ffc269912b254
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2022-12-14 17:31:09 +0100

    create conditional rendering in JSX

---
+++ JavaScript/ReactBeta/Conditional Rendering/App.js
@@ -0,0 +1,33 @@
+// To-do
+// Use the conditional operator (cond ? a : b) to render a ❌ if isPacked isn’t true.
+
+function Item({ name, isPacked }) {
+    return (
+        <li className="item">
+        {name} {isPacked ? '✔' : '❌'}
+        </li>
+    );
+  }
+  
+  export default function PackingList() {
+    return (
+      <section>
+        <h1>Sally Ride's Packing List</h1>
+        <ul>
+          <Item 
+            isPacked={true} 
+            name="Space suit" 
+          />
+          <Item 
+            isPacked={true} 
+            name="Helmet with a golden leaf" 
+          />
+          <Item 
+            isPacked={false} 
+            name="Photo of Tam" 
+          />
+        </ul>
+      </section>
+    );
+  }
+  
