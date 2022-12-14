15a999d7e3bbe646fe9b13b1f59d4f6bc464390a
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-14 11:41:33 +0100

    Update divideAndConquer.js

--- JavaScript/Codewars (restructured)/7kyu/divideAndConquer.js
+++ JavaScript/Codewars (restructured)/7kyu/divideAndConquer.js
@@ -4,3 +4,26 @@ https://www.codewars.com/kata/57eaec5608fed543d6000021/javascript
 // Given a mixed array of number and string representations of integers, add up the non-string integers and subtract this from the total of the string integers.
 
 // Return as a number.
+
+
+
+// My solution
+function divCon(x){
+  return x.reduce((a,c) => typeof c === 'string' ? a - (+c) : a + c, 0)
+}
+
+
+// other solution
+function divCon(x){
+  let numValue = 0;
+  let strValue = 0;
+  
+  for(let i = 0; i < x.length; i++) {
+    if(typeof(x[i]) === 'string' ) {
+      strValue += parseFloat(x[i]);
+    } else if(typeof(x[i]) === 'number' ) {
+      numValue += x[i];
+    }
+  }
+  return numValue - strValue;
+}
