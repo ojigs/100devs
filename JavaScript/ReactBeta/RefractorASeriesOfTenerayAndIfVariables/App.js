76500c17e06e7dce0efa3f445c1c442a3f52e334
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2022-12-14 18:03:09 +0100

    updated App.js

--- JavaScript/ReactBeta/RefractorASeriesOfTenerayAndIfVariables/App.js
+++ JavaScript/ReactBeta/RefractorASeriesOfTenerayAndIfVariables/App.js
@@ -32,3 +32,45 @@ export default function DrinkList() {
     </div>
   );
 }
+
+
+
+// optimized solution for future updates
+const drinks = {
+    tea: {
+      part: 'leaf',
+      caffeine: '15–70 mg/cup',
+      age: '4,000+ years'
+    },
+    coffee: {
+      part: 'bean',
+      caffeine: '80–185 mg/cup',
+      age: '1,000+ years'
+    }
+  };
+  
+  function Drink({ name }) {
+    const info = drinks[name];
+    return (
+      <section>
+        <h1>{name}</h1>
+        <dl>
+          <dt>Part of plant</dt>
+          <dd>{info.part}</dd>
+          <dt>Caffeine content</dt>
+          <dd>{info.caffeine}</dd>
+          <dt>Age</dt>
+          <dd>{info.age}</dd>
+        </dl>
+      </section>
+    );
+  }
+  
+  export default function DrinkList() {
+    return (
+      <div>
+        <Drink name="tea" />
+        <Drink name="coffee" />
+      </div>
+    );
+  }
