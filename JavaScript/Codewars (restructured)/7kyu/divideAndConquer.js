8d06f3d297507f6a0bd3f0aafefc8af5030a2d73
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-14 11:32:56 +0100

    Create divideAndConquer.js

---
+++ JavaScript/Codewars (restructured)/7kyu/divideAndConquer.js
@@ -0,0 +1,6 @@
+https://www.codewars.com/kata/57eaec5608fed543d6000021/javascript
+// Description:
+
+// Given a mixed array of number and string representations of integers, add up the non-string integers and subtract this from the total of the string integers.
+
+// Return as a number.
