645cb06ae6493f665dc5022e8293671117363226
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-15 15:19:18 +0100

    Update and rename alphabetWars.js to alphabetWar.js

--- JavaScript/Codewars (restructured)/7kyu/alphabetWar.js
+++ JavaScript/Codewars (restructured)/7kyu/alphabetWar.js
@@ -37,3 +37,31 @@ https://www.codewars.com/kata/59377c53e66267c8f6000027/train/javascript
 // Alphabet wars - reinforces massacre
 // Alphabet wars - nuclear strike
 // Alphabet war - Wo lo loooooo priests join the war
+
+
+// My solution
+function alphabetWar(fight) {
+   let left = { 'w': 4, 'p': 3, 'b': 2, 's': 1}
+   let right = { 'm': 4, 'q': 3, 'd': 2, 'z': 1}
+   let war = 0
+   for (let i = 0; i < fight.length; i++) {
+     if (left[fight[i]]) {
+       war += left[fight[i]]
+     } else if (right[fight[i]]) {
+       war -= right[fight[i]]
+     } else {
+       war += 0
+     }
+   }
+  
+  return war > 0 ? 'Left side wins!' : war < 0 ? 'Right side wins!' : "Let's fight again!"
+
+}
+
+
+// other solution
+function alphabetWar(fight) {
+    let map = { w: -4, p: -3, b: -2, s: -1, m: 4, q: 3, d: 2, z: 1 };
+    let result = fight.split('').reduce((a, b) => a + (map[b] || 0), 0);
+    return result ? (result < 0 ? "Left" : "Right") + " side wins!" : "Let's fight again!";
+}
