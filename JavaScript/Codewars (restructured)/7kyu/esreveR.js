fcace34f98d3c56a4e7c216df0b358eb15378182
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-15 15:23:08 +0100

    Create esreveR.js

---
+++ JavaScript/Codewars (restructured)/7kyu/esreveR.js
@@ -0,0 +1,6 @@
+https://www.codewars.com/kata/5413759479ba273f8100003d/javascript
+// Description:
+
+// Write a function reverse which reverses a list (or in clojure's case, any list-like data structure)
+
+// (the dedicated builtin(s) functionalities are deactivated)
