ff2115e86b58c66740dc8c84c4464c8110dbe860
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-15 14:44:58 +0100

    Create sumMixedArray.js

---
+++ JavaScript/Codewars solutions/8kyu/sumMixedArray.js
@@ -0,0 +1,12 @@
+https://www.codewars.com/kata/57eaeb9578748ff92a000009/javascript
+// Description:
+
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+// Fundamentals
+// Strings
+// Arrays
+
+
+// 
