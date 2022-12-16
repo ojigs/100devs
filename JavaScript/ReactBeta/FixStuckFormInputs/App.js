3b1bbd585475aa9516291388e6cb60c3bae13421
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2022-12-16 18:52:42 +0100

    updated App.js

--- JavaScript/ReactBeta/FixStuckFormInputs/App.js
+++ JavaScript/ReactBeta/FixStuckFormInputs/App.js
@@ -1,3 +1,4 @@
+// To-do
 // When you type into the input fields, nothing appears. It’s like the input values are “stuck” with empty strings. The value of the first <input> is set to always match the firstName variable, and the value for the second <input> is set to always match the lastName variable. This is correct. Both inputs have onChange event handlers, which try to update the variables based on the latest user input (e.target.value). However, the variables don’t seem to “remember” their values between re-renders. Fix this by using state variables instead.
 
 // export default function Form() {
