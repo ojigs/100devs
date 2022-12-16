a1e5643f69539904e1a1083fb3fa946de9614f25
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-16 12:31:58 +0100

    Create highestRankNumberInAnArray.js

---
+++ JavaScript/Codewars (restructured)/6kyu/highestRankNumberInAnArray.js
@@ -0,0 +1,14 @@
+https://www.codewars.com/kata/5420fc9bb5b2c7fd57000004/train/javascript
+// Description
+
+// Complete the method which returns the number which is most frequent in the given input array. If there is a tie for most frequent number, return the largest number among them.
+
+// Note: no empty arrays will be given.
+// Examples
+
+// [12, 10, 8, 12, 7, 6, 4, 10, 12]              -->  12
+// [12, 10, 8, 12, 7, 6, 4, 10, 12, 10]          -->  12
+// [12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]  -->   3
+
+// Arrays
+// Fundamentals
