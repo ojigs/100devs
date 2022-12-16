5a5679bef78e83d4a2cd3e4bc622f58c34ad0f86
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-16 12:37:20 +0100

    Update highestRankNumberInAnArray.js

--- JavaScript/Codewars (restructured)/6kyu/highestRankNumberInAnArray.js
+++ JavaScript/Codewars (restructured)/6kyu/highestRankNumberInAnArray.js
@@ -12,3 +12,40 @@ https://www.codewars.com/kata/5420fc9bb5b2c7fd57000004/train/javascript
 
 // Arrays
 // Fundamentals
+
+
+
+// My solution
+function highestRank(arr){
+  let high, freq = 0, obj = {}
+  for (let i = 0; i < arr.length; i++) {
+    if (obj[arr[i]]) {
+      obj[arr[i]] += 1
+    } else {
+      obj[arr[i]] = 1
+    }
+  }
+  
+  Object.entries(obj).forEach(e => {
+    if (freq <= e[1]) {
+      freq = e[1]
+      high = +e[0]
+    }
+  })
+  
+  return high
+}
+
+
+// other solution
+function highestRank(arr){
+  var high,max=0,rec={}
+  for(var x of arr){
+    rec[x]=(rec[x]||0)+1
+    if(rec[x]>max){
+      max=rec[x]
+      high=x
+    }
+  }
+  return high
+}
