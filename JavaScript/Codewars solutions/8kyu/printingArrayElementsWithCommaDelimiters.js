2820f96512eafa101c3007f2d6ed82066509b31a
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2022-12-19 13:54:35 +0100

    Create printingArrayElementsWithCommaDelimiters.js

---
+++ JavaScript/Codewars solutions/8kyu/printingArrayElementsWithCommaDelimiters.js
@@ -0,0 +1,14 @@
+https://www.codewars.com/kata/56e2f59fb2ed128081001328/javascript
+// Description:
+
+// Input: Array of elements
+
+// ["h","o","l","a"]
+
+// Output: String with comma delimited elements of the array in th same order.
+
+// "h,o,l,a"
+
+// Note: if this seems too simple for you try the next level
+// Arrays
+// Fundamentals
