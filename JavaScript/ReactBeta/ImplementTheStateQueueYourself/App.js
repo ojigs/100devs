60585979fbf51e59daed7d3c683e3e540dca9a54
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2022-12-20 17:11:12 +0100

    Update implement the state queue yourself; implementing how react handles state changes behind the scene

--- JavaScript/ReactBeta/ImplementTheStateQueueYourself/App.js
+++ JavaScript/ReactBeta/ImplementTheStateQueueYourself/App.js
@@ -1,16 +1,49 @@
-// To-do
-// In this challenge, you will reimplement a tiny part of React from scratch! It’s not as hard as it sounds.
+import { getFinalState } from "./processQueue.js";
 
-// Scroll through the sandbox preview. Notice that it shows four test cases. They correspond to the examples you’ve seen earlier on this page. Your task is to implement the getFinalState function so that it returns the correct result for each of those cases. If you implement it correctly, all four tests should pass.
+function increment(n) {
+  return n + 1;
+}
+increment.toString = () => "n => n+1";
 
-// You will receive two arguments: baseState is the initial state (like 0), and the queue is an array which contains a mix of numbers (like 5) and updater functions (like n => n + 1) in the order they were added.
+export default function App() {
+  return (
+    <>
+      <TestCase baseState={0} queue={[1, 1, 1]} expected={1} />
+      <hr />
+      <TestCase
+        baseState={0}
+        queue={[increment, increment, increment]}
+        expected={3}
+      />
+      <hr />
+      <TestCase baseState={0} queue={[5, increment]} expected={6} />
+      <hr />
+      <TestCase baseState={0} queue={[5, increment, 42]} expected={42} />
+    </>
+  );
+}
 
-// Your task is to return the final state, just like the tables on this page show!
-
-// export function getFinalState(baseState, queue) {
-//     let finalState = baseState;
-
-//     // TODO: do something with the queue...
-
-//     return finalState;
-//   }
+function TestCase({ baseState, queue, expected }) {
+  const actual = getFinalState(baseState, queue);
+  return (
+    <>
+      <p>
+        Base state: <b>{baseState}</b>
+      </p>
+      <p>
+        Queue: <b>[{queue.join(", ")}]</b>
+      </p>
+      <p>
+        Expected result: <b>{expected}</b>
+      </p>
+      <p
+        style={{
+          color: actual === expected ? "green" : "red",
+        }}
+      >
+        Your result: <b>{actual}</b> (
+        {actual === expected ? "correct" : "wrong"})
+      </p>
+    </>
+  );
+}
---
+++ JavaScript/ReactBeta/ImplementTheStateQueueYourself/processQueue.js.js
@@ -0,0 +1,16 @@
+// To-do
+// In this challenge, you will reimplement a tiny part of React from scratch! It’s not as hard as it sounds.
+
+// Scroll through the sandbox preview. Notice that it shows four test cases. They correspond to the examples you’ve seen earlier on this page. Your task is to implement the getFinalState function so that it returns the correct result for each of those cases. If you implement it correctly, all four tests should pass.
+
+// You will receive two arguments: baseState is the initial state (like 0), and the queue is an array which contains a mix of numbers (like 5) and updater functions (like n => n + 1) in the order they were added.
+
+// Your task is to return the final state, just like the tables on this page show!
+
+// export function getFinalState(baseState, queue) {
+//     let finalState = baseState;
+
+//     // TODO: do something with the queue...
+
+//     return finalState;
+//   }
