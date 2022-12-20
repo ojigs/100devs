f973b917d6d1b31c9c420e0ba90688e7d155a1ff
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2022-12-20 18:36:24 +0100

    fixing incorrect state updates; implementing immutability of objects in react

--- JavaScript/ReactBeta/FixIncorrectStateUpdates/App.js
+++ JavaScript/ReactBeta/FixIncorrectStateUpdates/App.js
@@ -46,3 +46,50 @@
 //     </>
 //   );
 // }
+
+import { useState } from "react";
+
+export default function Scoreboard() {
+  const [player, setPlayer] = useState({
+    firstName: "Ranjani",
+    lastName: "Shettar",
+    score: 10,
+  });
+
+  function handlePlusClick() {
+    setPlayer({ ...player, score: player.score + 1 });
+  }
+
+  function handleFirstNameChange(e) {
+    setPlayer({
+      ...player,
+      firstName: e.target.value,
+    });
+  }
+
+  function handleLastNameChange(e) {
+    setPlayer({
+      ...player,
+      lastName: e.target.value,
+    });
+  }
+
+  return (
+    <>
+      <label>
+        Score: <b>{player.score}</b>{" "}
+        <button onClick={handlePlusClick}>+1</button>
+      </label>
+      <label>
+        First name:
+        <input value={player.firstName} onChange={handleFirstNameChange} />
+      </label>
+      <label>
+        Last name:
+        <input value={player.lastName} onChange={handleLastNameChange} />
+      </label>
+    </>
+  );
+}
+
+// the bugged code was mutating the player object, so react didn't know that there was a need for re-render. So when the firstName was updated, react triggered a render which took the new score and updated it on the screen. For the lastname, the score gets lost in the UI because the player object was not copied when setting the new state object
