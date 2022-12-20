6aa3ed42190fa19e4b4f8532d65e4d1151cc26d4
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2022-12-20 19:11:55 +0100

    updating an object with immer library

--- JavaScript/ReactBeta/UpdateAnObjectWithImmer/App.js
+++ JavaScript/ReactBeta/UpdateAnObjectWithImmer/App.js
@@ -43,3 +43,50 @@
 //     </>
 //   );
 // }
+
+import { useState } from "react";
+import { useImmer } from "use-immer";
+import Background from "./Background.js";
+import Box from "./Box.js";
+
+const initialPosition = {
+  x: 0,
+  y: 0,
+};
+
+export default function Canvas() {
+  const [shape, setShape] = useImmer({
+    color: "orange",
+    position: initialPosition,
+  });
+
+  function handleMove(dx, dy) {
+    setShape((draft) => {
+      draft.position.x += dx;
+      draft.position.y += dy;
+    });
+  }
+
+  function handleColorChange(e) {
+    setShape({
+      ...shape,
+      color: e.target.value,
+    });
+  }
+
+  return (
+    <>
+      <select value={shape.color} onChange={handleColorChange}>
+        <option value="orange">orange</option>
+        <option value="lightpink">lightpink</option>
+        <option value="aliceblue">aliceblue</option>
+      </select>
+      <Background position={initialPosition} />
+      <Box color={shape.color} position={shape.position} onMove={handleMove}>
+        Drag me!
+      </Box>
+    </>
+  );
+}
+
+// immer does the update for us. And while although it looks like the object is being mutated, it actually is not
