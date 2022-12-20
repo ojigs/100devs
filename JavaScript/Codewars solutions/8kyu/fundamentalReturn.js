2074abff07b17d67f007172daa22b7329df87de6
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-20 12:38:52 +0100

    Update fundamentalReturn.js

--- JavaScript/Codewars solutions/8kyu/fundamentalReturn.js
+++ JavaScript/Codewars solutions/8kyu/fundamentalReturn.js
@@ -19,3 +19,30 @@ https://www.codewars.com/kata/55a5befdf16499bffb00007b/javascript
 
 // Note: All math operations will be: a (operation) b
 // Fundamentals
+
+
+
+// My solution
+function add(a,b){
+    return a + b
+}
+
+function divide(a,b){
+    return a / b
+}
+
+function multiply(a,b){
+    return a * b
+}
+
+function mod(a,b){
+    return a % b
+}
+   
+function exponent(a,b){
+    return a**b
+}
+    
+function subt(a,b){
+    return a - b
+}
