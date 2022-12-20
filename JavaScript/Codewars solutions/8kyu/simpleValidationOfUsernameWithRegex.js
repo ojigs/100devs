98d5186067fcfeedf8d7ead05ea1322e599245f0
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-20 14:49:24 +0100

    Update simpleValidationOfUsernameWithRegex.js

--- JavaScript/Codewars solutions/8kyu/simpleValidationOfUsernameWithRegex.js
+++ JavaScript/Codewars solutions/8kyu/simpleValidationOfUsernameWithRegex.js
@@ -8,3 +8,28 @@ https://www.codewars.com/kata/56a3f08aa9a6cc9b75000023/train/javascript
 //     underscore
 
 // Length should be between 4 and 16 characters (both included).
+
+
+
+// My solution
+function validateUsr(username) {
+  res =  /^[a-z0-9_]{4,16}$/.test(username) 
+  return res
+}
+
+
+// other solution
+function validateUsr(username) {
+  /**
+    - `^`        Start from the beginning of the string.
+    - `[]`       Allow any character specified, including...
+    - `a-z`      anything from a to z,
+    - `0-9`      anything from 0 to 9,
+    - `_`        and underscore.
+    - `{4,16}`   Accept 4 to 16 of allowed characters, both numbers included.
+    - `$`        End the string right after specified amount of allowed characters is given.
+  */
+  const validator = /^[a-z0-9_]{4,16}$/;
+  
+  return validator.test(username);
+}
