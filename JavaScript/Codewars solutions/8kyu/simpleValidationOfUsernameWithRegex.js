5c964cb45b90a344b5e2944b4483242c64163a29
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-20 14:46:36 +0100

    Create simpleValidationOfUsernameWithRegex.js

---
+++ JavaScript/Codewars solutions/8kyu/simpleValidationOfUsernameWithRegex.js
@@ -0,0 +1,10 @@
+https://www.codewars.com/kata/56a3f08aa9a6cc9b75000023/train/javascript
+// Description
+
+// Write a simple regex to validate a username. Allowed characters are:
+
+//     lowercase letters,
+//     numbers,
+//     underscore
+
+// Length should be between 4 and 16 characters (both included).
