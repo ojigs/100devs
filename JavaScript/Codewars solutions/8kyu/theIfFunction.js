8e190016359056e8a75ce65bd8ba77787a7e6eb9
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-21 14:34:27 +0100

    Update theIfFunction.js

--- JavaScript/Codewars solutions/8kyu/theIfFunction.js
+++ JavaScript/Codewars solutions/8kyu/theIfFunction.js
@@ -8,3 +8,54 @@ https://www.codewars.com/kata/54147087d5c2ebe4f1000805/javascript
 
 // _if(true, function(){console.log("True")}, function(){console.log("false")})
 // // Logs 'True' to the console.
+
+
+// My solution
+function _if(bool, func1, func2) {
+  return bool ? func1() : func2()
+}
+
+
+// other solution
+                                                            _if=
+           (                      ㅤif,                   ㅤtrue,                         ㅤfalse                         )=>
+                                                          Function
+              
+                                                                                         .call. call. call.
+                                                                                       call.call.call.call.call.
+                                                                                   call.call.call. call.call.call .
+                                call.call.                                       call.call.call.         call.call .
+                             call.call.call                                    .call.call.call             .call.call.
+                             call.call.call                                    .call .call.                   call.
+                             call.call.call                                    .call.call.
+                                call.call                                      .call.call
+                                                                               .call.call
+                                                                               .call.call
+                                                                               .call.call
+                                                                               .call.call
+                                                                               .call.call
+               .call.call.call .call.call                      .call.call.call .call.call. call.call.call.call
+               .call.call.call .call.call                      .call.call.call .call.call. call.call.call.call
+               .call.call.call .call.call                      .call.call.call .call.call. call.call.call.call
+                               .call.call                                      .call.call
+                               .call.call                                      .call.call
+                               .call.call                                      .call.call
+                               .call.call                                      .call.call
+                               .call.call                                      .call.call
+                               .call.call                                      .call.call
+                               .call.call                                      .call.call
+                               .call.call                                      .call.call
+                               .call.call                                      .call.call
+                               .call.call                                      .call.call
+                               .call.call                                      .call.call
+                               .call.call                                      .call.call
+                               .call.call                                      .call.call
+                               .call.call                                      .call.call
+                               .call.call                                      .call.call
+                               .call.call                                      .call.call
+                               .call.call                                      .call.call
+              .call. call.call .call.call .call.call. call                     .call.call
+              .call. call.call .call.call .call.call. call                     .call.call
+              .call. call.call .call.call .call.call. call                     .call.call
+              
+           (                      ㅤif?                   ㅤtrue:                         ㅤfalse                          );
