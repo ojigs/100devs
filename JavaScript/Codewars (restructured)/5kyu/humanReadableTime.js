289aa4ae35c78decc7f03d1f900d21cc8f2ca91d
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-22 14:34:15 +0100

    Update humanReadableTime.js

--- JavaScript/Codewars (restructured)/5kyu/humanReadableTime.js
+++ JavaScript/Codewars (restructured)/5kyu/humanReadableTime.js
@@ -10,3 +10,30 @@ https://www.codewars.com/kata/52685f7382004e774f0001f7/train/javascript
 // The maximum time never exceeds 359999 (99:59:59)
 
 // You can find some examples in the test fixtures.
+
+
+// My solution
+function humanReadable (seconds) {
+  let hh = (Math.floor((seconds % 360000) / 3600)).toString().padStart(2,0)
+  let mm = (Math.floor(((seconds % 360000) % 3600) / 60)).toString().padStart(2,0)
+  let ss = ((((seconds % 360000) % 3600) % 60)).toString().padStart(2,0)
+  
+  return `${hh}:${mm}:${ss}`
+}
+
+
+// other solution
+function humanReadable(seconds) {
+  var pad = function(x) { return (x < 10) ? "0"+x : x; }
+  return pad(parseInt(seconds / (60*60))) + ":" +
+         pad(parseInt(seconds / 60 % 60)) + ":" +
+         pad(seconds % 60)
+}
+
+
+function humanReadable(seconds) {
+  return [seconds / 3600, seconds % 3600 / 60, seconds % 60].map(function(v) {
+    v = Math.floor(v).toString();
+    return v.length == 1 ? '0' + v : v;
+  }).join(':');
+}
