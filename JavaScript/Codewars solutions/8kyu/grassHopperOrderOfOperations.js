32beca244ca468601115a7d0ab46c565e54725e0
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-23 14:01:12 +0100

    Update grassHopperOrderOfOperations.js

--- JavaScript/Codewars solutions/8kyu/grassHopperOrderOfOperations.js
+++ JavaScript/Codewars solutions/8kyu/grassHopperOrderOfOperations.js
@@ -4,3 +4,15 @@ https://www.codewars.com/kata/560ecf0cb040de130e00007d/javascript
 
 // You are running the calculation 2 + 2 * 2 + 2 * 2 and expect to get the answer 32 but instead the function keeps returning 10. Fix the function to make it return 32 without changing the number or the operators.
 // Fundamentals
+
+
+// Given
+function orderOperations () {
+  return 2 + 2 * 2 + 2 * 2
+}
+
+
+// My solution
+function orderOperations () {
+  return (2 + 2) * ((2 + 2) * 2)
+}
