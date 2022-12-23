c22a0f42b83af145adb110754e1bacc60632e92a
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-23 14:27:09 +0100

    Update stopGninnipsMySdrow.js

--- JavaScript/Codewars (restructured)/6kyu/stopGninnipsMySdrow.js
+++ JavaScript/Codewars (restructured)/6kyu/stopGninnipsMySdrow.js
@@ -13,3 +13,17 @@ https://www.codewars.com/kata/5264d2b162488dc400000001/solutions/javascript
 // Algorithms
 
 // Suggest kata description edits
+
+
+// My solutiob
+function spinWords(string){
+  return string.split(" ")
+          .map(e => e.length >= 5 ? e.split("").reverse().join("") : e)
+          .join(" ");
+}
+
+
+// other solution
+function spinWords(string){
+  return string.replace(/\w{5,}/g, function(w) { return w.split('').reverse().join('') })
+}
