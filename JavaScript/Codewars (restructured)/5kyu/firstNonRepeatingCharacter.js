2ef388658b8cee999c3a6dccf17143ab3b324dd9
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-23 16:24:51 +0100

    Update firstNonRepeatingCharacter.js

--- JavaScript/Codewars (restructured)/5kyu/firstNonRepeatingCharacter.js
+++ JavaScript/Codewars (restructured)/5kyu/firstNonRepeatingCharacter.js
@@ -8,3 +8,36 @@ https://www.codewars.com/kata/52bc74d4ac05d0945d00054e/train/javascript
 // As an added challenge, upper- and lowercase letters are considered the same character, but the function should return the correct case for the initial letter. For example, the input 'sTreSS' should return 'T'.
 
 // If a string contains all repeating characters, it should return an empty string ("") or None -- see sample tests.
+
+
+// My solution
+function firstNonRepeatingLetter(s) {
+  let lower = s.toLowerCase()
+  for (let i = 0; i < s.length; i++) {
+    if (s.indexOf(s[i]) === s.lastIndexOf(s[i]) && 
+        s.toLowerCase().indexOf(s[i]) === s.toLowerCase().lastIndexOf(s[i]) &&
+        s.toUpperCase().indexOf(s[i]) === s.toUpperCase().lastIndexOf(s[i]))
+      return s[i]
+  }
+  return ''
+}
+
+
+// other solution
+function firstNonRepeatingLetter(s) {
+  var t=s.toLowerCase();
+  for (var x=0;x<t.length;x++)
+    if(t.indexOf(t[x]) === t.lastIndexOf(t[x]))
+      return s[x];
+  return "";
+}
+
+
+function firstNonRepeatingLetter(s) {
+  for(var i in s) {
+    if(s.match(new RegExp(s[i],"gi")).length === 1) {
+      return s[i];
+    }
+  }
+  return '';
+}
