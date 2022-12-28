79548a783deea3bf1d5ddfc00e4dfa71025f4437
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-28 13:04:05 +0100

    Update sumOfMinimum.js

--- JavaScript/Codewars (restructured)/7kyu/sumOfMinimum.js
+++ JavaScript/Codewars (restructured)/7kyu/sumOfMinimum.js
@@ -1,3 +1,4 @@
+https://www.codewars.com/kata/5d5ee4c35162d9001af7d699/javascript
 // Description:
 
 // Given a 2D ( nested ) list ( array, vector, .. ) of size m * n, your task is to find the sum of the minimum values in each row.
