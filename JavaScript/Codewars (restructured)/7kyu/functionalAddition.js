1ae9b99513a4799b327184068567d75d00ac309e
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-28 12:10:14 +0100

    Update functionalAddition.js

--- JavaScript/Codewars (restructured)/7kyu/functionalAddition.js
+++ JavaScript/Codewars (restructured)/7kyu/functionalAddition.js
@@ -10,3 +10,16 @@ https://www.codewars.com/kata/538835ae443aae6e03000547/javascript
 
 // var addThree = add(3);
 // addThree(3); // 6
+
+
+// My solution
+function add(n) {
+  return (function (m) {
+    return n + m
+  })
+}
+
+
+
+// other solution
+const add = n => x => x + n;
