352cf3d5ab33a1463ca5f1a052b886b68745a718
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-29 12:44:01 +0100

    Update enumerableMagicDoesMyListIncludeThis.js

--- JavaScript/Codewars solutions/8kyu/enumerableMagicDoesMyListIncludeThis.js
+++ JavaScript/Codewars solutions/8kyu/enumerableMagicDoesMyListIncludeThis.js
@@ -3,3 +3,17 @@ https://www.codewars.com/kata/545991b4cbae2a5fda000158/javascript
 
 // Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
 // Fundamentals
+
+
+// My solution
+function include(arr, item){
+  return arr.includes(item)
+}
+
+
+// other solution
+function include(arr, item) {
+  return arr.indexOf(item) !== -1;
+}
+
+
