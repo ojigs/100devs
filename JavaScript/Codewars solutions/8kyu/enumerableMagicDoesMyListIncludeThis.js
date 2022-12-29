60f0faca98f9bdc29fe822df9620e929eb946317
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-29 12:41:23 +0100

    Create enumerableMagicDoesMyListIncludeThis.js

---
+++ JavaScript/Codewars solutions/8kyu/enumerableMagicDoesMyListIncludeThis.js
@@ -0,0 +1,5 @@
+https://www.codewars.com/kata/545991b4cbae2a5fda000158/javascript
+// Description:
+
+// Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+// Fundamentals
