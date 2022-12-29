c30b4ec92fa542f894a91696a7cf92fdffcdaca0
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-29 14:20:17 +0100

    Update commonDenominators.js

--- JavaScript/Codewars (restructured)/5kyu/commonDenominators.js
+++ JavaScript/Codewars (restructured)/5kyu/commonDenominators.js
@@ -40,3 +40,35 @@ https://www.codewars.com/kata/54d7660d2daf68c619000d95/train/javascript
 // Fundamentals
 // Algorithms
 // Mathematics
+
+
+
+// My solution
+function convertFrac(lst){
+//   Parameters: input is a 2d array of numerator and denometor pairs
+//   Return: To return a transformed 2d array with a common denominator, such that each pair is an equal fraction to the equivalent air in the original array
+//   Examples: [[1,2], [1,3], [1,4]] --> [[6,12], [4,12], [3,12]]
+//   Pseodocode: single out array elements in the second index which is the denominator
+//   reduce it, i.e programmatically take the L.C.M
+//   multiply the original fraction by the common denominator
+//   return the result of the multiplication as the numerator
+//   return the common denominator as the second index in the array
+  if (!lst.length) return ''
+  let denArr = []
+  lst.forEach(e => denArr.push(e[1]))
+//   Euclid algo for greatest common denominator
+  const gcd = (a, b) => a ? gcd(b%a, a): b
+//   get the l.c.m
+  let D = denArr.reduce((a, c) => a * c / gcd(a, c))
+  return lst.map(e => `(${Math.round(e[0]/e[1]*D)},${D})`).join('')
+}
+
+
+// other solution
+const gcd = (a, b) => b ? gcd(b, a % b) : a;
+const lcm = (a, b) => a * b / gcd(a, b);
+
+function convertFrac(arr) {
+  const cd = arr.reduce((a, [_, d]) => lcm(d, a), 1);
+  return arr.map(([n, d]) => `(${n * cd/d},${cd})`).join('');
+}
