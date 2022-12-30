07a4f6d40b60c48564b88bb4bc18524e52ceface
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2022-12-30 11:42:25 +0100

    Create regexBasicsIsItADigit.js

---
+++ JavaScript/Codewars solutions/8kyu/regexBasicsIsItADigit.js
@@ -0,0 +1,4 @@
+https://www.codewars.com/kata/567bf4f7ee34510f69000032/javascript
+// Description:
+
+// Implement String#digit? (in Java StringUtils.isDigit(String)), which should return true if given object is a digit (0-9), false otherwise.
