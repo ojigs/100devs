74d4d6398016ac789207fde40cc8b70a4d32b025
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2023-01-03 16:29:50 +0100

    Update collatzConjectureLength.js

--- JavaScript/Codewars (restructured)/7kyu/collatzConjectureLength.js
+++ JavaScript/Codewars (restructured)/7kyu/collatzConjectureLength.js
@@ -33,3 +33,19 @@ function collatz(n) {
   }
   return arr.length;
 }
+
+// other solution
+function collatz(n) {
+  var l = 1;
+  while (n > 1) {
+    l++;
+    n = n % 2 == 0 ? n / 2 : n * 3 + 1;
+  }
+  return l;
+}
+
+function collatz(n) {
+  if (n === 1) return 1;
+  else if (n % 2 === 0) return 1 + collatz(n / 2);
+  else return 1 + collatz(3 * n + 1);
+}
