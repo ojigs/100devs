270352274968720af613973057deff6c79623eed
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-01-04 14:19:52 +0100

    Create power.js

---
+++ JavaScript/Codewars solutions/8kyu/power.js
@@ -0,0 +1,11 @@
+https://www.codewars.com/kata/562926c855ca9fdc4800005b/train/javascript
+// Description
+
+// The goal is to create a function 'numberToPower(number, power)' that "raises" the number up to power (ie multiplies number by itself power times).
+// Examples
+
+// numberToPower(3,2)  // -> 9 ( = 3 * 3 )
+// numberToPower(2,3)  // -> 8 ( = 2 * 2 * 2 )
+// numberToPower(10,6) // -> 1000000
+
+// Note: Math.pow and some other Math functions like eval() and ** are disabled.
