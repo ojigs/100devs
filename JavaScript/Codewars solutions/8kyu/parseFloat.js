2a7aef2aaae6db90372fd68743e30586cba95093
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-01-05 12:33:59 +0100

    Update parseFloat.js

--- JavaScript/Codewars solutions/8kyu/parseFloat.js
+++ JavaScript/Codewars solutions/8kyu/parseFloat.js
@@ -2,3 +2,13 @@ https://www.codewars.com/kata/57a386117cb1f31890000039/javascript
 // Description:
 
 // Write function parseF which takes an input and returns a number or null if conversion is not possible. The input can be one of many different types so be aware.
+
+
+// My solution
+function parseF(s) {
+  return Number.isNaN(parseFloat(s)) ? null : parseFloat(s)
+}
+
+
+//other solution
+const parseF = s =>  isNaN(parseFloat(s)) ? null : parseFloat(s)
