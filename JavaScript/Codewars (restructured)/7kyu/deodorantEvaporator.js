78653d1efa593b31cd76e52be3803ffb5ce6d8ea
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-01-06 11:44:19 +0100

    Update deodorantEvaporator.js

--- JavaScript/Codewars (restructured)/7kyu/deodorantEvaporator.js
+++ JavaScript/Codewars (restructured)/7kyu/deodorantEvaporator.js
@@ -14,3 +14,22 @@ https://www.codewars.com/kata/5506b230a11c0aeab3000c1f/javascript
 
 // Content is in fact not necessary in the body of the function "evaporator", you can use it or not use it, as you wish. Some people might prefer to reason with content, some other with percentages only. It's up to you but you must keep it as a parameter because the tests have it as an argument.
 // Fundamentals
+
+
+// My solution
+function evaporator(content, evap_per_day, threshold){ 
+  let n = 100, res = 0
+  while (n > threshold) {
+    n -= n * evap_per_day/100
+    res++
+  }
+  return res
+}
+
+
+//other solution
+function evaporator(content, evap_per_day, threshold){ 
+  threshold = threshold / 100
+  evap_per_day = evap_per_day / 100
+  return  Math.ceil(Math.log(threshold) / Math.log(1-evap_per_day))
+}
