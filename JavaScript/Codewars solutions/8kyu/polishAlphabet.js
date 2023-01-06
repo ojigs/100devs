b525ca070dd171ab4cb89aa5cafaa303bca2c142
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-01-06 12:49:14 +0100

    Create polishAlphabet.js

---
+++ JavaScript/Codewars solutions/8kyu/polishAlphabet.js
@@ -0,0 +1,25 @@
+https://www.codewars.com/kata/57ab2d6072292dbf7c000039/javascript
+// Description:
+
+// There are 32 letters in the Polish alphabet: 9 vowels and 23 consonants.
+
+// Your task is to change the letters with diacritics:
+
+// ą -> a,
+// ć -> c,
+// ę -> e,
+// ł -> l,
+// ń -> n,
+// ó -> o,
+// ś -> s,
+// ź -> z,
+// ż -> z
+
+// and print out the string without the use of the Polish letters.
+
+// For example:
+
+// "Jędrzej Błądziński"  -->  "Jedrzej Bladzinski"
+
+// Strings
+// Fundamentals
