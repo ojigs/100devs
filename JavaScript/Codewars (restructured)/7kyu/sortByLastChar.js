e64b1be26a6505190159630edd5ffc287e9752d3
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-01-09 14:49:04 +0100

    Create sortByLastChar.js

---
+++ JavaScript/Codewars (restructured)/7kyu/sortByLastChar.js
@@ -0,0 +1,12 @@
+https://www.codewars.com/kata/57eba158e8ca2c8aba0002a0/javascript
+// Description:
+
+// Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+// If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+
+// All inputs will be valid.
+// Fundamentals
+// Strings
+// Arrays
+// Sorting
