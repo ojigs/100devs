15814fc79d8e05667d032bba793e491b8e4ee010
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-01-11 13:33:47 +0100

    Update duckDuckGoose.js

--- JavaScript/Codewars solutions/8kyu/duckDuckGoose.js
+++ JavaScript/Codewars solutions/8kyu/duckDuckGoose.js
@@ -26,3 +26,14 @@ https://www.codewars.com/kata/582e0e592029ea10530009ce/javascript
 function duckDuckGoose(players, goose) {
   return players[(goose-1) % players.length].name
 }
+
+
+
+// other solution
+function duckDuckGoose(players, goose) {
+  for (var i = 0; i < players.length; i++) {
+    if (i === (goose-1) % players.length) {
+      return players[i].name;
+    }
+  }
+}
