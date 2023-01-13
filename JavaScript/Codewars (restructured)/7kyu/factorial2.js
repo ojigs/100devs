8ac593e3de2149659dd5a2c9c3f1d3e8ef1e2dc1
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-01-13 12:26:18 +0100

    Update factorial2.js

--- JavaScript/Codewars (restructured)/7kyu/factorial2.js
+++ JavaScript/Codewars (restructured)/7kyu/factorial2.js
@@ -5,3 +5,29 @@ https://www.codewars.com/kata/57a049e253ba33ac5e000212/javascript
 
 // https://en.wikipedia.org/wiki/Factorial
 // Fundamentals
+
+
+
+/My solution
+function factorial(n){
+  if (n <= 1) {
+    return 1
+  } 
+  return n * factorial(n-1)
+}
+
+
+// other solution
+const factorial = n => n ? factorial(n - 1) * n : 1;
+
+
+function factorial(n){
+  let answer = 1;
+  
+  while (n > 0) {
+    answer *= n;
+    n--;
+  }
+  
+  return answer;
+}
