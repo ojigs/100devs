52e1f8436a567543645c7d00083630317be7cff1
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-01-13 11:36:43 +0100

    Create superDuperEasy.js

---
+++ JavaScript/Codewars solutions/8kyu/superDuperEasy.js
@@ -0,0 +1,4 @@
+https://www.codewars.com/kata/55a5bfaa756cfede78000026/javascript
+// Description:
+
+// Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
