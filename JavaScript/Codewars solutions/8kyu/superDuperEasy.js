8a9041e218549a95f0765d7e79000ef662ed0a9b
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-01-13 11:51:44 +0100

    Update superDuperEasy.js

--- JavaScript/Codewars solutions/8kyu/superDuperEasy.js
+++ JavaScript/Codewars solutions/8kyu/superDuperEasy.js
@@ -2,3 +2,14 @@ https://www.codewars.com/kata/55a5bfaa756cfede78000026/javascript
 // Description:
 
 // Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+
+// My solution
+function problem(x){
+  return typeof x === 'number' ? x*50+6 : 'Error'
+}
+
+
+// other solution
+const problem = x =>
+  Number.isFinite(x) ? x * 50 + 6 : `Error`;
