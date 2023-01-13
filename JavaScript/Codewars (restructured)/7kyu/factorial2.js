f576b8dc5d633858cad6a84586884890e332f757
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-01-13 12:17:41 +0100

    Create factorial2.js

---
+++ JavaScript/Codewars (restructured)/7kyu/factorial2.js
@@ -0,0 +1,7 @@
+https://www.codewars.com/kata/57a049e253ba33ac5e000212/javascript
+// Description:
+
+// Your task is to write function factorial.
+
+// https://en.wikipedia.org/wiki/Factorial
+// Fundamentals
