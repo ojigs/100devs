ca46cf7a6bff877bc63f7ea73bcaeb44d9101dd2
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-01-13 12:14:28 +0100

    Update sumOfMultiples.js

--- JavaScript/Codewars solutions/8kyu/sumOfMultiples.js
+++ JavaScript/Codewars solutions/8kyu/sumOfMultiples.js
@@ -14,3 +14,29 @@ https://www.codewars.com/kata/57241e0f440cd279b5000829/javascript
 // sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
 // sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
 // sumMul(4, -7)  ==> "INVALID"
+
+
+// My solution
+function sumMul(n,m){
+  if (n >= m) return 'INVALID'
+  let sum = n
+  for (let i = n; i < m - n; i+=n) {
+    sum += n + i
+  }
+  return sum
+}
+
+
+// other solution
+function sumMul(n,m){
+  if (n >= m) return "INVALID";
+
+var sum = 0;
+  for (var i = n; i < m; i+=n) {
+    sum += i;
+  }
+  return sum;
+}
+
+
+const sumMul = (n, m) => ~~(m/n) * (m+n - m%n) / 2 || 'INVALID';
