2f05900dddec7098fd2071907190ca1ee5187774
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-01-14 18:47:50 +0100

    Create recursion2Fibonacci.js

---
+++ JavaScript/Codewars (restructured)/7kyu/recursion2Fibonacci.js
@@ -0,0 +1,19 @@
+https://www.codewars.com/kata/5694cb0ec554589633000036/solutions/javascript
+// Description:
+// Do you know recursion?
+// This is a kata series that you can only solve using recursion.
+
+// ##2 - Fibonacci number
+
+// In mathematical terms, the sequence f(n) of fibonacci numbers is defined by the recurrence relation
+
+// f(n) = f(n-1) + f(n-2)
+
+// with seed values
+
+// f(1) = 1 and f(2) = 1
+
+// #Your task
+
+// You have to create the function fibonacci that receives n and returns f(n). You have to use recursion.
+// Previous (Recursion #1 - Factorial) 
