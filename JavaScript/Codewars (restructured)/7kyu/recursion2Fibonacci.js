52f7cda03244f956faa432a1a72db3867259be18
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-01-14 18:54:59 +0100

    Update recursion2Fibonacci.js

--- JavaScript/Codewars (restructured)/7kyu/recursion2Fibonacci.js
+++ JavaScript/Codewars (restructured)/7kyu/recursion2Fibonacci.js
@@ -17,3 +17,18 @@ https://www.codewars.com/kata/5694cb0ec554589633000036/solutions/javascript
 
 // You have to create the function fibonacci that receives n and returns f(n). You have to use recursion.
 // Previous (Recursion #1 - Factorial) 
+
+
+// My solution
+const fibonacci = n => {
+  if (n <= 2) {
+    return 1
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2)
+};
+
+
+// other solution
+var cache = { 1 : 1, 2 : 1}
+
+const fibonacci = n => cache[n] || (cache[n] = fibonacci(n - 1) + fibonacci(n - 2))
