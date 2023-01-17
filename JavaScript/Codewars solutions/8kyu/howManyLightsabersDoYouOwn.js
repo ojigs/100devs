f3bbb269b689f58db312334b163fa500603f2d25
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-01-17 22:48:54 +0100

    Update howManyLightsabersDoYouOwn.js

--- JavaScript/Codewars solutions/8kyu/howManyLightsabersDoYouOwn.js
+++ JavaScript/Codewars solutions/8kyu/howManyLightsabersDoYouOwn.js
@@ -16,3 +16,20 @@ https://www.codewars.com/kata/51f9d93b4095e0a7200001b8/javascript
 // "Zach" --> 18
 
 // Fundamentals
+
+
+// My solution
+function howManyLightsabersDoYouOwn(name) {
+  return name ==='Zach' ? 18 : 0
+}
+
+
+// other solution
+// TODO: Make some stuff more good here
+var howManyLightsabersDoYouOwn = function(name){
+  if (name === "Zach"){
+    return 18;
+  } else {
+    return 0;
+  }
+}
