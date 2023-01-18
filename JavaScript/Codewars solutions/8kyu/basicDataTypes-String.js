496f3df1675d295b6bf77461c3ef233a9c3cf9a5
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-01-18 16:05:36 +0100

    Create basicDataTypes-String.js

---
+++ JavaScript/Codewars solutions/8kyu/basicDataTypes-String.js
@@ -0,0 +1,16 @@
+https://www.codewars.com/kata/571edea4b625edcb51000d8e/javascript
+// Description:
+
+// In javascript, String is one of basic data types. Use "" or '' to declare a string variable. It can be letters:a,b,c,A,B,C, numbers: 123..., Various symbol:@#$%^&* , Control symbol:\n,\t etc..
+
+// String can use operators +, Connect two or more strings together.
+// Task
+
+// misson 1: I've create three function, and defined some global variables, please select some variables that can make up the name of the function, and return them(Please note the uppercase and lowercase letters are different).
+
+// When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+// misson 2: After misson 1 finished. you should click "Attempt" to see my three questions, and write the answer in function answer1, answer2,answer3
+
+// If everything is right, click "Submit" again to submit your code pass this kata.
+// Series
