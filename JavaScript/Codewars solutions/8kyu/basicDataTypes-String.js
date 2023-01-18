e3b85245e9747b19fd94d21435ca7fbf91fd2034
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-01-18 16:12:04 +0100

    Update basicDataTypes-String.js

--- JavaScript/Codewars solutions/8kyu/basicDataTypes-String.js
+++ JavaScript/Codewars solutions/8kyu/basicDataTypes-String.js
@@ -14,3 +14,46 @@ https://www.codewars.com/kata/571edea4b625edcb51000d8e/javascript
 
 // If everything is right, click "Submit" again to submit your code pass this kata.
 // Series
+
+
+// My solution
+var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
+function Dad(){
+  //select some variable to combine "Dad"
+  return d1+a2+d2;
+}
+function Bee(){
+  //select some variable to combine "Bee"
+  return b1+e2+e2;
+}
+function banana(){
+  //select some variable to combine "banana"
+  return b2+a2+n2+a2+n2+a2;
+}
+
+//answer some questions if you finished works above
+function answer1(){
+  //the answer should be "yes" or "no"
+  return "no";
+}
+function answer2(){
+  //the answer should be "yes" or "no"
+  return "no";
+}
+function answer3(){
+  //the answer should be "yes" or "no"
+  return "yes";
+}
+
+
+
+// ES6 fat arrow function solution
+var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
+
+const Dad = () => d1 + a2 + d2
+const Bee = () => b1 + e2 + e2
+const banana = () => b2 + a2 + n2 + a2 + n2 + a2
+
+const answer1 = () => "no"
+const answer2 = () => "no"
+const answer3 = () => "yes"
