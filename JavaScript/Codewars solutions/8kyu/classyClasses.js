d261ee8a0d0b7aaadca00e83ea899d1e18154764
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-01-20 22:16:15 +0100

    Update classyClasses.js

--- JavaScript/Codewars solutions/8kyu/classyClasses.js
+++ JavaScript/Codewars solutions/8kyu/classyClasses.js
@@ -8,3 +8,23 @@ https://www.codewars.com/kata/55a144eff5124e546400005a/javascript
 // Your task is to complete this Class, the Person class has been created. You must fill in the Constructor method to accept a name as string and an age as number, complete the get Info property and getInfo method/Info getter which should return johns age is 34
 
 // Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+
+
+// My solution
+class Person {
+  constructor(name, age) {
+    this.name = name
+    this.age = age
+  }
+  get info() {
+    return `${this.name}s age is ${this.age}`
+  }
+}
+
+
+// other solution
+class Person {
+  constructor(name, age) {
+    this.info = `${name}s age is ${age}`;
+  }
+}
