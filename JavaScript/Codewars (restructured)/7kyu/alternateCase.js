d50fc7b3a6a6fa1eeec9e9dd353716503b6efbac
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-01-21 12:05:44 +0100

    Update alternateCase.js

--- JavaScript/Codewars (restructured)/7kyu/alternateCase.js
+++ JavaScript/Codewars (restructured)/7kyu/alternateCase.js
@@ -3,3 +3,15 @@ https://www.codewars.com/kata/57a62154cf1fa5b25200031e/javascript
 
 // Write function alternateCase which switch every letter in string from upper to lower and from lower to upper. E.g: Hello World -> hELLO wORLD
 // Fundamentals
+
+
+// My solution
+function alternateCase(s) {
+  let arr = s.split('')
+  return arr.map(e => e.toUpperCase() === e ? e.toLowerCase() : e.toUpperCase()).join('')
+}
+
+
+// other solution
+const alternateCase = s =>
+  s.replace(/\w/g, val => val[`to${val < `a` ? `Low` : `Upp`}erCase`]());
