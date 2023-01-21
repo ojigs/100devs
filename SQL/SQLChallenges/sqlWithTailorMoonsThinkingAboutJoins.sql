35ad59c2d9814a97b86c606b048b941a4da36327
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-01-21 16:02:19 +0100

    Update sqlWithTailorMoonsThinkingAboutJoins.sql

--- SQL/SQLChallenges/sqlWithTailorMoonsThinkingAboutJoins.sql
+++ SQL/SQLChallenges/sqlWithTailorMoonsThinkingAboutJoins.sql
@@ -1,5 +1,5 @@
-https://www.codewars.com/kata/5ab7a736edbcfc8e62000007/train/sql
--- Practise some SQL fundamentals by making a simple database on a topic you feel familiar with. Or use mine, populated with a wealth of Sailor Moon trivia.
+/* https://www.codewars.com/kata/5ab7a736edbcfc8e62000007/train/sql
+Practise some SQL fundamentals by making a simple database on a topic you feel familiar with. Or use mine, populated with a wealth of Sailor Moon trivia.
 sailorsenshi schema
 
     id
@@ -18,13 +18,28 @@ schools schema
     id
     school
 
--- Return a results table - sailor_senshi, real_name, cat and school - of all characters, containing each character's high school, their civilian name and the cat who introduced them to their magical crime-fighting destiny.
+Return a results table - sailor_senshi, real_name, cat and school - of all characters, containing each character's high school, their civilian name and the cat who introduced them to their magical crime-fighting destiny.
 
--- Keep in mind some senshi were not initiated by a cat guardian and one is not in high school. The field can be left blank if this is the case.
+Keep in mind some senshi were not initiated by a cat guardian and one is not in high school. The field can be left blank if this is the case.
 Fundamentals
 SQL
-Databases
+Databases */
 
 -- My solution
+SELECT sailorsenshi.senshi_name AS sailor_senshi, sailorsenshi.real_name_jpn AS real_name, cats.name AS cat, schools.school AS school 
+FROM sailorsenshi 
+LEFT JOIN cats  
+ON sailorsenshi.cat_id = cats.id
+FULL OUTER JOIN schools
+ON sailorsenshi.school_id = schools.id
 
 
+-- other solution
+SELECT
+  senshi_name AS sailor_senshi,
+  real_name_jpn AS real_name,
+  cats.name AS cat,
+  school AS school
+FROM sailorsenshi
+LEFT JOIN cats ON cat_id = cats.id
+LEFT JOIN schools ON school_id = schools.id
