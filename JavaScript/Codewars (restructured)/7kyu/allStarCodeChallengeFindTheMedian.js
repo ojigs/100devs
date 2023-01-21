410755a5d4cc56558a747a6ebd31678c067ee6bd
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-01-21 15:28:05 +0100

    Update allStarCodeChallengeFindTheMedian.js

--- JavaScript/Codewars (restructured)/7kyu/allStarCodeChallengeFindTheMedian.js
+++ JavaScript/Codewars (restructured)/7kyu/allStarCodeChallengeFindTheMedian.js
@@ -6,3 +6,24 @@ https://www.codewars.com/kata/5963c18ecb97be020b0000a2/javascript
 // The median is the middle number of a sorted list of numbers. If the list is of even length, the 2 middle values are averaged.
 
 // Write a function that takes an array of integers as an argument and returns the median of those integers. 
+
+
+// My solution
+function median(array) {
+  let sortedArr = array.sort((a,b) => a - b)
+  if (array.length%2) {
+    return sortedArr[Math.floor(array.length/2)]
+  } else {
+    let mid1 = sortedArr[Math.floor(array.length/2)]
+    let mid2 = sortedArr[Math.floor(array.length/2) - 1]
+    return (mid1 + mid2)/2
+  }
+}
+
+
+// other solution
+var median = (arr) => {
+  const start = arr.length/2 + 0.9|0
+  const end = arr.length % 2 ? 1 : 2
+  return arr.sort((i,j) =>i-j).slice(start-1, start+end-1).reduce((a,b)=> a+b)/end
+}
