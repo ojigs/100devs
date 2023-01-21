99f7bf0228b5cae23adeb27ea44ce751aa6ec05b
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-01-21 15:25:51 +0100

    Create allStarCodeChallengeFindTheMedian.js

---
+++ JavaScript/Codewars (restructured)/7kyu/allStarCodeChallengeFindTheMedian.js
@@ -0,0 +1,8 @@
+https://www.codewars.com/kata/5963c18ecb97be020b0000a2/javascript
+// Description
+
+// Your non-profit company has assigned you the task of calculating some simple statistics on donations. You have an array of integers, representing various amounts of donations your company has been given. In particular, you're interested in the median value for donations.
+
+// The median is the middle number of a sorted list of numbers. If the list is of even length, the 2 middle values are averaged.
+
+// Write a function that takes an array of integers as an argument and returns the median of those integers. 
