3a70f9f8c6d7e0ec1d2c634fac6f650352fd48d7
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-01-21 11:37:45 +0100

    Update whoAteTheCookie.js

--- JavaScript/Codewars solutions/8kyu/whoAteTheCookie.js
+++ JavaScript/Codewars solutions/8kyu/whoAteTheCookie.js
@@ -9,3 +9,16 @@ https://www.codewars.com/kata/55a996e0e8520afab9000055/javascript
 
 // Please leave feedback for this kata. Cheers!
 // Fundamentals
+
+
+// My solution
+function cookie(x){
+  let name = typeof x === 'string'? 'Zach' : typeof x === 'number' ? 'Monica' : 'the dog'
+  return `Who ate the last cookie? It was ${name}!`
+}
+
+
+// other solution
+function cookie(x){
+  return `Who ate the last cookie? It was ${({string: "Zach", number: "Monica"}[typeof x] || "the dog")}!`;
+}
