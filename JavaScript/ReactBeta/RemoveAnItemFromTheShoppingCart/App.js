fa99e7efdc72ae4e81c3f566dd65c64733aca92e
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2023-01-23 19:08:03 +0100

    Update RemoveAnItemFromTheShoppingCart

--- JavaScript/ReactBeta/RemoveAnItemFromTheShoppingCart/App.js
+++ JavaScript/ReactBeta/RemoveAnItemFromTheShoppingCart/App.js
@@ -1,6 +1,63 @@
 // To do
 // This shopping cart has a working ”+” button, but the ”–” button doesn’t do anything. You need to add an event handler to it so that pressing it decreases the count of the corresponding product. If you press ”–” when the count is 1, the product should automatically get removed from the cart. Make sure it never shows 0.
 
+// import { useState } from "react";
+
+// const initialProducts = [
+//   {
+//     id: 0,
+//     name: "Baklava",
+//     count: 1,
+//   },
+//   {
+//     id: 1,
+//     name: "Cheese",
+//     count: 5,
+//   },
+//   {
+//     id: 2,
+//     name: "Spaghetti",
+//     count: 2,
+//   },
+// ];
+
+// export default function ShoppingCart() {
+//   const [products, setProducts] = useState(initialProducts);
+
+//   function handleIncreaseClick(productId) {
+//     setProducts(
+//       products.map((product) => {
+//         if (product.id === productId) {
+//           return {
+//             ...product,
+//             count: product.count + 1,
+//           };
+//         } else {
+//           return product;
+//         }
+//       })
+//     );
+//   }
+
+//   return (
+//     <ul>
+//       {products.map((product) => (
+//         <li key={product.id}>
+//           {product.name} (<b>{product.count}</b>)
+//           <button
+//             onClick={() => {
+//               handleIncreaseClick(product.id);
+//             }}
+//           >
+//             +
+//           </button>
+//           <button>–</button>
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// }
+
 import { useState } from "react";
 
 const initialProducts = [
@@ -39,6 +96,21 @@ export default function ShoppingCart() {
     );
   }
 
+  function handleDecreaseClick(productId) {
+    let nextProducts = products.map((product) => {
+      if (product.id === productId) {
+        return {
+          ...product,
+          count: product.count - 1,
+        };
+      } else {
+        return product;
+      }
+    });
+    nextProducts = nextProducts.filter((product) => product.count > 0);
+    setProducts(nextProducts);
+  }
+
   return (
     <ul>
       {products.map((product) => (
@@ -51,7 +123,13 @@ export default function ShoppingCart() {
           >
             +
           </button>
-          <button>–</button>
+          <button
+            onClick={() => {
+              handleDecreaseClick(product.id);
+            }}
+          >
+            –
+          </button>
         </li>
       ))}
     </ul>
