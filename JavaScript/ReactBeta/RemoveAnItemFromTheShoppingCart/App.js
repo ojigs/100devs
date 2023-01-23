6997eb88024ee13282ad57d7fdfa7e821ba150c8
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2023-01-23 18:38:58 +0100

    Create RemoveAnItemFromTheShoppingCart

---
+++ JavaScript/ReactBeta/RemoveAnItemFromTheShoppingCart/App.js
@@ -0,0 +1,59 @@
+// To do
+// This shopping cart has a working ”+” button, but the ”–” button doesn’t do anything. You need to add an event handler to it so that pressing it decreases the count of the corresponding product. If you press ”–” when the count is 1, the product should automatically get removed from the cart. Make sure it never shows 0.
+
+import { useState } from "react";
+
+const initialProducts = [
+  {
+    id: 0,
+    name: "Baklava",
+    count: 1,
+  },
+  {
+    id: 1,
+    name: "Cheese",
+    count: 5,
+  },
+  {
+    id: 2,
+    name: "Spaghetti",
+    count: 2,
+  },
+];
+
+export default function ShoppingCart() {
+  const [products, setProducts] = useState(initialProducts);
+
+  function handleIncreaseClick(productId) {
+    setProducts(
+      products.map((product) => {
+        if (product.id === productId) {
+          return {
+            ...product,
+            count: product.count + 1,
+          };
+        } else {
+          return product;
+        }
+      })
+    );
+  }
+
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>
+          {product.name} (<b>{product.count}</b>)
+          <button
+            onClick={() => {
+              handleIncreaseClick(product.id);
+            }}
+          >
+            +
+          </button>
+          <button>–</button>
+        </li>
+      ))}
+    </ul>
+  );
+}
