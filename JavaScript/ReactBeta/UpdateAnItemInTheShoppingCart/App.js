134234030305b60809565350f959d857b250110d
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2023-01-23 18:34:54 +0100

    Update UpdateAnItemInTheShoppingCart

--- JavaScript/ReactBeta/UpdateAnItemInTheShoppingCart/App.js
+++ JavaScript/ReactBeta/UpdateAnItemInTheShoppingCart/App.js
@@ -1,6 +1,49 @@
 // To-do
 // Fill in the handleIncreaseClick logic so that pressing ”+” increases the corresponding number:
 
+// import { useState } from "react";
+
+// const initialProducts = [
+//   {
+//     id: 0,
+//     name: "Baklava",
+//     count: 1,
+//   },
+//   {
+//     id: 1,
+//     name: "Cheese",
+//     count: 5,
+//   },
+//   {
+//     id: 2,
+//     name: "Spaghetti",
+//     count: 2,
+//   },
+// ];
+
+// export default function ShoppingCart() {
+//   const [products, setProducts] = useState(initialProducts);
+
+//   function handleIncreaseClick(productId) {}
+
+//   return (
+//     <ul>
+//       {products.map((product) => (
+//         <li key={product.id}>
+//           {product.name} (<b>{product.count}</b>)
+//           <button
+//             onClick={() => {
+//               handleIncreaseClick(product.id);
+//             }}
+//           >
+//             +
+//           </button>
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// }
+
 import { useState } from "react";
 
 const initialProducts = [
@@ -24,7 +67,16 @@ const initialProducts = [
 export default function ShoppingCart() {
   const [products, setProducts] = useState(initialProducts);
 
-  function handleIncreaseClick(productId) {}
+  function handleIncreaseClick(productId) {
+    let nextProducts = products.map((product) => {
+      if (product.id === productId) {
+        return { ...product, count: product.count + 1 };
+      } else {
+        return product;
+      }
+    });
+    setProducts(nextProducts);
+  }
 
   return (
     <ul>
