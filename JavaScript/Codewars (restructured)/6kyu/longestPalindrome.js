57d28f9cc015cef8c4fd96e27266f7ec9fbbe0d6
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-01-23 12:57:21 +0100

    Update longestPalindrome.js

--- JavaScript/Codewars (restructured)/6kyu/longestPalindrome.js
+++ JavaScript/Codewars (restructured)/6kyu/longestPalindrome.js
@@ -16,3 +16,71 @@ https://www.codewars.com/kata/54bb6f887e5a80180900046b/javascript
 // "" -> 0
 
 // Fundamentals
+
+
+
+//My solution
+longestPalindrome=function(s){
+  let longest = 0
+  let left = 0;
+  let right = 0;
+  for (let i = 0; i < s.length; i++) {
+    let len1 = expandFromMiddle(s, i, i);
+    let len2 = expandFromMiddle(s, i, i + 1);
+    let len = Math.max(len1, len2);
+    if (len > longest) {
+      left = i - ((len - 1) / 2);
+      right = i + (len / 2);
+      longest = len;
+    }
+  }
+  return longest;
+}
+
+// helper function
+function expandFromMiddle(s, left, right) {
+  if (s === null || left > right) return 0;
+  while (left >= 0 && right < s.length && s[left] === s[right]) {
+    left--;
+    right++;
+  }
+  return right - left - 1;
+}
+
+
+// other solution
+longestPalindrome=function(s){
+  var a = s.split('');
+  var length = 0;
+  a.forEach(function(c, i){
+    length = Math.max(exploreArrayForImpairPalindrome(i, a), length);
+    length = Math.max(exploreArrayForPairPalindrome(i, a), length);
+  });
+  return length;
+}
+
+exploreArrayForImpairPalindrome=function(i, a){
+  var length = 1;
+  var iter = 1;
+  while(thoseValuesAreEquals(i,i,a,iter)){
+    length+=2;
+    iter++;
+  }
+  return length;
+}
+
+exploreArrayForPairPalindrome=function(i, a){
+  var length = 0;
+  var iter = 0;
+  while(thoseValuesAreEquals(i,i+1,a,iter)){
+    length+=2;
+    iter++;
+  }
+  return length;
+}
+
+thoseValuesAreEquals=function(i1, i2, a, iter){
+  return a[i1-iter] && a[i1-iter]===a[i2+iter];
+}
+
+
