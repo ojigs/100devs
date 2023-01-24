2523073186cf4e4e20f1e257bed2808f614e1c56
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2023-01-24 15:25:46 +0100

    Update FixTheMutationsUsingNonMutativeMethods

--- JavaScript/ReactBeta/FixTheMutationsUsingNonMutativeMethods/App.js
+++ JavaScript/ReactBeta/FixTheMutationsUsingNonMutativeMethods/App.js
@@ -53,3 +53,61 @@
 //     </>
 //   );
 // }
+
+// My solution
+import { useState } from "react";
+import AddTodo from "./AddTodo.js";
+import TaskList from "./TaskList.js";
+
+let nextId = 3;
+const initialTodos = [
+  { id: 0, title: "Buy milk", done: true },
+  { id: 1, title: "Eat tacos", done: false },
+  { id: 2, title: "Brew tea", done: false },
+];
+
+export default function TaskApp() {
+  const [todos, setTodos] = useState(initialTodos);
+
+  function handleAddTodo(title) {
+    let nextTodo = {
+      id: nextId++,
+      title: title,
+      done: false,
+    };
+    setTodos([...todos, nextTodo]);
+  }
+
+  function handleChangeTodo(nextTodo) {
+    let updatedTodo = todos.map((todo) => {
+      if (todo.id === nextTodo.id) {
+        return {
+          ...todo,
+          title: nextTodo.title,
+          done: nextTodo.done,
+        };
+      } else {
+        return todo;
+      }
+    });
+    setTodos(updatedTodo);
+  }
+
+  function handleDeleteTodo(todoId) {
+    const nextTodos = todos.filter((todo) => {
+      return todo.id !== todoId;
+    });
+    setTodos(nextTodos);
+  }
+
+  return (
+    <>
+      <AddTodo onAddTodo={handleAddTodo} />
+      <TaskList
+        todos={todos}
+        onChangeTodo={handleChangeTodo}
+        onDeleteTodo={handleDeleteTodo}
+      />
+    </>
+  );
+}
