233a90c88c88d7a3b133c812a04b42389df574d4
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-01-25 13:20:57 +0100

    Update returnTheDay.js

--- JavaScript/Codewars solutions/8kyu/returnTheDay.js
+++ JavaScript/Codewars solutions/8kyu/returnTheDay.js
@@ -13,3 +13,25 @@ https://www.codewars.com/kata/59dd3ccdded72fc78b000b25/javascript
 //     Otherwise returns "Wrong, please enter a number between 1 and 7"
 
 // Fundamentals
+
+
+// My solution
+function whatday(num) { 
+let day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+return day[num - 1] || 'Wrong, please enter a number between 1 and 7'
+}
+
+
+// other solution
+function whatday(num) { 
+  let days = {
+    1: 'Sunday',
+    2: 'Monday',
+    3: 'Tuesday',
+    4: 'Wednesday',
+    5: 'Thursday',
+    6: 'Friday',
+    7: 'Saturday'
+  }
+  return days[ num ] || 'Wrong, please enter a number between 1 and 7';
+}
