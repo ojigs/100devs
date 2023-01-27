aacf6915aa39cb89cb25cad1ff172f9e9c0c2a55
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-01-27 12:00:16 +0100

    Update oopObjectOrientedPiracy.js

--- JavaScript/Codewars solutions/8kyu/oopObjectOrientedPiracy.js
+++ JavaScript/Codewars solutions/8kyu/oopObjectOrientedPiracy.js
@@ -38,3 +38,26 @@ https://www.codewars.com/kata/54fe05c4762e2e3047000add/javascript
 // Good luck and may you find GOOOLD!
 // Object-oriented Programming
 // Fundamentals
+
+
+// My solution
+class Ship {
+  constructor (draft, crew) {
+    this.draft = draft
+    this.crew = crew
+  }
+  isWorthIt () {
+    return (this.draft - (this.crew * 1.5) > 20)
+  }
+}
+
+
+// other solution
+function Ship(draft,crew) {
+ this.draft = draft;
+ this.crew = crew;
+}
+
+Ship.prototype.isWorthIt = function(){
+return this.draft-(this.crew*1.5) > 20;
+}
