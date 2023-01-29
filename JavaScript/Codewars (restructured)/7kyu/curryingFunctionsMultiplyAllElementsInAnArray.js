0b23af5ba5d13bcfae50a6bdbcc63306b8efc22b
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-01-29 23:36:37 +0100

    Create curryingFunctionsMultiplyAllElementsInAnArray.js

---
+++ JavaScript/Codewars (restructured)/7kyu/curryingFunctionsMultiplyAllElementsInAnArray.js
@@ -0,0 +1,14 @@
+https://www.codewars.com/kata/586909e4c66d18dd1800009b
+// Description:
+
+// To complete this Kata you need to make a function multiplyAll/multiply_all which takes an array of integers as an argument. This function must return another function, which takes a single integer as an argument and returns a new array.
+
+// The returned array should consist of each of the elements from the first array multiplied by the integer.
+
+// Example:
+
+// multiplyAll([1, 2, 3])(2) = [2, 4, 6];
+
+// You must not mutate the original array.
+
+// Here's a nice Youtube video about currying, which might help you if this is new to you.
