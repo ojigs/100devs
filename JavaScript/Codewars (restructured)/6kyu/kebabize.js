69ee17cd122179692a51d1043da4ecdf05b539e8
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-01-30 02:18:06 +0100

    Update kebabize.js

--- JavaScript/Codewars (restructured)/6kyu/kebabize.js
+++ JavaScript/Codewars (restructured)/6kyu/kebabize.js
@@ -13,3 +13,22 @@ https://www.codewars.com/kata/57f8ff867a28db569e000c4a/javascript
 // Fundamentals
 // Strings
 // Regular Expressions
+
+
+// My solution
+function kebabize(str) {
+  console.log(str)
+  return str.replace(/\d/g, '')
+            .replace(/(.)([A-Z])/g, (m, p1, p2) => p1 + '-'+p2)
+            .toLowerCase()
+}
+
+
+// other solution
+function kebabize(str) {
+
+  var nonumber = str.replace(/\d/g, '').split(/(?=[A-Z])/g);
+  return nonumber.join('-').toLowerCase();
+
+}
+
