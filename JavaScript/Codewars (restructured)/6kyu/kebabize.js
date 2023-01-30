64d4933a9d3c3288d423119270ee63abe46017d1
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-01-30 02:15:51 +0100

    Create kebabize.js

---
+++ JavaScript/Codewars (restructured)/6kyu/kebabize.js
@@ -0,0 +1,15 @@
+https://www.codewars.com/kata/57f8ff867a28db569e000c4a/javascript
+// Description
+
+// Modify the kebabize function so that it converts a camel case string into a kebab case.
+
+// kebabize('camelsHaveThreeHumps') // camels-have-three-humps
+// kebabize('camelsHave3Humps') // camels-have-humps
+
+// Notes:
+
+//     the returned string should only contain lowercase letters
+
+// Fundamentals
+// Strings
+// Regular Expressions
