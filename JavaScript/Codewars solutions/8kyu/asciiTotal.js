fba4dd05db1ab888e18a5ffb3fbb139045804868
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-02-02 14:22:03 +0100

    Create asciiTotal.js

---
+++ JavaScript/Codewars solutions/8kyu/asciiTotal.js
@@ -0,0 +1,9 @@
+https://www.codewars.com/kata/58f0ba42e89aa6158400000e/javascript
+// Description:
+
+// You'll be given a string, and have to return the sum of all characters as an int. The function should be able to handle all ASCII characters.
+
+// examples:
+
+// uniTotal("a") == 97 uniTotal("aaa") == 291
+// Fundamentals
