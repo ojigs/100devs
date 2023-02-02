6da975a2a264a780ff4f02964ff3db661d66e3ab
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-02-02 14:25:17 +0100

    Update asciiTotal.js

--- JavaScript/Codewars solutions/8kyu/asciiTotal.js
+++ JavaScript/Codewars solutions/8kyu/asciiTotal.js
@@ -7,3 +7,19 @@ https://www.codewars.com/kata/58f0ba42e89aa6158400000e/javascript
 
 // uniTotal("a") == 97 uniTotal("aaa") == 291
 // Fundamentals
+
+
+// My solution
+function uniTotal (string) {
+  return [...string].reduce((a,c) => a + c.charCodeAt(), 0)
+}
+
+
+// other solution
+function uniTotal(str) {
+  var count = 0;
+  for (var i = 0; i < str.length; i++) {
+    count += str.charCodeAt(i);
+  }
+  return count;
+}
