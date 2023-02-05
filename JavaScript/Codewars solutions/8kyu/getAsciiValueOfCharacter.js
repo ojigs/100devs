d36d4027d4ab69825240aac0149ca0c44a16465c
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2023-02-05 12:53:59 +0100

    Creeate getAsciiValueOfCharacter.js

---
+++ JavaScript/Codewars solutions/8kyu/getAsciiValueOfCharacter.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/55acfc59c3c23d230f00006d/javascript
+// Description:
+
+// Get ASCII value of a character.
+
+// For the ASCII table you can refer to http://www.asciitable.com/
+// Fundamentals
