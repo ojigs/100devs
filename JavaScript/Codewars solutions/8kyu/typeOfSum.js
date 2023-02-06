53949814f8d202f9305d8f5df317aace42fea95e
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-02-06 13:01:06 +0100

    Update typeOfSum.js

--- JavaScript/Codewars solutions/8kyu/typeOfSum.js
+++ JavaScript/Codewars solutions/8kyu/typeOfSum.js
@@ -3,3 +3,13 @@ https://www.codewars.com/kata/5a2e9ae2b6cfd7692a0000ba/javascript
 
 // Return the type of the sum of the two arguments
 // Fundamentals
+
+
+// My solution
+function typeOfSum(a, b) {
+  return typeof (a + b)
+}
+
+
+// othr solution
+const typeOfSum = (a, b) => typeof(a + b);
