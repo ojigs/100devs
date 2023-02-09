00e34196610d9b50d2a2d7e7423461cc4a117485
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-02-09 12:30:37 +0100

    Update checkSameCase.js

--- JavaScript/Codewars solutions/8kyu/checkSameCase.js
+++ JavaScript/Codewars solutions/8kyu/checkSameCase.js
@@ -19,3 +19,32 @@ https://www.codewars.com/kata/5dd462a573ee6d0014ce715b/javascript
 
 // '0' and '?' returns -1
 // Fundamentals
+
+
+// My solution
+function sameCase(a, b){
+  let codeA = a.charCodeAt()
+  let codeB = b.charCodeAt()
+  console.log(codeA, codeB)
+  if ((codeA >= 65 && codeA <= 90 && codeB >= 65 && codeB <= 90) ||
+     (codeA >= 97 && codeA <= 122 && codeB >= 97 && codeB <= 122)) {
+    return 1
+  } else if ((codeA >= 97 && codeA <= 122 && codeB >= 65 && codeB <= 90) || 
+            codeA >= 65 && codeA <= 90 && codeB >= 97 && codeB <= 122) {
+    return 0
+  } else {
+    return -1
+  }
+} // convoluted :o
+
+
+// others solution
+function sameCase(a, b){
+ if(a.toUpperCase() === a.toLowerCase() || b.toLowerCase() === b.toUpperCase()){
+    return -1
+  }else if(a === a.toLowerCase() && b === b.toLowerCase() || a === a.toUpperCase() && b === b.toUpperCase()){
+      return 1
+  }else{
+    return 0
+  }
+}
