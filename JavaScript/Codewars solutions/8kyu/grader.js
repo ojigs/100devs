200a255e906adf37e616903e0e81a56f49151da4
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-02-09 12:41:40 +0100

    Update grader.js

--- JavaScript/Codewars solutions/8kyu/grader.js
+++ JavaScript/Codewars solutions/8kyu/grader.js
@@ -19,3 +19,26 @@ https://www.codewars.com/kata/53d16bd82578b1fb5b00128c/javascript
 // grader(0.6) should be "D"
 
 // Fundamentals
+
+
+// My solution
+function grader(score) {
+  return score > 1 || score < 0.6 ? 'F' :
+    score >= 0.9 ? 'A' : score >= 0.8 ? 'B' : score >= 0.7 ? 'C' : 'D'
+}
+
+
+// other solution
+function grader(score) {
+  if (score === 1) return "A";
+  return ["D", "C", "B", "A"][Math.floor(score*10) - 6] || "F";
+}
+
+
+function grader(score) {
+  if (score>1||score<0.6) return 'F'
+  if (score<0.7) return 'D'
+  if (score<0.8) return 'C'
+  if (score<0.9) return 'B'
+  return 'A'
+}
