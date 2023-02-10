116fc66e6e9b3f1f720a7e4dd511ceec030fb405
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-02-10 11:20:42 +0100

    Update determineOffspringSexBasedOnGenes.js

--- JavaScript/Codewars solutions/8kyu/determineOffspringSexBasedOnGenes.js
+++ JavaScript/Codewars solutions/8kyu/determineOffspringSexBasedOnGenes.js
@@ -9,3 +9,15 @@ https://www.codewars.com/kata/56530b444e831334c0000020/train/javascript
 
 // If the sperm contains the X chromosome, return "Congratulations! You're going to have a daughter."; If the sperm contains the Y chromosome, return "Congratulations! You're going to have a son.";
 // Fundamentals
+
+
+// My solution
+function chromosomeCheck(sperm) {
+  return `Congratulations! You're going to have a ${sperm.includes('Y') ? 'son' : 'daughter'}.`
+}
+
+
+// other solution
+function chromosomeCheck(sperm) {
+  return `Congratulations! You're going to have a ${sperm === 'XY' ? 'son' : 'daughter'}.`
+}
