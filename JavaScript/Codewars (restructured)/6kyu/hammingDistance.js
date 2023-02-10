1fbf412068feb5c543e4391964f3e72ccdfb66c7
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-02-10 12:00:58 +0100

    Create hammingDistance.js

---
+++ JavaScript/Codewars (restructured)/6kyu/hammingDistance.js
@@ -0,0 +1,20 @@
+https://www.codewars.com/kata/5410c0e6a0e736cf5b000e69/train/javascript
+// Description
+
+// The Hamming Distance is a measure of similarity between two strings of equal length. Complete the function so that it returns the number of positions where the input strings do not match.
+// Examples:
+
+// a = "I like turtles"
+// b = "I like turkeys"
+// Result: 3
+
+// a = "Hello World"
+// b = "Hello World"
+// Result: 0
+
+// a = "espresso"
+// b = "Expresso"
+// Result: 2
+
+// You can assume that the two input strings are of equal length.
+// Algorithms
