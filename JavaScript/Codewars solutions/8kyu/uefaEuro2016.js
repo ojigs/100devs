e5a4b4243fd85b753d12e044d746504770da1153
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2023-02-12 12:03:36 +0100

    Create uefaEuro2016.js

---
+++ JavaScript/Codewars solutions/8kyu/uefaEuro2016.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/57613fb1033d766171000d60/javascript
+// Description:
+
+// Finish the uefaEuro2016() function so it return string just like in the examples below:
+
+// uefaEuro2016(['Germany', 'Ukraine'],[2, 0]) // "At match Germany - Ukraine, Germany won!"
+// uefaEuro2016(['Belgium', 'Italy'],[0, 2]) // "At match Belgium - Italy, Italy won!"
+// uefaEuro2016(['Portugal', 'Iceland'],[1, 1]) // "At match Portugal - Iceland, teams played draw."
+
+// Strings
+// Arrays
+// Fundamentals
