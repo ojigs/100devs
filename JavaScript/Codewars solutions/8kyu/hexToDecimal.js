23264b1c36857a977dc120ad4c1a89e839f0ed10
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2023-02-13 12:32:28 +0100

    Create hexToDecimal.js

---
+++ JavaScript/Codewars solutions/8kyu/hexToDecimal.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/57a4d500e298a7952100035d/javascript
+// Description:
+
+// Complete the function which converts hex number (given as a string) to a decimal number.
+// Fundamentals
