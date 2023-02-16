9f8260afee9d2c889d4204de3ecc7db1c372caf3
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-02-16 11:53:11 +0100

    Update and rename variableDebugAssignment.js to grasshopperVariableAssignmentDebug.js

--- JavaScript/Codewars solutions/8kyu/grasshopperVariableAssignmentDebug.js
+++ JavaScript/Codewars solutions/8kyu/grasshopperVariableAssignmentDebug.js
@@ -4,3 +4,11 @@ https://www.codewars.com/kata/5612e743cab69fec6d000077/javascript
 
 // Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
 // Fundamentals
+
+
+// My solution
+var a = "dev"
+var b = "Lab"
+
+var name = a + b
+
