b88bd9fe40cf8f642620831813f3a05a4c92c126
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-02-16 11:50:01 +0100

    Create variableDebugAssignment.js

---
+++ JavaScript/Codewars solutions/8kyu/variableDebugAssignment.js
@@ -0,0 +1,6 @@
+https://www.codewars.com/kata/5612e743cab69fec6d000077/javascript
+// Description:
+// Variable assignment
+
+// Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+// Fundamentals
