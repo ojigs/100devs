bf726cab4e16b495c8891fda978b1841b10d5ba4
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2023-02-17 07:07:37 +0100

    Update forUFCFansTotalBeginner.js

--- JavaScript/Codewars solutions/8kyu/forUFCFansTotalBeginner.js
+++ JavaScript/Codewars solutions/8kyu/forUFCFansTotalBeginner.js
@@ -18,3 +18,22 @@
 
 // The given name may varies in casing, eg., it can be "George Saint Pierre" or "geOrGe saiNT pieRRE". Your solution should treat both as the same thing (case-insensitive).
 // Fundamentals
+
+// My solution
+var quote = function (fighter) {
+  return fighter.toLowerCase() === "george saint pierre"
+    ? "I am not impressed by your performance."
+    : "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+};
+
+// other solution
+var quote = function (fighter) {
+  switch (fighter.toLowerCase()) {
+    case "conor mcgregor":
+      return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+    case "george saint pierre":
+      return "I am not impressed by your performance.";
+    default:
+      "Who are you?";
+  }
+};
