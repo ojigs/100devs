6d3c8d00dc7c8c25aefece9933d17c442157183d
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-02-20 12:47:12 +0100

    Update charCodeCalculation.js

--- JavaScript/Codewars (restructured)/7kyu/charCodeCalculation.js
+++ JavaScript/Codewars (restructured)/7kyu/charCodeCalculation.js
@@ -23,3 +23,25 @@ https://www.codewars.com/kata/57f75cc397d62fc93d000059/javascript
 // Arrays
 // Strings
 // Mathematics
+
+
+// My solution
+function calc(x){
+  let total1 = x.split('').reduce((a,c) => a + c.charCodeAt(), '')
+  let total2 = total1.replace(/7/g, 1)
+  let sum1 = total1.split('').reduce((a,c) => a + (+c), 0)
+  let sum2 = total2.split('').reduce((a,c) => a + (+c), 0)
+  return sum1 - sum2
+}
+
+
+// other ssolution
+function calc(x){
+  let sum = n => [...n].reduce((a,b) => +a + +b);
+  let total1 = x.replace(/./g, x => x.charCodeAt(0));
+  let total2 = total1.replace(/7/g,'1');
+  return sum(total1) - sum(total2);
+}
+
+
+const calc=x=>(x.replace(/./g,x=>x.charCodeAt()).match(/7/g)||[]).length*6
