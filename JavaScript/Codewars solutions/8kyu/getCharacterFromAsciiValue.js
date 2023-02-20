420fef9d27da8d34fe4cfe91245db844e259026d
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-02-20 12:14:01 +0100

    Create getCharacterFromAsciiValue.js

---
+++ JavaScript/Codewars solutions/8kyu/getCharacterFromAsciiValue.js
@@ -0,0 +1,15 @@
+https://www.codewars.com/kata/55ad04714f0b468e8200001c/javascript
+// Description:
+
+// Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+// Example:
+
+// get_char(65)
+
+// should return:
+
+// 'A'
+
+// For ASCII table, you can refer to http://www.asciitable.com/
+// Fundamentals
