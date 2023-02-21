ece39981bc34810a2b7f70975b1b535a5df3a3bc
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-02-21 12:28:46 +0100

    Update makeTheDeadFishSwim.js

--- JavaScript/Codewars (restructured)/6kyu/makeTheDeadFishSwim.js
+++ JavaScript/Codewars (restructured)/6kyu/makeTheDeadFishSwim.js
@@ -52,3 +52,27 @@ function parse(data) {
   
   return res;
 }
+
+
+const parse = data => {
+  const Commands = {
+      INCREMENT:  'i',
+      DECREMENT:  'd',
+      SQUARE:     's',
+      OUTPUT:     'o'
+  }
+
+  var outputs = [],
+      value = 0;
+  
+  data.split('').forEach(command => {
+    switch(command) {
+      case Commands.INCREMENT:  value++;                   break;
+      case Commands.DECREMENT:  value--;                   break;
+      case Commands.SQUARE:     value = Math.pow(value, 2);break;
+      case Commands.OUTPUT:     outputs.push(value);       break;
+    }
+  });
+  
+  return outputs;
+}
