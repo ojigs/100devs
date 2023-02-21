4fe27aa22db1903f716579114bbdb0171707db05
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-02-21 12:27:00 +0100

    Update makeTheDeadFishSwim.js

--- JavaScript/Codewars (restructured)/6kyu/makeTheDeadFishSwim.js
+++ JavaScript/Codewars (restructured)/6kyu/makeTheDeadFishSwim.js
@@ -16,3 +16,39 @@ https://www.codewars.com/kata/51e0007c1f9378fa810002a9
 
 // Parsing
 // Algorithms
+
+
+// My solution
+function parse( data ) {
+  let arr = [], v = 0
+  const op = {
+    i: () => v+=1,
+    d: () => v-=1,
+    s: () => v**=2,
+    o: () => arr.push(v)
+  }
+  for (let i = 0; i < data.length; i++) {
+    if (op[data[i]]) {
+      op[data[i]]()
+    }
+  }
+  return arr
+}
+
+
+
+// other solution
+function parse(data) {
+  let res = [];
+
+  data.split('').reduce((cur, s) => {
+    if (s === 'i') cur++;
+    if (s === 'd') cur--;
+    if (s === 's') cur = Math.pow(cur, 2);
+    if (s === 'o') res.push(cur);
+    
+    return cur;
+  }, 0);
+  
+  return res;
+}
