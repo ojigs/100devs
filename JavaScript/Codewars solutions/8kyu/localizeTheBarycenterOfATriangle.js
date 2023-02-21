9b96429b9bc481b7dc42dd017315f709045cbdec
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-02-21 11:41:13 +0100

    Update localizeTheBarycenterOfATriangle.js

--- JavaScript/Codewars solutions/8kyu/localizeTheBarycenterOfATriangle.js
+++ JavaScript/Codewars solutions/8kyu/localizeTheBarycenterOfATriangle.js
@@ -27,3 +27,21 @@ https://www.codewars.com/kata/5601c5f6ba804403c7000004/train/javascript
 // Fundamentals
 // Mathematics
 // Geometry
+
+
+// My solution
+function barTriang(p1, p2, p3){
+  let x = +((p1[0] + p2[0] + p3[0]) / 3).toFixed(4)
+  let y = +((p1[1] + p2[1] + p3[1]) / 3).toFixed(4)
+  return [x, y]
+}
+
+
+// other solution
+function barTriang(p1, p2, p3){
+  
+  var x = parseFloat(parseFloat((p1[0] + p2[0] + p3[0]) / 3).toFixed(4));
+  var y = parseFloat(parseFloat((p1[1] + p2[1] + p3[1]) / 3).toFixed(4));
+  
+  return [x, y];
+}
