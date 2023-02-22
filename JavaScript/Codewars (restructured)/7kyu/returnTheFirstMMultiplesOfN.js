9a03bb27dc0296fcfd7f97da405d9918b5369c94
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-02-22 11:31:24 +0100

    Create returnTheFirstMMultiplesOfN.js

---
+++ JavaScript/Codewars (restructured)/7kyu/returnTheFirstMMultiplesOfN.js
@@ -0,0 +1,15 @@
+https://www.codewars.com/kata/593c9175933500f33400003e/javascript
+// Description:
+
+// Implement a function, multiples(m, n), which returns an array of the first m multiples of the real number n. Assume that m is a positive integer.
+
+// Ex.
+
+// multiples(3, 5.0)
+
+// should return
+
+// [5.0, 10.0, 15.0]
+
+// Mathematics
+// Fundamentals
