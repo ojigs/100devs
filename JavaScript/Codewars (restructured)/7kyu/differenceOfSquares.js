300fff2a3dec196430b473933d203e855b6afc3a
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-02-23 13:05:06 +0100

    Update differenceOfSquares.js

--- JavaScript/Codewars (restructured)/7kyu/differenceOfSquares.js
+++ JavaScript/Codewars (restructured)/7kyu/differenceOfSquares.js
@@ -18,3 +18,20 @@ https://www.codewars.com/kata/558f9f51e85b46e9fa000025/javascript
 
 // Hence the difference between square of the sum of the first ten natural numbers and the sum of the squares of those numbes is: 3025 - 385 = 2640
 // Fundamentals
+
+
+// My solution
+function differenceOfSquares(n){
+  return (n*(n-1)/2+n)**2 - (n*(n+1)*(2*n+1)/6)
+}
+
+
+// other solution
+function differenceOfSquares(x){
+  var sum = 0, squareSum = 0, i;
+  for (i =1 ; i <= x; i++) {
+    sum += i*i;
+    squareSum += i;
+  }
+  return squareSum*squareSum - sum;
+}
