11d544ce728159bdb629de54227b3b09890b20d6
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2023-02-24 11:39:20 +0100

    Create theOldSwitvheroo.js

---
+++ JavaScript/Codewars (restructured)/7kyu/theOldSwitcheroo.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/55d410c492e6ed767000004f/javascript
+// Description:
+
+// Write a function
+
+// vowel2index(str)
+
+// that takes in a string and replaces all the vowels [a,e,i,o,u] with their respective positions within that string.
+// E.g:
+
+// vowel2index('this is my string') == 'th3s 6s my str15ng'
+// vowel2index('Codewars is the best site in the world') == 'C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld'
+// vowel2index('') == ''
+
+// Your function should be case insensitive to the vowels.
+// Strings
+// Regular Expressions
+// Fundamentals
