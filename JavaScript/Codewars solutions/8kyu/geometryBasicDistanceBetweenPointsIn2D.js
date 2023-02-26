bc561135ab839b6448647277f0026a25823ce3c0
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2023-02-26 12:25:58 +0100

    Create geometryBasicDistanceBeteenPointsIn2D.js

---
+++ JavaScript/Codewars solutions/8kyu/geometryBasicDistanceBetweenPointsIn2D.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/58dced7b702b805b200000be/train/javascript
+// Description
+
+// This series of katas will introduce you to basics of doing geometry with computers.
+
+// Point objects have attributes x and y.
+
+// Write a function calculating distance between Point a and Point b.
+
+// Tests compare expected result and actual answer with tolerance of 1e-6.
+// Geometry
+// Fundamentals
