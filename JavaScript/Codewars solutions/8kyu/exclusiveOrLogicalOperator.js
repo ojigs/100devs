6d8642bb2d05deb8f5f0927ee9afec9f34314a6d
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-03-01 12:23:40 +0100

    Update exclusiveOrLogicalOperator.js

--- JavaScript/Codewars solutions/8kyu/exclusiveOrLogicalOperator.js
+++ JavaScript/Codewars solutions/8kyu/exclusiveOrLogicalOperator.js
@@ -14,3 +14,18 @@ https://www.codewars.com/kata/56fa3c5ce4d45d2a52001b3c/javascript
 
 // Since we cannot define keywords in Javascript (well, at least I don't know how to do it), your task is to define a function xor(a, b) where a and b are the two expressions to be evaluated. Your xor function should have the behaviour described above, returning true if exactly one of the two expressions evaluate to true, false otherwise.
 // Fundamentals
+
+
+
+// My solution
+function xor(a, b) {
+  return a ^ b ? true : false
+}
+
+
+// other solution
+function xor(a, b) {
+  return a != b;
+}
+
+const xor = (a, b) => !!(a ^ b)
