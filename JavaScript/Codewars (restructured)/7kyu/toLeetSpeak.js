083c334cf56054364195f65a2fe04d222f859468
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-03-01 12:43:37 +0100

    Update toLeetSpeak.js

--- JavaScript/Codewars (restructured)/7kyu/toLeetSpeak.js
+++ JavaScript/Codewars (restructured)/7kyu/toLeetSpeak.js
@@ -43,3 +43,48 @@ https://www.codewars.com/kata/57c1ab3949324c321600013f/javascript
 // }
 
 // Fundamentals
+
+
+
+// My solution
+function toLeetSpeak(str) {
+  const leet = {
+  A : '@',
+  B : '8',
+  C : '(',
+  D : 'D',
+  E : '3',
+  F : 'F',
+  G : '6',
+  H : '#',
+  I : '!',
+  J : 'J',
+  K : 'K',
+  L : '1',
+  M : 'M',
+  N : 'N',
+  O : '0',
+  P : 'P',
+  Q : 'Q',
+  R : 'R',
+  S : '$',
+  T : '7',
+  U : 'U',
+  V : 'V',
+  W : 'W',
+  X : 'X',
+  Y : 'Y',
+  Z : '2'
+}
+  return str.replace(/\w/g, (m) => leet[m] ? leet[m] : m);
+}
+
+
+// other solution
+const toLeetSpeak = s => s.replace(/[ABCEGHILOSTZ]/g, c => D[c]);
+
+const D = {
+  A: '@', B: '8', C: '(', E: '3',
+  G: '6', H: '#', I: '!', L: '1',
+  O: '0', S: '$', T: '7', Z: '2'
+};
