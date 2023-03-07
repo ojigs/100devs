f9bac11d06542e94812c536bee40e4d8297a609d
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-03-07 11:53:52 +0100

    Create unluckyDays.js

---
+++ JavaScript/Codewars (restructured)/7kyu/unluckyDays.js
@@ -0,0 +1,17 @@
+https://www.codewars.com/kata/56eb0be52caf798c630013c0/javascript
+// Description:
+
+// Friday 13th or Black Friday is considered as unlucky day. Calculate how many unlucky days are in the given year.
+
+// Find the number of Friday 13th in the given year.
+
+// Input: Year in Gregorian calendar as integer.
+
+// Output: Number of Black Fridays in the year as an integer.
+
+// Examples:
+
+// unluckyDays(2015) == 3
+// unluckyDays(1986) == 1
+
+// Date Time
