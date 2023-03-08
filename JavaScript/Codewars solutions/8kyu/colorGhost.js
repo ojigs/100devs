dc845304cdd2dacf068a6d24a288ca44f3010d60
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-03-08 11:26:37 +0100

    Update colorGhost.js

--- JavaScript/Codewars solutions/8kyu/colorGhost.js
+++ JavaScript/Codewars solutions/8kyu/colorGhost.js
@@ -13,3 +13,19 @@ https://www.codewars.com/kata/53f1015fa9fe02cbda00111a/javascript
 
 // Object-oriented Programming
 // Fundamentals
+
+
+
+// My solution
+var Ghost = function() {
+  const color = ['yellow', 'white', 'purple', 'red']
+  this.color = color[Math.floor(Math.random() * color.length)]
+};
+
+
+// other solution
+class Ghost {
+  constructor() {
+    this.color = [`red`, `yellow`, `purple`, `white`][Math.random() * 4 ^ 0];
+  }
+}
