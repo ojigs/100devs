eb5e9cb775d4a24af8d9445dc36daf837601aeda
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-03-08 10:45:38 +0100

    Create catAndMouseEasyVersion.js

---
+++ JavaScript/Codewars (restructured)/7kyu/catAndMouseEasyVersion.js
@@ -0,0 +1,12 @@
+https://www.codewars.com/kata/57ee24e17b45eff6d6000164/javascript
+// Description:
+
+// You will be given a string (x) featuring a cat 'C' and a mouse 'm'. The rest of the string will be made up of '.'.
+
+// You need to find out if the cat can catch the mouse from it's current position. The cat can jump over three characters. So:
+
+// C.....m returns 'Escaped!' <-- more than three characters between
+
+// C...m returns 'Caught!' <-- as there are three characters between the two, the cat can jump.
+// Fundamentals
+// Strings
