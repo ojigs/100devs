de0c1d8fe7f5da22d18bcdd2fa72c413496e8da5
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-03-15 22:41:06 +0100

    Update and rename compareStringsBySumOfCharacters.js to compareStringsBySumOfChars.js

--- JavaScript/Codewars (restructured)/7kyu/compareStringsBySumOfChars.js
+++ JavaScript/Codewars (restructured)/7kyu/compareStringsBySumOfChars.js
@@ -21,3 +21,25 @@ https://www.codewars.com/kata/576bb3c4b1abc497ec000065/train/javascript
 // Mathematics
 // Strings
 // Fundamentals
+
+
+// My solution
+function compare(s1, s2) {
+  const sum = (str) => {
+    // check if string contains characters other than letters
+    if (str === null || !(/^[a-zA-Z]*$/.test(str))) {
+      str = ""
+    }
+    return str.toUpperCase().split('').reduce((a, c) => a + c.charCodeAt(), 0)
+  }
+  return sum(s1) === sum(s2)
+}
+
+
+// other solution
+function compare(s1, s2) {
+  if (/^\D+$/gi.test(s1) && /^\D+$/gi.test(s2))
+    return s1.split('').map(n => n.toUpperCase().charCodeAt()).reduce((a, b) => a + b) === 
+           s2.split('').map(n => n.toUpperCase().charCodeAt()).reduce((a, b) => a + b);
+  return true;
+}
