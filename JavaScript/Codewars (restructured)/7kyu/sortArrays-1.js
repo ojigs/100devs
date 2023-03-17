fb6c57a738ae2717100a6d5c8e01718ddf165122
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-03-17 11:34:34 +0100

    Create sortArrays-1.js

---
+++ JavaScript/Codewars (restructured)/7kyu/sortArrays-1.js
@@ -0,0 +1,7 @@
+https://www.codewars.com/kata/51f41b98e8f176e70d0002a8/train/javascript
+// Description
+
+// Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+// Sorting
+// Arrays
+// Fundamentals
