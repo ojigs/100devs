1c8f8d50fcd83e88ae9abda65ef66e1eccda0531
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2023-03-18 12:34:20 +0100

    Updatee freudianTranslator.js

--- JavaScript/Codewars solutions/8kyu/freudianTranslator.js
+++ JavaScript/Codewars solutions/8kyu/freudianTranslator.js
@@ -8,3 +8,19 @@
 // Now, basically, when a person hears about Freud, s/he hears "sex" because for Freud, everything was related to, and explained by sex.
 
 // In this kata, the function will take a string as its argument, and return a string with every word replaced by the explanation to everything, according to Freud. Note that an empty string, or no arguments, should return an empty string.
+
+// My solution
+function toFreud(string) {
+  return string.replace(/\S+/g, "sex");
+}
+
+// other solution
+const toFreud = (str) =>
+  str === ""
+    ? ""
+    : str
+        .split(" ")
+        .map((e) => "sex")
+        .join(" ");
+
+var toFreud = (s) => s.replace(/[^ ]+/g, "sex");
