1cb4d94b3a0c4d647d288eb11aacd80cf0e4149c
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-03-20 13:58:56 +0100

    Update alanPartridgeII-AppleTurnover.js

--- JavaScript/Codewars solutions/8kyu/alanPartridgeII-AppleTurnover.js
+++ JavaScript/Codewars solutions/8kyu/alanPartridgeII-AppleTurnover.js
@@ -26,3 +26,16 @@ https://www.codewars.com/kata/580a094553bd9ec5d800007d/javascript
 // Fundamentals
 // Strings
 // Mathematics
+
+
+
+// My solution
+function apple(x){
+  return x**2 > 1000 ? "It's hotter than the sun!!" : "Help yourself to a honeycomb Yorkie for the glovebox."
+}
+
+
+// other solution
+function apple(x){
+  return Math.pow(x, 2) > 1000 ? 'It\'s hotter than the sun!!' : 'Help yourself to a honeycomb Yorkie for the glovebox.' ;
+}
