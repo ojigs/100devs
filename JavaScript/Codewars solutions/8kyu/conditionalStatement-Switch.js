6f777d53f63a8c98fbc5d2edd458fd29f5c3cd71
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-03-22 13:00:15 +0100

    Update conditionalStatement-Switch.js

--- JavaScript/Codewars solutions/8kyu/conditionalStatement-Switch.js
+++ JavaScript/Codewars solutions/8kyu/conditionalStatement-Switch.js
@@ -74,3 +74,40 @@ https://www.codewars.com/kata/572059afc2f4612825000d8a/javascript
 
 // Fundamentals
 // Tutorials
+
+
+
+// My solution
+function howManydays(month){
+  var days;
+  switch (month){
+    case 4:
+      days = 30;
+      break;
+    case 6:
+      days = 30;
+      break;
+    case 9:
+      days = 30;
+      break;
+    case 11:
+      days = 30;
+      break;
+    case 2:
+      days = 28;
+      break;
+    default:
+      days = 31;
+  }
+  return days;
+}
+
+
+// other solution
+function howManydays(month){
+  switch ( month ){
+    case 2 : return 28
+    case 4: case 6: case 9: case 11: return 30
+    default: return 31
+  }
+}
