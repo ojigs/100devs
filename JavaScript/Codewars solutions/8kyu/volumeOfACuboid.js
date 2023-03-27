835ecfc5819daa7dfc2d74f653e680017c8c4679
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-03-27 15:27:05 +0100

    Update volumeOfACuboid.js

--- JavaScript/Codewars solutions/8kyu/volumeOfACuboid.js
+++ JavaScript/Codewars solutions/8kyu/volumeOfACuboid.js
@@ -5,3 +5,12 @@ https://www.codewars.com/kata/58261acb22be6e2ed800003a/train/javascript
 // Geometry
 // Fundamentals
 // Mathematics
+
+
+
+// My solution
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    return length * width* height
+  }
+}
