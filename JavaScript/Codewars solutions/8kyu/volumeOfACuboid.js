2fe8ba3385752755828cd8ef7ae4416da5ccfebe
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-03-27 15:26:19 +0100

    Create volumeOfACuboid.js

---
+++ JavaScript/Codewars solutions/8kyu/volumeOfACuboid.js
@@ -0,0 +1,7 @@
+https://www.codewars.com/kata/58261acb22be6e2ed800003a/train/javascript
+// Description
+
+// Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+// Geometry
+// Fundamentals
+// Mathematics
