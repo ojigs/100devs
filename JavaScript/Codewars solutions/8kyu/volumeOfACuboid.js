e4e83ba62228fe0be9f42d8b707db47e708f83e3
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-03-27 15:31:16 +0100

    Update volumeOfACuboid.js

--- JavaScript/Codewars solutions/8kyu/volumeOfACuboid.js
+++ JavaScript/Codewars solutions/8kyu/volumeOfACuboid.js
@@ -14,3 +14,27 @@ class Kata {
     return length * width* height
   }
 }
+
+
+
+// other solution
+var Kata;
+
+Kata = (function() {
+  function Kata() {}
+
+  Kata.getVolumeOfCuboid = function(length, width, height) {
+    var l = parseFloat(length);
+    var w = parseFloat(width);
+    var h = parseFloat(height);
+
+    if(isNaN(l) || isNaN(w) || isNaN(h)) return 0;
+    if(l<=0 || w<=0 ||h <= 0)  return 0;
+    
+    return l*w*h;
+  };
+
+  return Kata;
+
+})(); 
+// comic :D
