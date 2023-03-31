89fe1010f4235fd013d2a35ae278315712dddc27
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-03-31 22:45:07 +0100

    Create returnStringOfFirstCharacter.js

---
+++ JavaScript/Codewars (restructured)/7kyu/returnStringOfFirstCharacter.js
@@ -0,0 +1,13 @@
+https://www.codewars.com/kata/5639bdcef2f9b06ce800005b/javascript
+// Description:
+
+// In this exercise, a string is passed to a method and a new string has to be returned with the first character of each word in the string.
+
+// For example:
+
+// "This Is A Test" ==> "TIAT"
+
+// Strings will only contain letters and spaces, with exactly 1 space between words, and no leading/trailing spaces.
+// Strings
+// Regular Expressions
+// Fundamentals
