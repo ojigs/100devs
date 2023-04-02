0f9d73740264d4212421a0c4e97df387405d6c1a
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2023-04-02 18:13:03 +0100

    indexedCapitalization.js

---
+++ JavaScript/Codewars (restructured)/7kyu/indexedCapitalization.js
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/59cfc09a86a6fdf6df0000f1/javascript
+// Description:
+
+// Given a string and an array of integers representing indices, capitalize all letters at the given indices.
+
+// For example:
+
+//     capitalize("abcdef",[1,2,5]) = "aBCdeF"
+//     capitalize("abcdef",[1,2,5,100]) = "aBCdeF". There is no index 100.
+
+// The input will be a lowercase string with no spaces and an array of digits.
+
+// Good luck!
+
+// Be sure to also try:
+
+// Alternate capitalization
+
+// String array revisal
+// Fundamentals
