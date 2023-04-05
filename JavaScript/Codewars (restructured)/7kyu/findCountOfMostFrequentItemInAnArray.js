17c3eb94042aa0f1d9aea942cdd64a7f42792870
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-04-05 14:53:43 +0100

    Create findCountOfMostFrequentItemInAnArray.js

---
+++ JavaScript/Codewars (restructured)/7kyu/findCountOfMostFrequentItemInAnArray.js
@@ -0,0 +1,12 @@
+https://www.codewars.com/kata/56582133c932d8239900002e/javascript
+// Description:
+
+// Complete the function to find the count of the most frequent item of an array. You can assume that input is an array of integers. For an empty array return 0
+// Example
+
+// input array: [3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3]
+// ouptut: 5 
+
+// The most frequent number in the array is -1 and it occurs 5 times.
+// Data Structures
+// Fundamentals
