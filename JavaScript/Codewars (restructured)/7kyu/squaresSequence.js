55e4f1d186408e83fbd1e31fa50f1f6987cee861
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-04-11 13:43:30 +0100

    Create squaresSequence.js

---
+++ JavaScript/Codewars (restructured)/7kyu/squaresSequence.js
@@ -0,0 +1,11 @@
+https://www.codewars.com/kata/5546180ca783b6d2d5000062/train/javascript
+// Description
+
+// Complete the function that returns an array of length n, starting with the given number x and the squares of the previous number. If n is negative or zero, return an empty array/list.
+// Examples
+
+// 2, 5  -->  [2, 4, 16, 256, 65536]
+// 3, 3  -->  [3, 9, 81]
+
+// Mathematics
+// Fundamentals
