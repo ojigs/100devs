eda96dc5b3a979ceafda2131a9a4a3b14c53360c
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-04-12 13:18:03 +0100

    Create flatten.js

---
+++ JavaScript/Codewars (restructured)/7kyu/flatten.js
@@ -0,0 +1,12 @@
+https://www.codewars.com/kata/5250a89b1625e5decd000413/javascript
+// Description:
+
+// Write a function that flattens an Array of Array objects into a flat Array. Your function must only do one level of flattening.
+
+// flatten([1,2,3]) // => [1,2,3]
+// flatten([[1,2,3],["a","b","c"],[1,2,3]])  // => [1,2,3,"a","b","c",1,2,3]
+// flatten([[[1,2,3]]]) // => [[1,2,3]]
+
+// Functional Programming
+// Arrays
+// Fundamentals
