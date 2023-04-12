556532fa24d72a8d671f2b707af3e5a956938d95
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-04-12 12:39:56 +0100

    Update numbersToLetters.js

--- JavaScript/Codewars (restructured)/7kyu/numbersToLetters.js
+++ JavaScript/Codewars (restructured)/7kyu/numbersToLetters.js
@@ -1,5 +1,28 @@
+https://www.codewars.com/kata/57ebaa8f7b45ef590c00000c/train/javascript
 // Descriotion
 
 // Given an array of numbers (in string format), you must return a string. The numbers correspond to the letters of the alphabet in reverse order: a=26, z=1 etc. You should also account for '!', '?' and ' ' that are represented by '27', '28' and '29' respectively.
 
 // All inputs will be valid.
+
+
+
+// My solution
+function switcher(x){
+  let map = "_zyxwvutsrqponmlkjihgfedcba!? ".split('')
+  return x.map(e => map[e]).join('')
+}
+
+
+// other solution
+function switcher(x)
+{
+  return x.map(a => 
+    {
+      if(a == 27) return "!";
+      if(a == 28) return "?";
+      if(a == 29) return " ";
+      return String.fromCharCode(97 + 26 - parseInt(a));
+      
+    }).join('');
+}
