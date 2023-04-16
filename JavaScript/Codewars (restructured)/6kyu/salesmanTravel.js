d865129b00f43d54b85458b24c182a28ccf44ce1
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2023-04-16 12:58:17 +0100

    Update salesmanTravel.js

--- JavaScript/Codewars (restructured)/6kyu/salesmanTravel.js
+++ JavaScript/Codewars (restructured)/6kyu/salesmanTravel.js
@@ -35,3 +35,39 @@
 // You can see a few addresses and zipcodes in the test cases.
 // Fundamentals
 // Strings
+
+// My solution
+function travel(r, zipcode) {
+  if (zipcode === "") return ":/";
+  const matchingAddresses = r
+    .split(",")
+    .filter((addr) => addr.trim().endsWith(zipcode))
+    .map((addr) => addr.trim().split(" ").slice(1, -2).join(" "));
+  const houseNumbers = r
+    .split(",")
+    .filter((addr) => addr.trim().endsWith(zipcode))
+    .map((addr) => addr.trim().split(" ")[0]);
+  return houseNumbers.length > 0
+    ? `${zipcode}:${matchingAddresses.join(",")}/${houseNumbers.join(",")}`
+    : `${zipcode}:/`;
+}
+
+// other solution
+function travel(r, zipcode) {
+  const addressList = r.split(",");
+
+  const streets = [];
+  const numbers = [];
+
+  addressList.forEach((address) => {
+    let zip = address.substring(address.length - 8, address.length);
+    if (zip === zipcode) {
+      let number = address.substring(0, address.indexOf(" ") + 1);
+      let street = address.substring(address.indexOf(" "), address.length - 8);
+
+      streets.push(street.trim());
+      numbers.push(number.trim());
+    }
+  });
+  return `${zipcode}:${streets.join(",")}/${numbers.join(",")}`;
+}
