43a919f7c18b643e10815eaa6148fcf6a2ac9c93
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-04-17 13:19:27 +0100

    Update checkThreeAndTwo.js

--- JavaScript/Codewars (restructured)/7kyu/checkThreeAndTwo.js
+++ JavaScript/Codewars (restructured)/7kyu/checkThreeAndTwo.js
@@ -20,3 +20,13 @@ function checkThreeAndTwo(array) {
   const val = Object.values(count)
   return val.includes(3) && val.includes(2)
 }
+
+
+// other solution
+function checkThreeAndTwo(array) {
+  let as = array.filter(x =>x === 'a').length;
+  let bs = array.filter(x =>x === 'b').length;
+  let cs = array.filter(x =>x === 'c').length;
+  return (as === 3 || bs === 3 || cs=== 3)  
+          && (as === 2 || bs === 2 || cs === 2);
+}
