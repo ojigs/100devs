7752031a9a5ee63d7ce9ebd162f32ff0da26b7fa
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-04-19 15:11:07 +0100

    Create areTheyBalanced.js

---
+++ JavaScript/Codewars (restructured)/6kyu/areTheyBalanced.js
@@ -0,0 +1,14 @@
+https://www.codewars.com/kata/57fb44a12b53146fe1000136/javascript
+// Description:
+
+// Each exclamation mark's weight is 2; each question mark's weight is 3. Putting two strings left and right on the balance - are they balanced?
+
+// If the left side is more heavy, return "Left"; if the right side is more heavy, return "Right"; if they are balanced, return "Balance".
+// Examples
+
+// "!!", "??"     -->  "Right"
+// "!??", "?!!"   -->  "Left"
+// "!?!!", "?!?"  -->  "Left"
+// "!!???!????", "??!!?!!!!!!!"  -->  "Balance"
+
+// Fundamentals
