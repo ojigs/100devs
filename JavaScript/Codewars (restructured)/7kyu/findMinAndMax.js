e0b728a2f7355901c8becbd42b77bea45d89c07d
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2023-04-23 17:03:33 +0100

    Create findMinAndMax.js

---
+++ JavaScript/Codewars (restructured)/7kyu/findMinAndMax.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/57a1ae8c7cb1f31e4e000130/javascript
+// Description:
+
+// Implement a function that returns the minimal and the maximal value of a list (in this order).
+// Fundamentals
