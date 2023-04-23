c20e0a2322d69d1611170fd48e0792365e05613b
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2023-04-23 17:08:23 +0100

    Update findMinAndMax.js

--- JavaScript/Codewars (restructured)/7kyu/findMinAndMax.js
+++ JavaScript/Codewars (restructured)/7kyu/findMinAndMax.js
@@ -3,3 +3,14 @@
 
 // Implement a function that returns the minimal and the maximal value of a list (in this order).
 // Fundamentals
+
+// My solution
+function getMinMax(arr) {
+  return [Math.min(...arr), Math.max(...arr)];
+}
+
+// other solution
+function getMinMax(arr) {
+  const sorted = arr.sort((a, b) => a - b);
+  return [sorted[0], sorted[sorted.length - 1]];
+}
