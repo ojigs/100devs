46df6d249ebac06ca6aede4fb13b782b2263d8a3
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-04-25 12:14:28 +0100

    Update basicDataTypes-Array.js

--- JavaScript/Codewars solutions/8kyu/basicDataTypes-Array.js
+++ JavaScript/Codewars solutions/8kyu/basicDataTypes-Array.js
@@ -30,3 +30,37 @@ https://www.codewars.com/kata/571effabb625ed9b0600107a/javascript
 // When you have finished the work, click "Run Tests" to see if your code is working properly.
 
 // In the end, click "Submit" to submit your code pass this kata.
+
+
+
+// My solution
+function getLength(arr){
+  return arr.length
+}
+function getFirst(arr){
+  return arr[0]
+}
+function getLast(arr){
+  return arr[arr.length - 1]
+}
+function pushElement(arr){
+  var el=1;
+  arr.push(el)
+  return arr
+}
+function popElement(arr){
+  arr.pop()
+  return arr
+}
+
+
+// other solution
+const getLength = array => array.length;
+
+const getFirst = array => array[0];
+
+const getLast = array => array[array.length - 1];
+
+const pushElement = array => array.concat(0);
+
+const popElement = array => array.splice(0, array.length - 1);
