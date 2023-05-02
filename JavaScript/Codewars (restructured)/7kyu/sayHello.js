43f420377d0252c427c18badfdefc9b6bf075d15
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-05-02 14:18:44 +0100

    Create sayHello.js

---
+++ JavaScript/Codewars (restructured)/7kyu/sayHello.js
@@ -0,0 +1,14 @@
+https://www.codewars.com/kata/55955a48a4e9c1a77500005a/javascript
+// Description:
+
+// Say hello!
+
+// Write a function to greet a person. Function will take name as input and greet the person by saying hello. Return null/nil/None if input is empty string or null/nil/None.
+
+// Example:
+
+// greet("Niks") === "hello Niks!";
+// greet("") === null; // Return null if input is empty string
+// greet(null) === null; // Return null if input is null
+
+// Fundamentals
