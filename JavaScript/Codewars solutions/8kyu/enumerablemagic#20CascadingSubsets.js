6a096743ae16a110861698a52a03609fabbde008
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-05-02 14:16:25 +0100

    Update enumerablemagic#20CascadingSubsets.js

--- JavaScript/Codewars solutions/8kyu/enumerablemagic#20CascadingSubsets.js
+++ JavaScript/Codewars solutions/8kyu/enumerablemagic#20CascadingSubsets.js
@@ -11,3 +11,25 @@ https://www.codewars.com/kata/545af3d185166a3dec001190/train/javascript
   
 
 // As you can see, the lists are cascading; ie, they overlap, but never out of order.
+
+
+// My solution
+function eachCons(array, n) {
+  let res = []
+	for (let i = 0; i < array.length - n + 1; i++) {
+    res.push(array.slice(i, n+i))
+  }
+  return res
+}
+
+
+// other solution
+function eachCons(array, n) {
+  let res = [];
+  
+  for(let i = 0; i < array.length; i++){
+    res.push(array.slice(i,i + n));
+  }
+  
+	return res.filter(e => e.length === n);
+}
