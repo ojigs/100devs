dbefe3fa483c6938f26ee52435ac3185f9e4ed1e
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-05-02 14:59:56 +0100

    Update tribonacciSequence.js

--- JavaScript/Codewars (restructured)/6kyu/tribonacciSequence.js
+++ JavaScript/Codewars (restructured)/6kyu/tribonacciSequence.js
@@ -24,3 +24,26 @@ https://www.codewars.com/kata/556deca17c58da83c00002db/train/javascript
 // Arrays
 // Lists
 // Fundamentals
+
+
+
+// My solution
+function tribonacci(signature,n){
+  if (n === 0) {return []};
+  if (n < 3) {return signature.slice(0, n)}
+  
+  for (let i = 3; i < n; i++) {
+    signature.push((signature[i - 1] + signature[i - 2] + signature[i - 3]))
+  }
+  return signature;
+}
+
+
+// other solution
+function tribonacci(s,n){
+  var arr = [];
+  for(var i=0; i<n; i++) {
+    arr.push((i<3) ? s[i] : arr[i-1]+arr[i-2]+arr[i-3]);
+  }
+  return arr;
+}
