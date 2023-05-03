970aa4f20b26a61e0dc85e9a3b0bb99a025515cd
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-05-03 12:41:56 +0100

    Create basicMathAddOrSubtract.js

---
+++ JavaScript/Codewars (restructured)/7kyu/basicMathAddOrSubtract.js
@@ -0,0 +1,13 @@
+https://www.codewars.com/kata/5809b62808ad92e31b000031/javascript
+// Description:
+
+// In this kata, you will do addition and subtraction on a given string. The return value must be also a string.
+
+// Note: the input will not be empty.
+// Examples
+
+// "1plus2plus3plus4"  --> "10"
+// "1plus2plus3minus4" -->  "2"
+
+// Strings
+// Fundamentals
