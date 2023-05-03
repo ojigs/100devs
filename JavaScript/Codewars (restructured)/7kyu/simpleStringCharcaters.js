32d2214fd30a233e63f1c15266e0fce654d87003
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-05-03 13:20:08 +0100

    Update simpleStringCharcaters.js

--- JavaScript/Codewars (restructured)/7kyu/simpleStringCharcaters.js
+++ JavaScript/Codewars (restructured)/7kyu/simpleStringCharcaters.js
@@ -10,3 +10,32 @@ https://www.codewars.com/kata/5a29a0898f27f2d9c9000058/javascript
 
 // Good luck!
 // Fundamentals
+
+
+
+// My solution
+function solve(s){
+ const upperCase = (s.match(/[A-Z]/g) || []).length
+ const lowerCase = (s.match(/[a-z]/g) || []).length
+ const numbers = (s.match(/\d/g) || []).length
+ const specialChar = (s.match(/[^A-Za-z\d]/g) || []).length
+ return [upperCase, lowerCase, numbers, specialChar]
+}
+
+
+
+// other solution
+const solve = (string) => (
+  Object.values([...string].reduce((acc, char) => {
+    if (/[A-Z]/.test(char)) {
+      acc.upper++;
+    } else if (/[a-z]/.test(char)) {
+      acc.lower++;
+    } else if (/[0-9]/.test(char)) {
+      acc.number++;
+    } else {
+      acc.other++;
+    }
+    return acc;
+  }, { upper: 0, lower: 0, number: 0, other: 0 }))
+);
