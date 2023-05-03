19b5c042c9f00f6136e8cb5f94bdb1ba33b218f7
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-05-03 13:02:50 +0100

    Create digitsExplosion.js

---
+++ JavaScript/Codewars (restructured)/7kyu/digitsExplosion.js
@@ -0,0 +1,21 @@
+https://www.codewars.com/kata/585b1fafe08bae9988000314/javascript
+// Description:
+
+// Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value.
+// Examples
+
+// explode("312")
+
+// should return :
+
+// "333122"
+
+// explode("102269")
+
+// should return :
+
+// "12222666666999999999"
+
+// Strings
+// Regular Expressions
+// Fundamentals
