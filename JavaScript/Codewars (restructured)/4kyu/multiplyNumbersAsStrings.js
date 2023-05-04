817bf62a44c46a1013ad8531b483088c4a8562b6
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2023-05-04 11:44:07 +0100

    Create multiplyyNumbersAsStrings.js

---
+++ JavaScript/Codewars (restructured)/4kyu/multiplyNumbersAsStrings.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/55911ef14065454c75000062/train/javascript
+// Description
+
+// This is the first part. You can solve the second part here when you are done with this. Multiply two numbers! Simple!
+
+//     The arguments are passed as strings.
+//     The numbers may be way very large
+//     Answer should be returned as a string
+//     The returned "number" should not start with zeros e.g. 0123 is invalid
+
+// Note: 100 randomly generated tests!
+// Strings
+// Big Integers
+// Algorithms
