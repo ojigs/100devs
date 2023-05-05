0aa8e714fc3a7fa568a3e5f0beb7d34720067ed1
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-05-05 12:16:20 +0100

    Update regexpBasicsIsItAVowel.js

--- JavaScript/Codewars (restructured)/7kyu/regexpBasicsIsItAVowel.js
+++ JavaScript/Codewars (restructured)/7kyu/regexpBasicsIsItAVowel.js
@@ -4,3 +4,16 @@ https://www.codewars.com/kata/567bed99ee3451292c000025/javascript
 // Implement the function which should return true if given object is a vowel (meaning a, e, i, o, u, uppercase or lowercase), and false otherwise.
 // Regular Expressions
 // Fundamentals
+
+
+
+// My solution
+String.prototype.vowel = function() {
+  return /^[aeiou]$/i.test(this)
+};
+
+
+// othter solution
+String.prototype.vowel = function() {
+  return /^[aeiouAEIOU]$/.test(this);
+};
