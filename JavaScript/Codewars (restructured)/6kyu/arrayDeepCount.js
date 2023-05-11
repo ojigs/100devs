e38e4365469dd3f784e9a635b7adc1a2b2775994
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-05-11 10:47:15 +0100

    Create arrayDeepCount.js

---
+++ JavaScript/Codewars (restructured)/6kyu/arrayDeepCount.js
@@ -0,0 +1,15 @@
+https://www.codewars.com/kata/596f72bbe7cd7296d1000029/javascript
+// Description:
+
+// You are given an array. Complete the function that returns the number of ALL elements within an array, including any nested arrays.
+// Examples
+
+// []                   -->  0
+// [1, 2, 3]            -->  3
+// ["x", "y", ["z"]]    -->  4
+// [1, 2, [3, 4, [5]]]  -->  7
+
+// The input will always be an array.
+// Arrays
+// Recursion
+// Fundamentals
