9442d96dc62bb34f6f88634cdfa6e22bf49efe67
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2023-05-14 11:56:04 +0100

    Update simpleFun#352-ReagebtFormula.js

--- JavaScript/Codewars solutions/8kyu/simpleFun#352-ReagentFormula.js
+++ JavaScript/Codewars solutions/8kyu/simpleFun#352-ReagentFormula.js
@@ -37,3 +37,19 @@
 //     Happy Coding ^_^
 
 // Fundamentals
+
+// My solution
+function isValid(formula) {
+  if (formula.includes(1) && formula.includes(2)) return false;
+  if (formula.includes(3) && formula.includes(4)) return false;
+  if (formula.includes(5) !== formula.includes(6)) return false;
+  if (!(formula.includes(7) || formula.includes(8))) return false;
+  return true;
+}
+
+// other solution
+const isValid = (a) =>
+  !(a.includes(1) && a.includes(2)) &&
+  !(a.includes(3) && a.includes(4)) &&
+  a.includes(5) === a.includes(6) &&
+  (a.includes(7) || a.includes(8));
