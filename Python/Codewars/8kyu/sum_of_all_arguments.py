324fcdd8529a4aaf54779c68202ad5fbf2b0f7d6
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-05-15 12:20:20 +0100

    Update sum_of_all_arguments.py

--- Python/Codewars/8kyu/sum_of_all_arguments.py
+++ Python/Codewars/8kyu/sum_of_all_arguments.py
@@ -17,3 +17,22 @@ https://www.codewars.com/kata/540c33513b6532cd58000259/train/python
 
 # C: https://www.geeksforgeeks.org/variadic-functions-in-c/
 # Fundamentals
+
+
+# My solution
+def sum_args(*args):
+    return sum(args)
+  
+def sum_args(*args):
+  sum = 0
+  for arg in args:
+    sum += arg
+  return sum
+
+
+# other solution
+sum_args = lambda *x:sum(x)
+
+
+# learning curve: we can use lambda function to define an anonymous function with any number of arguments but only one expression with Python. This is similar to arrow functions in javascript
+# these get passed as arguments to higher order functions, but are used sparingly for readibility.
