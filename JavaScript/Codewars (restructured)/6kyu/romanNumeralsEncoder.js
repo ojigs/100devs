fa58a8eca99670da03e7dbc255a4aa454648e28e
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-05-16 12:18:20 +0100

    Update romanNumeralsEncoder.js

--- JavaScript/Codewars (restructured)/6kyu/romanNumeralsEncoder.js
+++ JavaScript/Codewars (restructured)/6kyu/romanNumeralsEncoder.js
@@ -24,3 +24,50 @@ https://www.codewars.com/kata/51b62bf6a9c58071c600001b/train/javascript
 
 // More about roman numerals - http://en.wikipedia.org/wiki/Roman_numerals
 // Algorithms
+
+
+
+// My solution
+function solution(number){
+  const soln = {
+    1: "I",
+    4: "IV",
+    5: "V",
+    9: "IX",
+    10: "X",
+    40: "XL",
+    50: "L",
+    90: "XC",
+    100: "C",
+    400: "CD",
+    500: "D",
+    900: "CM",
+    1000: "M"
+  }
+  let numeral = ""
+  const keys = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+  let rem = number
+  for (const key of keys) {
+    while (rem >= key) {
+      numeral += soln[key]
+      rem -= +key
+    }
+  }
+  return numeral
+}
+
+
+// other solution
+function solution(number){
+  // convert the number to a roman numeral
+var  roman = {M:1000,CM:900, D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1 }
+
+var ans = '';
+while(number>0){
+    for(a in roman){ 
+        if(roman[a]<=number){ ans += a; number-=roman[a]; break;}
+            
+    }
+}
+return ans;
+}
