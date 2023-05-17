5bf80d5f5672803aa5373ddde3a5b57ebaef4df0
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-05-17 15:35:23 +0100

    Update scramblies.js

--- JavaScript/Codewars (restructured)/5kyu/scramblies.js
+++ JavaScript/Codewars (restructured)/5kyu/scramblies.js
@@ -17,3 +17,34 @@ https://www.codewars.com/kata/55c04b4cc56a697bb0000048/train/javascript
 // Strings
 // Performance
 // Algorithms
+
+
+// My solution
+function scramble(str1, str2) {
+  const count = {}
+  for (let i = 0; i < str1.length; i++) {
+    count[str1[i]] = (count[str1[i]] || 0) + 1
+  }
+  for (let i = 0; i < str2.length; i++) {
+    if (!count[str2[i]]) return false
+    count[str2[i]]--
+  }
+  return true
+}
+
+
+
+// other solution
+function scramble(str1, str2) {
+ let str1Hash = {}
+ let containsScramble = true
+ for (const char1 of str1) {
+   str1Hash[char1] ? str1Hash[char1] += 1 : str1Hash[char1] = 1
+ }
+ 
+ for (const char2 of str2) {
+   str1Hash[char2] ? str1Hash[char2] -= 1 : containsScramble = false
+ }
+ 
+ return containsScramble
+}
