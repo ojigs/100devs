022a76e2799a75136309b416a43f73727333ab5c
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-05-18 16:19:42 +0100

    Update howManyArguments.js

--- JavaScript/Codewars (restructured)/7kyu/howManyArguments.js
+++ JavaScript/Codewars (restructured)/7kyu/howManyArguments.js
@@ -5,3 +5,15 @@ https://www.codewars.com/kata/5c44b0b200ce187106452139/train/javascript
 // args_count(1, 2, 3, 10) -> 4
 
 // Fundamentals
+
+
+// My solution
+function args_count(...args) {
+  return args.length
+}
+
+
+// other solution
+function args_count() {
+  return arguments.length;
+}
