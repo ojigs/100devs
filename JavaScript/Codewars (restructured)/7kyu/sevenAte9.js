e9114db31eac62fb0ab991a0a313e4616071e3f3
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-05-19 10:50:46 +0100

    Create sevenAte9.js

---
+++ JavaScript/Codewars (restructured)/7kyu/sevenAte9.js
@@ -0,0 +1,11 @@
+https://www.codewars.com/kata/559f44187fa851efad000087/javascript
+// Description:
+
+// Write a function that removes every lone 9 that is inbetween 7s.
+
+// "79712312" --> "7712312"
+// "79797"    --> "777"
+
+// Logic
+// Strings
+// Fundamentals
