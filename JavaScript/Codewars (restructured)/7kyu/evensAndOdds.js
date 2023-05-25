43cc561c82888d0c33341a403088539179827afb
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2023-05-25 11:41:51 +0100

    Create evensAndOdds.js

---
+++ JavaScript/Codewars (restructured)/7kyu/evensAndOdds.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/583ade15666df5a64e000058/javascript
+// Description:
+
+// This kata is about converting numbers to their binary or hexadecimal representation:
+
+//     If a number is even, convert it to binary.
+//     If a number is odd, convert it to hex.
+
+// Numbers will be positive. The hexadecimal string should be lowercased.
+// Fundamentals
+// Binary
