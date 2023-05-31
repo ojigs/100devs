82eb218437305cf5ed1d9be3f00dad4055a4703e
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-05-31 14:06:20 +0100

    Create filterLongWords.js

---
+++ JavaScript/Codewars (restructured)/7kyu/filterLongWords.js
@@ -0,0 +1,11 @@
+https://www.codewars.com/kata/5697fb83f41965761f000052/javascript
+// Description:
+
+// Write a function that takes a string and an an integer n as parameters and returns a list of all words that are longer than n.
+
+// Example:
+
+// * With input "The quick brown fox jumps over the lazy dog", 4
+// * Return ['quick', 'brown', 'jumps']
+
+// Fundamentals
