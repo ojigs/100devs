abe9144adce72a3ebd8fcb0728e9025c62f920df
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-06-09 15:59:26 +0100

    Create initializeMyName.js

---
+++ JavaScript/Codewars (restructured)/7kyu/initializeMyName.js
@@ -0,0 +1,15 @@
+https://www.codewars.com/kata/5768a693a3205e1cc100071f/javascript
+// Description:
+
+// Some people just have a first name; some people have first and last names and some people have first, middle and last names.
+
+// You task is to initialize the middle names (if there is any).
+// Examples
+
+// 'Jack Ryan'                   => 'Jack Ryan'
+// 'Lois Mary Lane'              => 'Lois M. Lane'
+// 'Dimitri'                     => 'Dimitri'
+// 'Alice Betty Catherine Davis' => 'Alice B. C. Davis'
+
+// Strings
+// Fundamentals
