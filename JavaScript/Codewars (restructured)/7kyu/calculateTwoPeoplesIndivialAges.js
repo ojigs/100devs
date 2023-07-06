c502c00e05eec5e3663ee0ac6887d97a51012a25
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2023-07-06 12:04:25 +0100

    Create calculateTwoPeoplesIndividualAges.js

---
+++ JavaScript/Codewars (restructured)/7kyu/calculateTwoPeoplesIndivialAges.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/58e0bd6a79716b7fcf0013b1/discuss/javascript
+// Description:
+
+// Create a function that takes in the sum and age difference of two people, calculates their individual ages, and returns a pair of values (oldest age first) if those exist or null/None if:
+
+//     sum < 0
+//     difference < 0
+//     Either of the calculated ages come out to be negative
+
+// Fundamentals
+// Algorithms
+// Arrays
