78e8ef54da179cd6b1f79dbe92d2b66045f960d7
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-07-24 13:01:35 +0100

    Update tripChecker.js

--- JavaScript/Codewars (restructured)/5kyu/tripChecker.js
+++ JavaScript/Codewars (restructured)/5kyu/tripChecker.js
@@ -40,3 +40,54 @@ https://www.codewars.com/kata/52ee76fdf5f13567000005ae/train/javascript
 // Note for Ruby: The Hash provided for the 'links' argument will always have default = []
 // Algorithms
 // Graph Theory
+
+
+// My solution
+function checkTrip(start, target, stationLinks) {
+  // Queue for BFS
+  const queue = [start];
+  // Set to keep track of visited stations
+  const visited = new Set();
+
+  while (queue.length > 0) {
+    const currentStation = queue.shift();
+    visited.add(currentStation);
+
+    if (currentStation === target) {
+      return "Possible";
+    }
+
+    const nextStations = stationLinks[currentStation] || [];
+    for (const nextStation of nextStations) {
+      if (!visited.has(nextStation)) {
+        queue.push(nextStation);
+      }
+    }
+  }
+
+  return "Impossible";
+};
+
+
+// other solution
+function checkTrip(start, target, stationLinks) {
+	let everBeen = [start]
+	let to = stationLinks[start]
+	let ck = false
+	return recurFindLocation(everBeen,to,target,stationLinks,ck) ? 'Possible':'Impossible'
+	
+};
+
+function recurFindLocation(everBeen,to,target,stationLinks,ck){
+	if (to && to.length > 0 && to.includes(target))return true
+	else if (to && to.length > 0){
+		to = to.filter((locate)=>{
+			if(!everBeen.includes(locate))return locate
+		})
+		to.forEach((locate)=>{
+			everBeen.push(locate)
+			if (recurFindLocation(everBeen,stationLinks[locate],target,stationLinks))return ck = true
+		})	
+	}
+	return ck
+}
