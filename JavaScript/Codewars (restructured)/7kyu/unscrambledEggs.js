5fd06c076ea96ae3dc1873f3ef5707086d055914
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-07-25 13:39:43 +0100

    Update unscrambledEggs.js

--- JavaScript/Codewars (restructured)/7kyu/unscrambledEggs.js
+++ JavaScript/Codewars (restructured)/7kyu/unscrambledEggs.js
@@ -11,3 +11,18 @@ https://www.codewars.com/kata/55ea5650fe9247a2ea0000a7/train/javascript
 
 // Kata is supposed to be for beginners to practice regular expressions, so commenting would be appreciated.
 // Fundamentals
+
+
+// My solution
+function unscrambleEggs(word){
+  return word.replace(/^[aeiou]/, '')
+}
+
+
+// other solution
+function unscrambleEggs(word){
+  return word.replace(/egg/g, "");
+}
+
+
+const unscrambleEggs = word => word.replace(/([^aeiou])egg/gi, '$1');
