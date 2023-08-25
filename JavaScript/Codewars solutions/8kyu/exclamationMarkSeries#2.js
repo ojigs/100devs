ad250f4a7c0078d5d1d2ac85f9776024784c6bff
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-08-25 11:59:49 +0100

    Create exclamationMarkSeries#2.js

---
+++ JavaScript/Codewars solutions/8kyu/exclamationMarkSeries#2.js
@@ -0,0 +1,15 @@
+https://www.codewars.com/kata/57faece99610ced690000165/javascript
+// Description:
+// Description:
+
+// Remove all exclamation marks from the end of sentence.
+// Examples
+
+// "Hi!"     ---> "Hi"
+// "Hi!!!"   ---> "Hi"
+// "!Hi"     ---> "!Hi"
+// "!Hi!"    ---> "!Hi"
+// "Hi! Hi!" ---> "Hi! Hi"
+// "Hi"      ---> "Hi"
+
+// Fundamentals
