854260222c45359bf5b6610545701a63832144d0
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-08-25 12:09:39 +0100

    Update exclamationMarkSeries#2.js

--- JavaScript/Codewars solutions/8kyu/exclamationMarkSeries#2.js
+++ JavaScript/Codewars solutions/8kyu/exclamationMarkSeries#2.js
@@ -13,3 +13,20 @@ https://www.codewars.com/kata/57faece99610ced690000165/javascript
 // "Hi"      ---> "Hi"
 
 // Fundamentals
+
+
+// My solution
+function remove (string) {  
+  return string.replace(/!+$/, "")
+}
+
+
+// other solution
+function remove(s)
+{
+    while(s && s.slice(-1) == "!") 
+    { 
+        s = s.slice(0,-1) 
+    }
+    return s;
+}
