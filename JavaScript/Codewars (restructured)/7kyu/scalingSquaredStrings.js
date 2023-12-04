a9f3e3eb779e1b7d7d2050540754d5f4a3b5d9db
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2023-12-04 15:44:02 +0100

    Update scalingSquaredStrings.js

--- JavaScript/Codewars (restructured)/7kyu/scalingSquaredStrings.js
+++ JavaScript/Codewars (restructured)/7kyu/scalingSquaredStrings.js
@@ -1,43 +1,57 @@
 https://www.codewars.com/kata/56ed20a2c4e5d69155000301/train/javascript
-Description
+// Description
 
-You are given a string of n lines, each substring being n characters long. For example:
+// You are given a string of n lines, each substring being n characters long. For example:
 
-s = "abcd\nefgh\nijkl\nmnop"
+// s = "abcd\nefgh\nijkl\nmnop"
 
-We will study the "horizontal" and the "vertical" scaling of this square of strings.
+// We will study the "horizontal" and the "vertical" scaling of this square of strings.
 
-A k-horizontal scaling of a string consists of replicating k times each character of the string (except '\n').
+// A k-horizontal scaling of a string consists of replicating k times each character of the string (except '\n').
 
-    Example: 2-horizontal scaling of s: => "aabbccdd\neeffgghh\niijjkkll\nmmnnoopp"
+//     Example: 2-horizontal scaling of s: => "aabbccdd\neeffgghh\niijjkkll\nmmnnoopp"
 
-A v-vertical scaling of a string consists of replicating v times each part of the squared string.
+// A v-vertical scaling of a string consists of replicating v times each part of the squared string.
 
-    Example: 2-vertical scaling of s: => "abcd\nabcd\nefgh\nefgh\nijkl\nijkl\nmnop\nmnop"
+//     Example: 2-vertical scaling of s: => "abcd\nabcd\nefgh\nefgh\nijkl\nijkl\nmnop\nmnop"
 
-Function scale(strng, k, v) will perform a k-horizontal scaling and a v-vertical scaling.
+// Function scale(strng, k, v) will perform a k-horizontal scaling and a v-vertical scaling.
 
-Example: a = "abcd\nefgh\nijkl\nmnop"
-scale(a, 2, 3) --> "aabbccdd\naabbccdd\naabbccdd\neeffgghh\neeffgghh\neeffgghh\niijjkkll\niijjkkll\niijjkkll\nmmnnoopp\nmmnnoopp\nmmnnoopp"
+// Example: a = "abcd\nefgh\nijkl\nmnop"
+// scale(a, 2, 3) --> "aabbccdd\naabbccdd\naabbccdd\neeffgghh\neeffgghh\neeffgghh\niijjkkll\niijjkkll\niijjkkll\nmmnnoopp\nmmnnoopp\nmmnnoopp"
 
-Printed:
+// Printed:
 
-abcd   ----->   aabbccdd
-efgh            aabbccdd
-ijkl            aabbccdd
-mnop            eeffgghh
-                eeffgghh
-                eeffgghh
-                iijjkkll
-                iijjkkll
-                iijjkkll
-                mmnnoopp
-                mmnnoopp
-                mmnnoopp
+// abcd   ----->   aabbccdd
+// efgh            aabbccdd
+// ijkl            aabbccdd
+// mnop            eeffgghh
+//                 eeffgghh
+//                 eeffgghh
+//                 iijjkkll
+//                 iijjkkll
+//                 iijjkkll
+//                 mmnnoopp
+//                 mmnnoopp
+//                 mmnnoopp
 
-Task:
+// Task:
 
-    Write function scale(strng, k, v) k and v will be positive integers. If strng == "" return "".
+//     Write function scale(strng, k, v) k and v will be positive integers. If strng == "" return "".
 
-Fundamentals
-Strings
+// Fundamentals
+// Strings
+
+
+// My solution
+function scale(strng, k, n) {
+  if (!strng) return ""
+  return strng.split('\n').map(line => line.split('').map(char => char.repeat(k)).join('')).map(line => Array(n).fill(line).join('\n')).join('\n');
+}
+
+
+// other solution
+const scale = (str, k, n) =>
+  str
+    .replace(/[^\n]/g,  c => Array(k + 1).join(c))
+    .replace(/[^\n]+/g, c => Array(n + 1).join("\n" + c).slice(1));
