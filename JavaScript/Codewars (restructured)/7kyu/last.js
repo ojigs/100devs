ad49e5adbf234c8da86e2d8c0253ad5f31b20076
Emmanuel Ojighoro <97135926+ojigs@users.noreply.github.com>
2024-09-24 11:36:41 +0100

    Create last.js

---
+++ JavaScript/Codewars (restructured)/7kyu/last.js
@@ -0,0 +1,10 @@
+https://www.codewars.com/kata/541629460b198da04e000bb9/train/javascript
+// Find the last element of the given argument(s). If a single argument is passed and is a list/array or a string, return its last element. It is guaranteed that there will be at least one argument and that single-argument arrays/lists/strings will not be empty.
+
+// Examples
+// last(5)               ==> 5
+// last([1, 2, 3, 4])    ==>  4
+// last("xyz")           ==> "z"
+// last(1, 2, 3, 4)      ==>  4
+// last([1, 2], [3, 4])  ==>  [3, 4]
+// last([[1, 2], [3, 4]])  ==>  [3, 4]
